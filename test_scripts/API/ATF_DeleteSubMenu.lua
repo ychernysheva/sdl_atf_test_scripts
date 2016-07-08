f609bd98ba98ada29624e8fe99af8268daf21885
Irina Getmanets <IGetmanets@luxoft.com>
2016-07-08 18:50:04 +0300

    Updated ATF_DeleteSubMenu.lua: added deviceMAC and second protocol

--- test_scripts/API/ATF_DeleteSubMenu.lua
+++ test_scripts/API/ATF_DeleteSubMenu.lua
@@ -3,6 +3,10 @@ require('cardinalities')
 local events = require('events')	
 local mobile_session = require('mobile_session')
 
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+--ToDo: shall be removed when APPLINK-16610 is fixed
+config.defaultProtocolVersion = 2
+
 local storagePath = config.pathToSDL .. "storage/" ..config.application1.registerAppInterfaceParams.appID.. "_" .. config.deviceMAC.. "/"
 require('user_modules/AppTypes')
 
