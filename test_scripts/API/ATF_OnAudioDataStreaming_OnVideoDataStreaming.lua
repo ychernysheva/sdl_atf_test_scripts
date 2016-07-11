b6ccc3913b58b64be2906c6d72b6c26322bd7958
Irina Getmanets <IGetmanets@luxoft.com>
2016-07-11 12:23:07 +0300

    Updated ATF_OnAudioDataStreaming_OnVideoDataStreaming.lua: added deviceMAC and second protocol

--- test_scripts/API/ATF_OnAudioDataStreaming_OnVideoDataStreaming.lua
+++ test_scripts/API/ATF_OnAudioDataStreaming_OnVideoDataStreaming.lua
@@ -13,6 +13,10 @@ local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
 local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
 
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+--ToDo: shall be removed when APPLINK-16610 is fixed
+config.defaultProtocolVersion = 2
+
 ---------------------------------------------------------------------------------------------
 -------------------------------------------User functions------------------------------------
 ---------------------------------------------------------------------------------------------
