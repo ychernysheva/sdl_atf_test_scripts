8e784679eda1ac1e0ffb28b1e67c5f1b568e7b0f
ovikhrov <ovikhrov@luxoft.com>
2016-07-25 13:36:56 +0300

    adding config.defaultProtocolVersion = 2 due to APPLINK-16610

--- test_scripts/API/ATF_OnWayPointChange.lua
+++ test_scripts/API/ATF_OnWayPointChange.lua
@@ -10,6 +10,8 @@ local events = require('events')
 local srcPath = config.pathToSDL .. "sdl_preloaded_pt.json"
 local dstPath = config.pathToSDL .. "sdl_preloaded_pt.json.origin"
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+--ToDo: shall be removed when APPLINK-16610 is fixed
+config.defaultProtocolVersion = 2
 ---------------------------------------------------------------------------------------------
 -----------------------------Required Shared Libraries---------------------------------------
 ---------------------------------------------------------------------------------------------
