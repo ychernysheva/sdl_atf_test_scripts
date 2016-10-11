bc451d1ef4068b5abe55ae599dce0137ae5523b7
Dong Ta <ttdong@luxoft.com>
2016-10-11 14:34:32 +0700

    Move onHashChange expected result for ChangeRegisteration tests, uncomment other invalid VR.IsReady response cases

--- test_scripts/API/ATF_VR_IsReady_missed_splitRPC_VR_responds_UNSUPPORTED_RESOURCE_other_SUCCESS.lua
+++ test_scripts/API/ATF_VR_IsReady_missed_splitRPC_VR_responds_UNSUPPORTED_RESOURCE_other_SUCCESS.lua
@@ -18,7 +18,7 @@ local testCasesForPolicyTable = require('user_modules/shared_testcases/testCases
 
 
 DefaultTimeout = 3
-local iTimeout = 10000
+local iTimeout = 3000
 local commonPreconditions = require ('/user_modules/shared_testcases/commonPreconditions')
 
 
@@ -67,7 +67,9 @@ require('user_modules/AppTypes')
 -----------------------------------------------------------------------------------------------
 --Cover APPLINK-25286: [HMI_API] VR.IsReady
 function Test:initHMI_onReady_VR_IsReady(case)
-    critical(true)
+    --critical(true)
+	commonTestCases:DelayedExp(iTimeout)
+	
     local function ExpectRequest(name, mandatory, params)
 	
 	
@@ -326,7 +328,7 @@ function Test:initHMI_onReady_VR_IsReady(case)
           "PT-BR","CS-CZ","DA-DK","NO-NO"
         }
       })
-	  --:Times(0)
+
     ExpectRequest("UI.GetSupportedLanguages", true, {
         languages =
         {
@@ -335,7 +337,7 @@ function Test:initHMI_onReady_VR_IsReady(case)
           "PT-BR","CS-CZ","DA-DK","NO-NO"
         }
       })
-	  --:Times(0)
+
     ExpectRequest("VehicleInfo.GetVehicleType", true, {
         vehicleType =
         {
@@ -345,9 +347,9 @@ function Test:initHMI_onReady_VR_IsReady(case)
           trim = "SE"
         }
      })
-	 :Times(0) 
+
+	 
     ExpectRequest("VehicleInfo.GetVehicleData", true, { vin = "52-452-52-752" })
-	:Times(0)
 
     local function button_capability(name, shortPressAvailable, longPressAvailable, upDownAvailable)
       xmlReporter.AddMessage(debug.getinfo(1, "n").name, tostring(name))
@@ -396,7 +398,6 @@ function Test:initHMI_onReady_VR_IsReady(case)
           "NEGATIVE_JINGLE"
         }
       })
-	  --:Times(0)
 
     local function text_field(name, characterSet, width, rows)
       xmlReporter.AddMessage(debug.getinfo(1, "n").name, tostring(name))
@@ -515,7 +516,6 @@ function Test:initHMI_onReady_VR_IsReady(case)
           imageSupported = true
         }
       })
-	  --:Times(0)
 
     ExpectRequest("VR.IsReady", true, { available = true })
     ExpectRequest("TTS.IsReady", true, { available = true })
@@ -579,76 +579,76 @@ end
 --ToDo: Uncomment invalid cases when APPLINK-15494 is resolved (According to answers on question APPLINK-27524).
 local TestData = {
 
---caseID 1-3 are used to checking special cases
-{caseID = 1, description = "HMI_Does_Not_Repond"},
--- {caseID = 2, description = "MissedAllParamaters"},
--- {caseID = 3, description = "Invalid_Json"},
+	--caseID 1-3 are used to checking special cases
+	{caseID = 1, description = "HMI_Does_Not_Repond"},
+	{caseID = 2, description = "MissedAllParamaters"},
+	{caseID = 3, description = "Invalid_Json"},
 
-		
--- --caseID 11-14 are used to checking "collerationID" parameter
-	-- --11. IsMissed
-	-- --12. IsNonexistent
-	-- --13. IsWrongType
-	-- --14. IsNegative 	
--- -- {caseID = 11, description = "collerationID_IsMissed"},
--- -- {caseID = 12, description = "collerationID_IsNonexistent"},
--- -- {caseID = 13, description = "collerationID_IsWrongType"},
--- -- {caseID = 14, description = "collerationID_IsNegative"},
-
--- --caseID 21-27 are used to checking "method" parameter
-	-- --21. IsMissed
-	-- --22. IsNotValid
-	-- --23. IsOtherResponse
-	-- --24. IsEmpty
-	-- --25. IsWrongType
-	-- --26. IsInvalidCharacter - \n, \t, only spaces
--- {caseID = 21, description = "method_IsMissed"},
--- {caseID = 22, description = "method_IsNotValid"},
- {caseID = 23, description = "method_IsOtherResponse"},
--- {caseID = 24, description = "method_IsEmpty"},
--- {caseID = 25, description = "method_IsWrongType"},
--- {caseID = 26, description = "method_IsInvalidCharacter_Splace"},
--- {caseID = 26, description = "method_IsInvalidCharacter_Tab"},
--- {caseID = 26, description = "method_IsInvalidCharacter_NewLine"},
-
-	-- -- --caseID 31-35 are used to checking "resultCode" parameter
-		-- -- --31. IsMissed
-		-- -- --32. IsNotExist
-		-- -- --33. IsEmpty
-		-- -- --34. IsWrongType
--- {caseID = 31,  description = "resultCode_IsMissed"},
--- {caseID = 32,  description = "resultCode_IsNotExist"},
--- {caseID = 33,  description = "resultCode_IsWrongType"},
--- {caseID = 34,  description = "resultCode_INVALID_DATA"},
--- {caseID = 35,  description = "resultCode_DATA_NOT_AVAILABLE"},
--- {caseID = 36,  description = "resultCode_GENERIC_ERROR"},
-
-
-	-- --caseID 41-45 are used to checking "message" parameter
-		-- --41. IsMissed
-		-- --42. IsLowerBound
-		-- --43. IsUpperBound
-		-- --44. IsOutUpperBound
-		-- --45. IsEmpty/IsOutLowerBound
-		-- --46. IsWrongType
-		-- --47. IsInvalidCharacter - \n, \t, only spaces
--- {caseID = 41,  description = "message_IsMissed"},
--- {caseID = 42,  description = "message_IsLowerBound"},
--- {caseID = 43,  description = "message_IsUpperBound"},
--- {caseID = 44,  description = "message_IsOutUpperBound"},
--- {caseID = 45,  description = "message_IsEmpty_IsOutLowerBound"},
--- {caseID = 46,  description = "message_IsWrongType"},
--- {caseID = 47,  description = "message_IsInvalidCharacter_Tab"},
--- {caseID = 48,  description = "message_IsInvalidCharacter_OnlySpaces"},
--- {caseID = 49,  description = "message_IsInvalidCharacter_Newline"},
-
-
--- --caseID 51-55 are used to checking "available" parameter
-	-- --51. IsMissed
-	-- --52. IsWrongType
--- {caseID = 51,  description = "available_IsMissed"},
--- {caseID = 52,  description = "available_IsWrongType"},
 			
+	--caseID 11-14 are used to checking "collerationID" parameter
+		--11. IsMissed
+		--12. IsNonexistent
+		--13. IsWrongType
+		--14. IsNegative 	
+	{caseID = 11, description = "collerationID_IsMissed"},
+	{caseID = 12, description = "collerationID_IsNonexistent"},
+	{caseID = 13, description = "collerationID_IsWrongType"},
+	{caseID = 14, description = "collerationID_IsNegative"},
+
+	--caseID 21-27 are used to checking "method" parameter
+		--21. IsMissed
+		--22. IsNotValid
+		--23. IsOtherResponse
+		--24. IsEmpty
+		--25. IsWrongType
+		--26. IsInvalidCharacter - \n, \t, only spaces
+	{caseID = 21, description = "method_IsMissed"},
+	{caseID = 22, description = "method_IsNotValid"},
+	 {caseID = 23, description = "method_IsOtherResponse"},
+	{caseID = 24, description = "method_IsEmpty"},
+	{caseID = 25, description = "method_IsWrongType"},
+	{caseID = 26, description = "method_IsInvalidCharacter_Splace"},
+	{caseID = 26, description = "method_IsInvalidCharacter_Tab"},
+	{caseID = 26, description = "method_IsInvalidCharacter_NewLine"},
+
+	--caseID 31-35 are used to checking "resultCode" parameter
+		--31. IsMissed
+		--32. IsNotExist
+		--33. IsEmpty
+		--34. IsWrongType
+	{caseID = 31,  description = "resultCode_IsMissed"},
+	{caseID = 32,  description = "resultCode_IsNotExist"},
+	{caseID = 33,  description = "resultCode_IsWrongType"},
+	{caseID = 34,  description = "resultCode_INVALID_DATA"},
+	{caseID = 35,  description = "resultCode_DATA_NOT_AVAILABLE"},
+	{caseID = 36,  description = "resultCode_GENERIC_ERROR"},
+
+
+	--caseID 41-45 are used to checking "message" parameter
+		--41. IsMissed
+		--42. IsLowerBound
+		--43. IsUpperBound
+		--44. IsOutUpperBound
+		--45. IsEmpty/IsOutLowerBound
+		--46. IsWrongType
+		--47. IsInvalidCharacter - \n, \t, only spaces
+	{caseID = 41,  description = "message_IsMissed"},
+	{caseID = 42,  description = "message_IsLowerBound"},
+	{caseID = 43,  description = "message_IsUpperBound"},
+	{caseID = 44,  description = "message_IsOutUpperBound"},
+	{caseID = 45,  description = "message_IsEmpty_IsOutLowerBound"},
+	{caseID = 46,  description = "message_IsWrongType"},
+	{caseID = 47,  description = "message_IsInvalidCharacter_Tab"},
+	{caseID = 48,  description = "message_IsInvalidCharacter_OnlySpaces"},
+	{caseID = 49,  description = "message_IsInvalidCharacter_Newline"},
+
+
+	--caseID 51-55 are used to checking "available" parameter
+		--51. IsMissed
+		--52. IsWrongType
+	{caseID = 51,  description = "available_IsMissed"},
+	{caseID = 52,  description = "available_IsWrongType"},
+				
 }
 
 
@@ -702,9 +702,7 @@ local TestData = {
 		for i = 1, #resultCodes do
 			
 			Test[TestCaseName .. "_AddCommand_VR_responds_UNSUPPORTED_RESOURCE_UI_responds_".. tostring(resultCodes[i].resultCode)] = function(self)
-			
-				commonTestCases:DelayedExp(iTimeout)
-		
+				
 				--mobile side: sending AddCommand request
 				local cid = self.mobileSession:SendRPC("AddCommand",
 				{
@@ -800,8 +798,7 @@ local TestData = {
 		
 			Test[TestCaseName .. "_DeleteCommand_VR_responds_UNSUPPORTED_RESOURCE_UI_responds_".. tostring(resultCodes[i].resultCode)] = function(self)
 			
-				commonTestCases:DelayedExp(iTimeout)
-				
+								
 				--mobile side: sending DeleteCommand request
 				local cid = self.mobileSession:SendRPC("DeleteCommand", {cmdID = commandID})
 				
@@ -880,6 +877,8 @@ local TestData = {
 			
 			Test[TestCaseName .. "_PerformInteraction_VR_responds_UNSUPPORTED_RESOURCE_UI_responds_" .. tostring(resultCodes[i].resultCode)] = function(self)
 
+				commonTestCases:DelayedExp(iTimeout)
+				
 				local params = 
 					{		       
 						initialText = "StartPerformInteraction",
@@ -962,9 +961,7 @@ local TestData = {
 		--4. ChangeRegistration
 		for i = 1, #resultCodes do
 			Test[TestCaseName .. "_ChangeRegistration_VR_responds_UNSUPPORTED_RESOURCE_UI_responds_" .. resultCodes[i].resultCode] = function(self)
-			
-				commonTestCases:DelayedExp(iTimeout)
-				
+						
 				local request = {	language ="EN-US",
 									hmiDisplayLanguage ="EN-US",
 									appName ="SyncProxyTester_" .. tostring(i),
@@ -1008,9 +1005,7 @@ local TestData = {
 
 				--mobile side: expect response
 				EXPECT_RESPONSE(cid, {success = true, resultCode = "UNSUPPORTED_RESOURCE", info = resultCodes[i].info})
-				
-				--mobile side: expect OnHashChange notification
-				EXPECT_NOTIFICATION("OnHashChange")
+			
 
 			end
 		end
