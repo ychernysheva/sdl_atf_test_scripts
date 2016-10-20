66ee88079707098016e11d0f08a3757578e2b558
maria_mih <mmihaylova-banska@luxoft.com>
2016-10-20 12:48:31 +0300

    Tests for VR.IsReady created according to template

---
+++ test_scripts/API/IsReady/VR_IsReady/ATF_VR_IsReady_missing_SplitRPC_ABORTED.lua
@@ -0,0 +1,18 @@
+---------------------------------------------------------------------------------------------
+-- CRQ: APPLINK-20918 [GENIVI] VR interface: SDL behavior in case HMI does not respond to 
+--                     IsReady_request or respond with "available" = false
+--
+-- Requirement(s): APPLINK-25044:[VR Interface] HMI does NOT respond to IsReady and 
+--                               mobile app sends RPC that must be splitted
+--                 APPLINK-25286:[HMI_API] VR.IsReady
+---------------------------------------------------------------------------------------------
+
+
+TestedInterface = "VR"
+Tested_resultCode = "ABORTED" 
+Tested_wrongJSON = false
+
+
+Test = require('user_modules/IsReady_Template/ATF_Interface_IsReady_missing_SplitRPC_Template')
+
+return Test
