e64395564ac3373173fcabab4965869c54407228
istoimenova <istoimenova@luxoft.com>
2016-10-21 17:48:41 +0300

    Update for GetWayPoints RPC is done.

--- user_modules/IsReady_Template/Interfaces_RPC.lua
+++ user_modules/IsReady_Template/Interfaces_RPC.lua
@@ -1387,7 +1387,7 @@ interfaces.RPC = {
                                     name = "EndAudioPassThru",
                                     splitted = false,
                                     --No params
-                                    params = {}
+                                    params = {fakeparams = nil}
                                   },
                                   --Speak
                                   {
