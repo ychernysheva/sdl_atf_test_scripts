337305d5415770d9bf6e429455364545ba2ec33b
Anna Rotar <ARotar@luxoft.com>
2016-11-15 15:18:28 +0200

    Added script covering [Policies] SDL.ActivateApp from HMI, the device this app is running on is unconsented

---
+++ test_scripts/Polices/user_consent_of_Policies/ATF_UpdateDeviceList_isSDLAllowed_false.lua
@@ -0,0 +1,69 @@
+---------------------------------------------------------------------------------------------
+-- Description: 
+--     SDL behavior if DataConsent status was never asked for the corresponding device.
+--     1. Used preconditions:
+-- 	      Delete files and policy table from previous ignition cycle if any
+--        Close current connection 
+--     2. Performed steps:
+--        Connect device 
+
+-- Requirement summary: 
+--    [UpdateDeviceList] isSDLAllowed:false
+--
+-- Expected result:
+--    PoliciesManager must provide "isSDLAllowed:false" param of "DeviceInfo" struct ONLY when sending "UpdateDeviceList" RPC to HMI
+---------------------------------------------------------------------------------------------
+--[[ General Settings for configuration ]]
+Test = require('user_modules/connecttest_resumption')
+require('cardinalities')
+require('mobile_session')
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
+require('user_modules/AppTypes')
+
+--[[ Preconditions ]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+function Test:CloseConnection()
+	self.mobileConnection:Close()
+	commonTestCases:DelayedExp(3000)		
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+commonFunctions:userPrint(34, "Test is intended to check that for unconsented device is sent isSDLAllowed:false")
+
+function Test:UpdateDeviceList_isSDLAllowed_false()
+	commonTestCases:DelayedExp(2000)
+	self:connectMobile()
+	EXPECT_HMICALL("BasicCommunication.UpdateDeviceList",
+					{
+						deviceList = {
+							{
+								id = config.deviceMAC,
+								isSDLAllowed = false,
+								name = "127.0.0.1",
+								transportType = "WIFI"
+							}
+						}
+			        })
+	:Do(function(_,data)
+		if  isSDLAllowed ~= false then
+			commonFunctions:userPrint(31, "Error: SDL should not be allowed for a new unconsented device")
+		else
+			self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+	    end
+	end)
+	:Times(AtLeast(1)) 
+end
+
+--[[ Postconditions ]]
+commonFunctions:SDLForceStop()
+
+  
