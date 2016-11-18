a9fb0f083a1062a2d0afa8de73a750899b03797a
istoimenova <istoimenova@luxoft.com>
2016-11-18 16:49:39 +0200

    Call of common function is updated

--- test_scripts/Polices/Policy_Table_Update/007_ATF_P_TC_OnStatusUpdate_UPDATE_NEEDED_new_PTU_request.lua
+++ test_scripts/Polices/Policy_Table_Update/007_ATF_P_TC_OnStatusUpdate_UPDATE_NEEDED_new_PTU_request.lua
@@ -1,4 +1,8 @@
 ---------------------------------------------------------------------------------------------
+-- Requirements summary:
+-- [PolicyTableUpdate] OnStatusUpdate(UPDATE_NEEDED) on new PTU request
+-- [HMI API] OnStatusUpdate
+--
 -- Description:
 -- SDL should request PTU in case new application is registered and is not listed in PT
 -- and device is consented.
@@ -7,10 +11,6 @@
 -- Connect mobile phone over WiFi.
 -- 2. Performed steps
 -- Register new application
---
--- Requirements summary:
--- [PolicyTableUpdate] OnStatusUpdate(UPDATE_NEEDED) on new PTU request
--- [HMI API] OnStatusUpdate
 --
 -- Expected result:
 -- PTU is requested. PTS is created.
@@ -25,8 +25,8 @@ config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd40
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonPreconditions = require ('user_modules/shared_testcases/commonPreconditions')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
 local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
+local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
 
 --[[ General Precondition before ATF start ]]
 -- commonFunctions:SDLForceStop()
@@ -66,22 +66,26 @@ function Test:TestStep_OnStatusUpdate_UPDATE_NEEDED_new_PTU_request()
 
   EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application1.appName } })
   :Do(function(_,data)
+      local hmi_app1_id = self.applications[config.application1.registerAppInterfaceParams.appName]
       EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
 
-      testCasesForPolicyTable:create_PTS(true,
-        {config.application1.registerAppInterfaceParams.appID},
-        {config.deviceMAC} )
+      testCasesForPolicyTableSnapshot:create_PTS(true, {
+          config.application1.registerAppInterfaceParams.appID,
+          config.application2.registerAppInterfaceParams.appID,
+        },
+        {config.deviceMAC},
+        {hmi_app1_id})
 
-      local timeout_after_x_seconds = testCasesForPolicyTable:get_data_from_PTS("module_config.timeout_after_x_seconds")
-      local seconds_between_retry = {}
-      for i = 1, #testCasesForPolicyTable.seconds_between_retries do
-        seconds_between_retry[i] = testCasesForPolicyTable.seconds_between_retries[i].value
+      local timeout_after_x_seconds = testCasesForPolicyTableSnapshot:get_data_from_PTS("module_config.timeout_after_x_seconds")
+      local seconds_between_retries = {}
+      for i = 1, #testCasesForPolicyTableSnapshot.pts_seconds_between_retries do
+        seconds_between_retries[i] = testCasesForPolicyTableSnapshot.pts_seconds_between_retries[i].value
       end
       EXPECT_HMICALL("BasicCommunication.PolicyUpdate",
         {
           file = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate",
           timeout = timeout_after_x_seconds,
-          retry = seconds_between_retry
+          retry = seconds_between_retries
         })
       :Do(function(_,data)
           self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
