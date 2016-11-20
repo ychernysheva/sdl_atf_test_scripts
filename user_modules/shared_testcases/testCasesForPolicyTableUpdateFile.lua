df264ce61b8ca9333194a5f0bedbbac8591c85ae
Georgi <istoimenova@luxoft.com>
2016-11-20 23:12:37 +0200

    New file testCasesForPolicyTableUpdateFile.lua is added

---
+++ user_modules/shared_testcases/testCasesForPolicyTableUpdateFile.lua
@@ -0,0 +1,9 @@
+testCasesForPolicyTableUpdateFile = {}
+local mobile_session = require('mobile_session')
+
+function testCasesForPolicyTableUpdateFile:create_PTU_policyfie(is_valid, app_IDs, device_IDs)
+	print(" \27[31m Test step create_PTU_policyfie is not implemented! \27[0m")							
+	return false
+end
+
+return testCasesForPolicyTableUpdateFile
