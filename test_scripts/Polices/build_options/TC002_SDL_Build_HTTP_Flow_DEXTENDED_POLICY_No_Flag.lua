090760d64ed0fa08f1acaf02d8915ab5e7e28cec
maria_mih <mmihaylova-banska@luxoft.com>
2016-11-21 14:56:30 +0200

    Test updated according to comments

--- test_scripts/Polices/build_options/TC002_SDL_Build_HTTP_Flow_DEXTENDED_POLICY_No_Flag.lua
+++ test_scripts/Polices/build_options/TC002_SDL_Build_HTTP_Flow_DEXTENDED_POLICY_No_Flag.lua
@@ -1,58 +1,50 @@
 ---------------------------------------------------------------------------------------------
--- Requirements summary: 
---     [PolicyTableUpdate] Support of "http" flow of Policy Table Update
+-- Requirements summary:
+-- [PolicyTableUpdate] Support of "http" flow of Policy Table Update
 --
--- Description: 
--- SDL should be successfully built "-DEXTENDED_POLICY:HTTP" flag is empty
+-- Description:
+-- SDL should be successfully built with empty flag "EXTENDED_POLICY:"
 -- 1. Performed steps
--- Build SDL 
+-- Build SDL
 --
 -- Expected result:
 -- SDL is successfully built
--- The flag -DEXTENDED_POLICY:HTTP has no value
--- PTU passes successfully 
-
+-- The flag EXTENDED_POLICY: has no value set
+-- PTU passes successfully
 ---------------------------------------------------------------------------------------------
 
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-local commonSteps = require('user_modules/shared_testcases/commonSteps')
---local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
-local commonPreconditions = require ('user_modules/shared_testcases/commonPreconditions')
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
 local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
-  
+
 --[[ General Precondition before ATF start ]]
--- commonFunctions:SDLForceStop()
-testCasesForBuildingSDLPolicyFlag:Update_PolicyFlag("ENABLE_HTTP_POLICY")
-testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("ENABLE_HTTP_POLICY")
-commonSteps:DeleteLogsFileAndPolicyTable()
-commonPreconditions:Connecttest_without_ExitBySDLDisconnect_WithoutOpenConnectionRegisterApp("connecttest_RAI.lua")
- 
-  --ToDo (mmihaylova-banska): Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
-  config.defaultProtocolVersion = 2
-  
+commonFunctions:SDLForceStop()
+testCasesForBuildingSDLPolicyFlag:Update_PolicyFlag("EXTENDED_POLICY","")
+testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("EXTENDED_POLICY","")
+
+--TODO(mmihaylova-banska): Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+config.defaultProtocolVersion = 2
+
 --[[ General Settings for configuration ]]
-  Test = require('connecttest')
-  require('cardinalities')
-  require('user_modules/AppTypes')
-  --local mobile_session = require('mobile_session')
- 
+Test = require('connecttest')
+require('cardinalities')
+require('user_modules/AppTypes')
+
 --[[ Test ]]
- --ToDo: Function should be debugged! Runtime error!
- --commonFunctions:newTestCasesGroup("TC_PTU_HTTP_FLOW") 
-  function Test:HTTP_Flow_AfterBuild ()
-   testCasesForPolicyTable:flow_PTU_SUCCEESS_EXTERNAL_HTTP (self)
-  end
+commonFunctions:newTestCasesGroup("Test")
+--TODO(mmihaylova-banska): Function still not implemented
+function Test:TestStep_HTTP_Flow_AfterBuild ()
+  testCasesForPolicyTable:flow_PTU_SUCCEESS_EXTERNAL_HTTP (self)
+end
 
-  
 --[[ Postconditions ]]
- --ToDo: Function should be debugged! Runtime error!
--- commonFunctions:newTestCasesGroup("Postconditions")
--- function Test:Postcondition_SDLForceStop()
---   commonFunctions:SDLForceStop(self)
--- end
+commonFunctions:newTestCasesGroup("Postconditions")
+function Test:Postcondition_SDLForceStop()
+  commonFunctions:SDLForceStop(self)
+end
 
-return Test	
+return Test
