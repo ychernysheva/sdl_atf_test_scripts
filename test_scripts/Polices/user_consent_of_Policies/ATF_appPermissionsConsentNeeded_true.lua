d1bf4813438ac498ccdc7c33bde223748ee1ca7e
Anna Rotar (GitHub) <ARotar@luxoft.com>
2016-11-24 10:24:04 +0200

    Update ATF_appPermissionsConsentNeeded_true.lua

--- test_scripts/Polices/user_consent_of_Policies/ATF_appPermissionsConsentNeeded_true.lua
+++ test_scripts/Polices/user_consent_of_Policies/ATF_appPermissionsConsentNeeded_true.lua
@@ -19,6 +19,11 @@
 -- 	PoliciesManager must respond with the list of <groupName>s that have the field "user_consent_prompt" in corresponding <functional grouping> and 
 --	are assigned to the specified application (section "<appID>" -> "groups") 
 ---------------------------------------------------------------------------------------------
+--[[ General Settings for configuration ]]
+Test = require('connecttest')
+require('cardinalities')
+local mobile_session = require('mobile_session')
+
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
@@ -27,11 +32,6 @@ local commonFunctions = require ('user_modules/shared_testcases/commonFunctions'
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
 require('user_modules/AppTypes')
-
---[[ General Settings for configuration ]]
-Test = require('connecttest')
-require('cardinalities')
-local mobile_session = require('mobile_session')
 
 --[[ Preconditions ]]
 commonSteps:DeleteLogsFileAndPolicyTable()
