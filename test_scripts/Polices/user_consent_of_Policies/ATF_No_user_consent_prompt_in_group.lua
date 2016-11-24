c489cf99623630ad9c6c03c2988123f688faf9ae
Anna Rotar (GitHub) <ARotar@luxoft.com>
2016-11-24 10:26:54 +0200

    Update ATF_No_user_consent_prompt_in_group.lua

--- test_scripts/Polices/user_consent_of_Policies/ATF_No_user_consent_prompt_in_group.lua
+++ test_scripts/Polices/user_consent_of_Policies/ATF_No_user_consent_prompt_in_group.lua
@@ -19,21 +19,21 @@
 -- Expected result:
 --		PoliciesManager must apply such <functional grouping> without asking User`s consent for it
 ---------------------------------------------------------------------------------------------
+--[[ General Settings for configuration ]]
+Test = require('user_modules/connecttest_resumption')
+require('cardinalities')
+local mobile_session = require('mobile_session')
+
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-Test = require('user_modules/connecttest_resumption')
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
 local Preconditions = require('user_modules/shared_testcases/commonPreconditions')
 local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
 require('user_modules/AppTypes')
-
---[[ General Settings for configuration ]]
-require('cardinalities')
-local mobile_session = require('mobile_session')
 
 --[[ Preconditions ]]
 commonSteps:DeleteLogsFileAndPolicyTable()
