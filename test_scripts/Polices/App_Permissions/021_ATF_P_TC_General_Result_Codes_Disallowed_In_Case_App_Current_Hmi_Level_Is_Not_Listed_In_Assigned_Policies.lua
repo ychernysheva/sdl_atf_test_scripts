ea9505f2d1e081a06334067b5a561260b7f6c867
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2016-11-24 15:27:15 +0200

    Removed verifyResponse() custom function

--- test_scripts/Polices/App_Permissions/021_ATF_P_TC_General_Result_Codes_Disallowed_In_Case_App_Current_Hmi_Level_Is_Not_Listed_In_Assigned_Policies.lua
+++ test_scripts/Polices/App_Permissions/021_ATF_P_TC_General_Result_Codes_Disallowed_In_Case_App_Current_Hmi_Level_Is_Not_Listed_In_Assigned_Policies.lua
@@ -15,7 +15,7 @@
 -- in which for a specified PRC there're defined HMILevels: HMILevel_2, HMILevel_3
 -- Steps:
 -- 1. Send RPC App -> SDL
--- 2. Verify status of response
+-- 2. Verify status of respo
 --
 -- Expected result:
 -- SDL -> App: RPC (DISALLOWED, success: "false")
@@ -23,20 +23,6 @@
 
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
-
---[[ Local Functions ]]
-local function verifyResponse(test, corId)
-  test.mobileSession:ExpectResponse(corId)
-  :ValidIf(function(_, d)
-      local exp = {success = false, resultCode = "DISALLOWED"}
-      if (d.payload.success == exp.success) and (d.payload.resultCode == exp.resultCode) then
-        return true
-      else
-        return false, "Expected: {success = '" .. tostring(exp.success) .. "', resultCode = '" .. exp.resultCode
-        .. "'}, got: {success = '" .. tostring(d.payload.success) .. "', resultCode = '" .. d.payload.resultCode .. "'}"
-      end
-    end)
-end
 
 --[[ Required Shared libraries ]]
 local testCasesForPolicyAppIdManagament = require("user_modules/shared_testcases/testCasesForPolicyAppIdManagament")
@@ -63,32 +49,32 @@ end
 commonFunctions:newTestCasesGroup("Test")
 function Test:SendRPC_AddCommand()
   local corId = self.mobileSession:SendRPC("AddCommand", {cmdID = 1,menuParams = {menuName = "Options"}, vrCommands = {"Options"} })
-  verifyResponse(self, corId)
+  self.mobileSession:ExpectResponse(corId, {success = false, resultCode = "DISALLOWED"})
 end
 
 function Test:SendRPC_AddSubMenu()
   local corId = self.mobileSession:SendRPC("AddSubMenu", {menuID = 1000, position = 500, menuName ="SubMenupositive"})
-  verifyResponse(self, corId)
+  self.mobileSession:ExpectResponse(corId, {success = false, resultCode = "DISALLOWED"})
 end
 
 function Test:SendRPC_Alert()
   local corId = self.mobileSession:SendRPC("Alert", {alertText1 = "alertText1"})
-  verifyResponse(self, corId)
+  self.mobileSession:ExpectResponse(corId, {success = false, resultCode = "DISALLOWED"})
 end
 
 function Test:SendRPC_Show()
   local corId = self.mobileSession:SendRPC("Show", {mainField1 = "mainField1"})
-  verifyResponse(self, corId)
+  self.mobileSession:ExpectResponse(corId, {success = false, resultCode = "DISALLOWED"})
 end
 
 function Test:SendRPC_SystemRequest()
   local corId = self.mobileSession:SendRPC("SystemRequest", {requestType = "PROPRIETARY", fileName = "PolicyTableUpdate"})
-  verifyResponse(self, corId)
+  self.mobileSession:ExpectResponse(corId, {success = false, resultCode = "DISALLOWED"})
 end
 
 function Test:SendRPC_UnregisterAppInterface()
   local corId = self.mobileSession:SendRPC("UnregisterAppInterface",{})
-  verifyResponse(self, corId)
+  self.mobileSession:ExpectResponse(corId, {success = false, resultCode = "DISALLOWED"})
 end
 
 return Test
