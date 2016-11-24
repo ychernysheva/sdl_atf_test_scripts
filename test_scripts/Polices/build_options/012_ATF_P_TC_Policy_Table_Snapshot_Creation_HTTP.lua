d52e72885c7bbe8cc6ed2535b3d08f5a2917051c
maria_mih <mmihaylova-banska@luxoft.com>
2016-11-24 18:30:34 +0200

    Added script for requirement [PolicyTableUpdate] PTS creation rule

---
+++ test_scripts/Polices/build_options/012_ATF_P_TC_Policy_Table_Snapshot_Creation_HTTP.lua
@@ -0,0 +1,97 @@
+---------------------------------------------------------------------------------------------
+-- Requirements summary:
+-- [PolicyTableUpdate] PTS creation rule
+--
+-- Description:
+-- SDL should request PTU in case new application is registered and is not listed in PT
+-- and device is consented.
+-- 1. Used preconditions
+-- SDL is built with "-DEXTENDED_POLICY: HTTP" flag
+-- Connect mobile phone over WiFi.
+-- 2. Performed steps
+-- Register new application
+--
+-- Expected result:
+-- PTU is requested. PTS is created.
+---------------------------------------------------------------------------------------------
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local commonPreconditions = require ('user_modules/shared_testcases/commonPreconditions')
+local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+
+--[[ General Precondition before ATF start ]]
+-- commonFunctions:SDLForceStop()
+commonSteps:DeleteLogsFileAndPolicyTable()
+commonPreconditions:Connecttest_without_ExitBySDLDisconnect_WithoutOpenConnectionRegisterApp("connecttest_RAI.lua")
+
+--ToDo: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+config.defaultProtocolVersion = 2
+
+--[[ General Settings for configuration ]]
+Test = require('user_modules/connecttest_RAI')
+require('cardinalities')
+require('user_modules/AppTypes')
+local mobile_session = require('mobile_session')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup("Preconditions")
+function Test.Precondition_remove_user_connecttest()
+  os.execute( "rm -f ./user_modules/connecttest_RAI.lua" )
+end
+
+function Test:Precondition_ConnectMobile()
+  self:connectMobile()
+end
+
+function Test:Precondition_StartNewSession()
+  self.mobileSession = mobile_session.MobileSession( self, self.mobileConnection)
+  self.mobileSession:StartService(7)
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+--TODO(mmihaylova-banska): Function still not implemented
+function Test:TestStep_PTS_Creation_rule()
+  local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application1.appName } })
+  :Do(function(_,data)
+      local hmi_app1_id = data.params.application.appID
+      EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
+
+      testCasesForPolicyTableSnapshot:verify_PTS(true,
+        {config.application1.registerAppInterfaceParams.appID},
+        {config.deviceMAC},
+        {hmi_app1_id})
+
+      local timeout_after_x_seconds = testCasesForPolicyTableSnapshot:get_data_from_PTS("module_config.timeout_after_x_seconds")
+      local seconds_between_retries = {}
+      for i = 1, #testCasesForPolicyTableSnapshot.pts_seconds_between_retries do
+        seconds_between_retries[i] = testCasesForPolicyTableSnapshot.pts_seconds_between_retries[i].value
+      end
+      EXPECT_HMICALL("BasicCommunication.PolicyUpdate",
+        {
+          file = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate",
+          timeout = timeout_after_x_seconds,
+          retry = seconds_between_retries
+        })
+      :Do(function(_,data2)
+          self.hmiConnection:SendResponse(data2.id, data2.method, "SUCCESS", {})
+        end)
+    end)
+
+  self.mobileSession:ExpectResponse(correlationId, { success = true, resultCode = "SUCCESS"})
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+function Test:Postcondition_SDLForceStop()
+  commonFunctions:SDLForceStop(self)
+end
+
+return Test
