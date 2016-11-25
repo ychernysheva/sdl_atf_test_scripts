d15e578fedda3e1841586f80782ec012134cf1d8
maria_mih <mmihaylova-banska@luxoft.com>
2016-11-25 14:43:11 +0200

    Added script for requirement In case HMI sends GetURLs and at least one app is registered SDL must return only default url and url related to registered app

---
+++ test_scripts/Polices/build_options/017_ATF_P_TC_HMI_sends_GetURLs_one_app_registered_HTTP.lua
@@ -0,0 +1,86 @@
+-- UNREADY:
+-- Please note still not implemented functions:
+-- testCasesForPolicyTable:flow_PTU_SUCCEESS_HTTP() and testCasesForPolicyTable:trigger_PTU_user_press_button_HMI
+-- testCasesForPolicyTable:flow_PTU_SUCCEESS_HTTP() is still not included in testCasesForPolicyTable
+
+---------------------------------------------------------------------------------------------
+-- Requirements summary:
+-- In case HMI sends GetURLs and at least one app is registered SDL must return only default url and url related to registered app
+-- [HMI API] GetURLs request/response
+--
+-- Description:
+-- SDL should request PTU in case user requests PTU
+-- 1. Used preconditions
+-- SDL is built with "EXTENDED_POLICY: HTTP" flag
+-- Application is registered.
+-- No PTU is requested.
+-- 2. Performed steps
+-- User press button on HMI to request PTU.
+-- HMI->SDL: SDL.GetURLs(service=0x07)
+--
+-- Expected result:
+-- PTU is requested. PTS is created.
+-- SDL.GetURLs({urls[] = registered_App1, default})
+---------------------------------------------------------------------------------------------
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+
+--[[ General Precondition before ATF start ]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--TODO(mmihaylova-banska): Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+config.defaultProtocolVersion = 2
+
+--[[ General Settings for configuration ]]
+Test = require('connecttest')
+require('cardinalities')
+require('user_modules/AppTypes')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup("Preconditions")
+--TODO(mmihaylova-banska): Function still not implemented
+function Test.Precondition_PTU_SUCCESS ()
+  testCasesForPolicyTable:flow_PTU_SUCCEESS_HTTP()
+end
+
+--TODO(mmihaylova-banska): Function still not implemented
+function Test.Precondition_Request_PTU ()
+  testCasesForPolicyTable:trigger_PTU_user_press_button_HMI()
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+
+function Test:TestStep_PTU_GetURLs()
+  local endpoints = {}
+  local is_app_esxist = false
+
+  for i = 1, #testCasesForPolicyTableSnapshot.pts_endpoints do
+    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "0x07") then
+      endpoints[#endpoints + 1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
+    end
+
+    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "app1") then
+      endpoints[#endpoints + 1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
+      is_app_esxist = true
+    end
+  end
+
+  local RequestId = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+
+  EXPECT_HMIRESPONSE(RequestId,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints} } )
+  :Do(function(_,_)
+      if(is_app_esxist == false) then
+        self:FailTestCase("endpoints for application doesn't exist!")
+      end
+    end)
+end
+
+return Test
