a4239fc7672ab2bbc92868ab04d7dc731efb4e79
Georgi <istoimenova@luxoft.com>
2016-11-26 10:29:41 +0200

    Script is done, but not be merged. Waiting clarification of usage common functions.

--- test_scripts/Polices/appID_Management/162_ATF_P_TC_HMI_Status_Appid_Gets_Null_In_Case_Of_PTU.lua
+++ test_scripts/Polices/appID_Management/162_ATF_P_TC_HMI_Status_Appid_Gets_Null_In_Case_Of_PTU.lua
@@ -10,8 +10,8 @@
 -- Preconditions:
 -- 1. appID="123_xyz" is not registered to SDL yet
 -- 2. appID="123_xyz" has NOT null policies
--- 3. appID="123_xyz" in FULL and AUDIBLE
--- 4. PTU is triggered (by registering new application "456_abc")
+-- 3. appID="123_xyz" in FULL
+
 -- Steps:
 -- 1. After PTU appID="123_xyz" gets "null" policies
 -- 2. Verify status of OnHMIStatus notification
@@ -29,19 +29,22 @@ local mobileSession = require("mobile_session")
 local testCasesForPolicyAppIdManagament = require("user_modules/shared_testcases/testCasesForPolicyAppIdManagament")
 local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
 local commonSteps = require("user_modules/shared_testcases/commonSteps")
-local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
+local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
 
 --[[ General Precondition before ATF start ]]
-testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("EXTERNAL_PROPRIETARY")
-commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFileAndPolicyTable()
 
 --[[ General Settings for configuration ]]
 Test = require("connecttest")
 require("user_modules/AppTypes")
 
+local HMIAppID
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
+function Test:Pecondition_trigger_getting_device_consent()
+  testCasesForPolicyTable:trigger_getting_device_consent(self, config.application1.registerAppInterfaceParams.appName, config.deviceMAC)
+end
+
 function Test:UpdatePolicy()
   testCasesForPolicyAppIdManagament:updatePolicyTable(self, "files/jsons/Policies/appID_Management/ptu_013_1.json")
 end
@@ -54,32 +57,28 @@ end
 function Test:RegisterNewApp()
   config.application2.registerAppInterfaceParams.appName = "Media Application"
   config.application2.registerAppInterfaceParams.appID = "123_xyz"
-  testCasesForPolicyAppIdManagament:registerApp(self, self.mobileSession2, config.application2)
-  self.mobileSession2:ExpectNotification("OnHMIStatus", { hmiLevel = "NONE" })
+  local correlationId = self.mobileSession2:SendRPC("RegisterAppInterface", config.application2.registerAppInterfaceParams)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
+  :Do(function(_,data)
+      HMIAppID = data.params.application.appID
+      self.applications[config.application2.registerAppInterfaceParams.appName] = data.params.application.appID
+    end)
+
+  self.mobileSession2:ExpectNotification("OnHMIStatus", {hmiLevel = "NONE", systemContext = "MAIN", audioStreamingState = "NOT_AUDIBLE" })
+  self.mobileSession2:ExpectResponse(correlationId, { success = true })
+end
+
+function Test:ActivateApp()
+  self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = HMIAppID})
+  self.mobileSession2:ExpectNotification("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN"})
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
-function Test:ActivateApp()
-  testCasesForPolicyAppIdManagament:activateApp(self, self.mobileSession2)
-end
-
-function Test:StartNewSession()
-  self.mobileSession3 = mobileSession.MobileSession(self, self.mobileConnection)
-  self.mobileSession3:StartService(7)
-end
-
-function Test:RegisterNewApp()
-  config.application3.registerAppInterfaceParams.appName = "New Application"
-  config.application3.registerAppInterfaceParams.appID = "456_abc"
-  testCasesForPolicyAppIdManagament:registerApp(self, self.mobileSession3, config.application3)
-end
-
-function Test:UpdatePolicy()
+function Test:TestStep_UpdatePolicy()
   EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
   testCasesForPolicyAppIdManagament:updatePolicyTable(self, "files/jsons/Policies/appID_Management/ptu_013_2.json")
-  self.mobileSession2:ExpectNotification("OnHMIStatus",
-    {hmiLevel="NONE", systemContext="MAIN", audioStreamingState="NOT_AUDIBLE"})
+  self.mobileSession2:ExpectNotification("OnHMIStatus", { hmiLevel ="NONE", systemContext = "MAIN", audioStreamingState = "NOT_AUDIBLE" })
 end
 
 return Test
