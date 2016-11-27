578df10bd5b15570d3108c2976134bd6d21bc183
Georgi <istoimenova@luxoft.com>
2016-11-27 23:13:56 +0200

    Script is ready, but won't be merged. Waiting for clarification of using common functions.

--- test_scripts/Polices/Policy_Table_Update/023_ATF_P_TC_PoliciesManager_changes_status_to_UP_TO_DATE.lua
+++ test_scripts/Polices/Policy_Table_Update/023_ATF_P_TC_PoliciesManager_changes_status_to_UP_TO_DATE.lua
@@ -33,149 +33,35 @@ config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd40
 --[[ Required Shared libraries ]]
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
-local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
+local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
 local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
-local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
-local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
-local testCasesForPolicyTableUpdateFile = require('user_modules/shared_testcases/testCasesForPolicyTableUpdateFile')
 
 --[[ General Precondition before ATF start ]]
--- commonFunctions:SDLForceStop()
-testCasesForBuildingSDLPolicyFlag:Update_PolicyFlag("EXTENDED_POLICY", "EXTERNAL_PROPRIETARY")
-testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("EXTENDED_POLICY","EXTERNAL_PROPRIETARY")
 commonSteps:DeleteLogsFileAndPolicyTable()
-commonPreconditions:Connecttest_without_ExitBySDLDisconnect_WithoutOpenConnectionRegisterApp("connecttest_RAI.lua")
 
 --ToDo: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
 config.defaultProtocolVersion = 2
 
 --[[ General Settings for configuration ]]
-Test = require('user_modules/connecttest_RAI')
+Test = require('connecttest')
 require('cardinalities')
 require('user_modules/AppTypes')
-local mobile_session = require('mobile_session')
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
-function Test:Precondition_remove_user_connecttest()
-  os.execute( "rm -f ./user_modules/connecttest_RAI.lua" )
-end
-
-function Test:Precondition_ConnectMobile()
-  self:connectMobile()
-end
-
-function Test:Precondition_StartNewSession()
-  self.mobileSession = mobile_session.MobileSession( self, self.mobileConnection)
-  self.mobileSession:StartService(7)
+function Test:Precondition_trigger_getting_device_consent()
+  testCasesForPolicyTable:trigger_getting_device_consent(self, config.application1.registerAppInterfaceParams.appName, config.deviceMAC)
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 
-function Test:TestStep_Sending_PTS_to_mobile_application()
-  local is_test_fail = false
-  local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+function Test:TestStep_PoliciesManager_changes_UP_TO_DATE()
+  testCasesForPolicyTable:flow_SUCCEESS_EXTERNAL_PROPRIETARY(self)
 
-  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application1.appName } })
-  :Do(function(_,data)
-      local hmi_app_id = data.params.application.appID
-      EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
-
-      testCasesForPolicyTableSnapshot:create_PTS(true,
-        {config.application1.registerAppInterfaceParams.appID},
-        {config.deviceMAC},
-        {hmi_app1_id})
-
-      local timeout_pts = testCasesForPolicyTableSnapshot:get_data_from_PTS("module_config.timeout_after_x_seconds")
-      local seconds_between_retries = {}
-
-      for i = 1, #testCasesForPolicyTableSnapshot.pts_seconds_between_retries do
-        seconds_between_retries[i] = testCasesForPolicyTableSnapshot.pts_seconds_between_retries[i].value
-      end
-
-      --TODO(istoimenova): function for reading INI file should be implemented
-      --local SystemFilesPath = commonSteps:get_data_form_SDL_ini("SystemFilesPath")
-      local SystemFilesPath = "/tmp/fs/mp/images/ivsu_cache/"
-      local file_pts = SystemFilesPath.."sdl_snapshot.json"
-
-      EXPECT_HMICALL("BasicCommunication.PolicyUpdate", { file = file_pts, timeout = timeout_pts, retry = seconds_between_retries})
-      :Do(function(_,data)
-          self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
-        end)
-
-      local endpoints = {}
-      local is_app_esxist = false
-
-      for i = 1, #testCasesForPolicyTableSnapshot.pts_endpoints do
-        if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "0x07") then
-          endpoints[#endpoints + 1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
-        end
-
-        if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "app1") then
-          endpoints[#endpoints + 1] = {
-            url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value,
-            appID = testCasesForPolicyTableSnapshot.pts_endpoints[i].appID}
-          is_app_esxist = true
-        end
-      end
-
-      if(is_app_esxist == false) then
-        commonFunctions:printError("endpoints for application doesn't exist!")
-        is_test_fail = true
-        endpoints[#endpoints + 1] = { url = endpoints[#endpoints].value, appID = hmi_app_id}
-      end
-
-      local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-
-      EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints} } )
-      :Do(function(_,data)
-          local app_urls = {}
-          if(data.result ~= nil) then
-            for i = 1, #data.result.urls do
-              if(data.result.urls[i].appID == hmi_app_id) then
-                app_urls = data.result.urls[i]
-              end
-            end
-          else
-            is_test_fail = true
-            commonFunctions:printError("Endpoints of GetUrls response are empty.")
-          end
-          --Left for debugging
-          --self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",{ requestType = "PROPRIETARY", fileName = "PolicyTableUpdate"})
-
-          self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",{
-              fileName = "PolicyTableUpdate",
-              requestType = "PROPRIETARY",
-              url = endpoints[1].url,
-              appID = "default" })
-
-          EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "PROPRIETARY", fileType = "JSON", url = endpoints[1].url,appID = config.application1.registerAppInterfaceParams.appID })
-          :Do(function(_,data)
-              EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATING"})
-              local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "PROPRIETARY", fileName = "PolicyTableUpdate"}, "files/ptu.json")
-
-              EXPECT_HMICALL("BasicCommunication.SystemRequest",{
-                  requestType = "PROPRIETARY",
-                  fileName = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate",
-                  appID = hmi_app_id})
-              :Do(function(_,data)
-                  self.hmiConnection:SendResponse(systemRequestId,"BasicCommunication.SystemRequest", "SUCCESS", {})
-                  testCasesForPolicyTableUpdateFile:create_PTU_policyfie()
-                  self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
-                    { policyfile = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate"})
-                  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"})
-                end)
-
-            end)--EXPECT_NOTIFICATION("OnSystemRequest", {
-
-        end)
-
-    end)
-  self.mobileSession:ExpectResponse(correlationId, { success = true, resultCode = "SUCCESS"})
-  if(is_test_fail == true) then
-    self:FailTestCase("Test is FAILED. See prints.")
-  end
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {}):Times(0)
+  EXPECT_HMICALL("BasicCommunication.PolicyUpdate",{}):Times(0)
+  commonTestCases:DelayedExp(60*1000)
 end
 
 --[[ Postconditions ]]
