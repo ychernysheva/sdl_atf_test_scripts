05b5a06cc1870ab6d54ddc46cb654b706585ae02
Anna Rotar (GitHub) <ARotar@luxoft.com>
2016-11-28 15:04:29 +0200

    Corrected script (flag=false )to pass luacheck.

--- test_scripts/Polices/user_consent_of_Policies/ATF_isPermissionsConsentNeeded_false.lua
+++ test_scripts/Polices/user_consent_of_Policies/ATF_isPermissionsConsentNeeded_false.lua
@@ -1,14 +1,14 @@
 ---------------------------------------------------------------------------------------------
--- Requirement summary: 
+-- Requirement summary:
 --    [Policies] SDL.ActivateApp from HMI and 'isPermissionsConsentNeeded' parameter in the response
 --
--- Description: 
+-- Description:
 --     SDL receives request for app activation from HMI and LocalPT contains permission that don't require User`s consent
 --     1. Used preconditions:
---		Delete SDL log file and policy table
--- 			
+--    Delete SDL log file and policy table
+--
 --     2. Performed steps
--- 		Activate with default permissions
+--    Activate with default permissions
 --
 -- Expected result:
 --      On receiving SDL.ActivateApp PoliciesManager must respond with "isPermissionsConsentNeeded:false" to HMI, consent for custom permissions is not appeared
@@ -33,22 +33,23 @@ commonSteps:DeleteLogsFileAndPolicyTable()
 commonFunctions:newTestCasesGroup("Test")
 
 function Test:IsPermissionsConsentNeeded_false_on_app_activation()
-	local RequestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications["Test Application"]})
-	EXPECT_HMIRESPONSE(RequestId,{ result = { code = 0, method = "SDL.ActivateApp", isPermissionsConsentNeeded = false, isSDLAllowed = true, priority ="NONE" }})
-	:Do(function(_,data)
-		if data.result.isPermissionsConsentNeeded ~= false then
-			commonFunctions:userPrint(31, "Wrong SDL behavior: isPermissionsConsentNeeded should be false for app with default permissions")
-		end
-	end)
-	EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN", audioStreamingState = "AUDIBLE"})	
+  local RequestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications["Test Application"]})
+  EXPECT_HMIRESPONSE(RequestId,{ result = { code = 0, method = "SDL.ActivateApp", isPermissionsConsentNeeded = false, isSDLAllowed = true, priority ="NONE" }})
+  :Do(function(_,data)
+  if data.result.isPermissionsConsentNeeded ~= false then
+    commonFunctions:userPrint(31, "Wrong SDL behavior: isPermissionsConsentNeeded should be false for app with default permissions")
+  end
+  end)
+  EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN", audioStreamingState = "AUDIBLE"})
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-	
-function Test:Postcondition_SDLForceStop()
-	commonFunctions:SDLForceStop()
-end										
-function Test:Postcondition_RestorePreloadedPT()										
-	testCasesForPolicyTable:Restore_preloaded_pt()
+
+function Test.Postcondition_SDLForceStop()
+  commonFunctions:SDLForceStop()
+end
+
+function Test.Postcondition_RestorePreloadedPT()
+  testCasesForPolicyTable:Restore_preloaded_pt()
 end
