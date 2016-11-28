18036f4e4e13bb8e38858c7ecb7bd2756614528d
maria_mih <mmihaylova-banska@luxoft.com>
2016-11-28 11:25:09 +0200

    Added script for requirement [PolicyTableUpdate] Got PTU from mobile application

---
+++ test_scripts/Polices/build_options/022_ATF_P_TC_Received_PTU_From_Mobile_Application_HTTP.lua
@@ -0,0 +1,100 @@
+-- UNREADY: 
+--function Test:TestStep_Sending_PTS_to_mobile_application()
+--should be applicable for HTTP flag as well
+
+---------------------------------------------------------------------------------------------
+-- Requirements summary:
+-- [PolicyTableUpdate] Got PTU from mobile application
+-- [HMI API] SystemRequest request/response
+--
+-- Description:
+-- Upon receiving the response (before timeout) from the application via SystemRequest, 
+-- SDL must stop the timer of PTU "timeout" and Base64-decode the payload, 
+-- which is the Policy Table Update.
+-- 1. Used preconditions
+-- SDL is built with "-DEXTENDED_POLICY: HTTP" flag
+-- Application is registered.
+-- PTU is requested.
+-- SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
+-- SDL->HMI:SDL.PolicyUpdate(file, timeout, retry[])
+-- HMI -> SDL: SDL.GetURLs (<service>)
+-- HMI->SDL: BasicCommunication.OnSystemRequest ('url', requestType:HTTP, appID="default")
+-- SDL->app: OnSystemRequest ('url', requestType:HTTP, fileType="JSON", appID)
+-- 2. Performed steps
+-- app->SDL:SystemRequest(requestType=HTTP)
+--
+-- Expected result:
+-- SDL->HMI: SystemRequest(requestType=HTTP, fileName, appID)
+---------------------------------------------------------------------------------------------
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
+local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+
+--[[ General Precondition before ATF start ]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--ToDo: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+config.defaultProtocolVersion = 2
+
+--[[ General Settings for configuration ]]
+Test = require('connecttest')
+require('cardinalities')
+require('user_modules/AppTypes')
+
+--[[ Test ]]
+--TODO(mmihaylova-banska): Function should be implmented for HTTP flag 
+commonFunctions:newTestCasesGroup("Test")
+function Test:TestStep_Sending_PTS_to_mobile_application()
+  local is_test_fail = false
+  local endpoints = {}
+  local hmi_app_id = self.applications[config.application1.registerAppInterfaceParams.appName]
+  print("hmi_app_id = " ..hmi_app_id)
+
+  for i = 1, #testCasesForPolicyTableSnapshot.pts_endpoints do
+    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "0x07") then
+      endpoints[#endpoints + 1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
+    end
+
+    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "app1") then
+      endpoints[#endpoints + 1] = {
+      url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value,
+      appID = testCasesForPolicyTableSnapshot.pts_endpoints[i].appID}
+    end
+  end
+
+  local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints} } )
+  :Do(function(_,_)
+    self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",{ fileName = "PolicyTableUpdate", requestType = "HTTP", url = endpoints[1].url})
+    EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "HTTP", fileType = "JSON", url = endpoints[1].url,appID = config.application1.registerAppInterfaceParams.appID })
+    :Do(function(_,_)
+      local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "HTTP", fileName = "PolicyTableUpdate"}, "files/ptu.json")
+      EXPECT_HMICALL("BasicCommunication.SystemRequest",{
+        requestType = "HTTP",
+        fileName = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate",
+        appID = hmi_app_id})
+      :Do(function(_,_data1)
+        self.hmiConnection:SendResponse(_data1.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
+        --self.hmiConnection:SendNotification ("SDL.OnReceivedPolicyUpdate", { policyfile = SystemFilesPath.."/PolicyTableUpdate"})
+      end)
+      EXPECT_RESPONSE(CorIdSystemRequest, { success = true, resultCode = "SUCCESS"})
+    end)
+  end)
+  if(is_test_fail == true) then
+    self:FailTestCase("Test is FAILED. See prints.")
+  end
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+function Test:Postcondition_Force_Stop_SDL()
+  commonFunctions:SDLForceStop(self)
+end
+
+return Test
