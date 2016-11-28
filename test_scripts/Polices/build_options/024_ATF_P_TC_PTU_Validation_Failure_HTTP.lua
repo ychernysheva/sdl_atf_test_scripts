5ecdf2a72a0fb6ae5335c15360b6627836d81e1a
maria_mih <mmihaylova-banska@luxoft.com>
2016-11-28 18:02:10 +0200

    Added script for requirement [PolicyTableUpdate] PTU validation failure

---
+++ test_scripts/Polices/build_options/024_ATF_P_TC_PTU_Validation_Failure_HTTP.lua
@@ -0,0 +1,130 @@
+-- UNREADY:
+--function Test:TestStep_PTU_validation_failure()
+--should be applicable for HTTP flag as well
+--please also note that parameter appID should be used only with requestType = LAUNCH_APP
+--according to APPLINK-18219
+
+---------------------------------------------------------------------------------------------
+-- Requirements summary:
+-- [PolicyTableUpdate] PTU validation failure
+-- [HMI API] OnStatusUpdate
+-- [HMI API] OnReceivedPolicyUpdate notification
+--
+-- Description:
+-- In case PTU validation fails, SDL must log the error locally and discard the policy table update
+-- with No notification of Cloud about invalid data and
+-- notify HMI with OnPolicyUpdate(UPDATE_NEEDED) .
+-- 1. Used preconditions
+-- SDL is built with "-DEXTENDED_POLICY: HTTP" flag
+-- Application is registered.
+-- PTU is requested.
+-- SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
+-- SDL->HMI:SDL.PolicyUpdate(file, timeout, retry[])
+-- HMI -> SDL: SDL.GetURLs (<service>)
+-- HMI->SDL: BasicCommunication.OnSystemRequest ('url', requestType:HTTP)
+-- SDL->app: OnSystemRequest ('url', requestType:HTTP, fileType="JSON")
+-- app->SDL: SystemRequest(requestType=HTTP)
+-- SDL->HMI: SystemRequest(requestType=HTTP, fileName)
+-- HMI->SDL: SystemRequest(SUCCESS)
+-- 2. Performed steps
+-- HMI->SDL: OnReceivedPolicyUpdate(policy_file): policy_file with missing mandatory seconds_between_retries.
+--
+-- Expected result:
+-- SDL->HMI: OnStatusUpdate(UPDATE_NEEDED)
+-- SDL removes 'policyfile' from the directory
+---------------------------------------------------------------------------------------------
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
+local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+
+--local testCasesForPolicyTableUpdateFile = require('user_modules/shared_testcases/testCasesForPolicyTableUpdateFile')
+
+--[[ General Precondition before ATF start ]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--TODO(mmihaylova-banska): Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+config.defaultProtocolVersion = 2
+
+--[[ General Settings for configuration ]]
+Test = require('connecttest')
+require('cardinalities')
+require('user_modules/AppTypes')
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+
+function Test:TestStep_PTU_validation_failure()
+  local is_test_fail = false
+  local endpoints = {}
+  local hmi_app_id = self.applications[config.application1.registerAppInterfaceParams.appName]
+  print("hmi_app_id = " ..hmi_app_id)
+
+  for i = 1, #testCasesForPolicyTableSnapshot.pts_endpoints do
+    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "0x07") then
+      endpoints[#endpoints + 1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
+    end
+
+    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "app1") then
+      endpoints[#endpoints + 1] = {
+        url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value,
+        appID = testCasesForPolicyTableSnapshot.pts_endpoints[i].appID}
+    end
+  end
+
+  testCasesForPolicyTableSnapshot:extract_pts({hmi_app_id})
+  local timeout_after_x_seconds = testCasesForPolicyTableSnapshot:get_data_from_PTS("module_config.timeout_after_x_seconds")
+  local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints} } )
+  :Do(function(_,_)
+
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
+        { fileType = "JSON", timeout = timeout_after_x_seconds, requestType = "HTTP", url = endpoints[1].url})
+      EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "HTTP",
+          fileType = "JSON",
+          url = endpoints[1].url,
+          timeout = timeout_after_x_seconds
+        })
+      :Do(function(_,_)
+          local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "HTTP", fileName = "PolicyTableUpdate"},
+          "files/jsons/Policies/PTU_ValidationRules/invalid_PTU_missing_seconds_between_retries.json")
+
+          EXPECT_HMICALL("BasicCommunication.SystemRequest",{
+              requestType = "HTTP",
+              fileName = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate"
+              --,appID = hmi_app_id
+            })
+          :Do(function(_,_data1)
+              self.hmiConnection:SendResponse(_data1.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
+              self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
+                { policyfile = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate"})
+
+              EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
+              :Do(function(_,_)
+                  if( commonSteps:file_exists("/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate") == true) then
+                    is_test_fail = true
+                    commonFunctions:printError("File /tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate exists!")
+                  end
+                end)
+            end)
+
+          EXPECT_RESPONSE(CorIdSystemRequest, { success = true, resultCode = "SUCCESS"})
+        end)
+    end)
+
+  if(is_test_fail == true) then
+    self:FailTestCase("Test is FAILED. See prints.")
+  end
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+function Test:Postcondition_Force_Stop_SDL()
+  commonFunctions:SDLForceStop(self)
+end
+
+return Test
