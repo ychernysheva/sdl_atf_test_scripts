2a63287157de12444b494d84601f3626e424d66a
istoimenova <istoimenova@luxoft.com>
2016-11-28 13:38:29 +0200

    Done

--- test_scripts/Polices/Related_HMI_API/200_ATF_OnAllowSDLFunctionality_allowed_false_with_device.lua
+++ test_scripts/Polices/Related_HMI_API/200_ATF_OnAllowSDLFunctionality_allowed_false_with_device.lua
@@ -60,32 +60,27 @@ function Test:TestStep_Allowed_false_with_device()
   end
 end
 
-function Test:TestStep_Check_Device_IsnotAllowed()
-  testCasesForPolicyTableSnapshot:extract_pts({self.applications[config.application1.registerAppInterfaceParams.appName]})
-  device_consent = testCasesForPolicyTableSnapshot:get_data_from_PTS("device_data."..config.deviceMAC..".user_consent_records.device.consent_groups.DataConsent-2")
+function Test:TestStep_Check_Device_IsnotAllowed() 
   local RequestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications[config.application1.registerAppInterfaceParams.appName]})
   EXPECT_HMIRESPONSE(RequestId)
   :Do(function(_,data)
       if (data.result.isSDLAllowed == true) then
         self:FailTestCase("Device is still consented after user disallowed access to it.")
       end
+  end)
+end
 
-      local RequestId1 = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = {"DataConsent"}})
-      --hmi side: expect SDL.GetUserFriendlyMessage message response
-      EXPECT_HMIRESPONSE( RequestId1, {result = {code = 0, method = "SDL.GetUserFriendlyMessage"}})
-      :Do(function(_,_)
-          self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality",
-            {allowed = false, source = "GUI", device = {id = config.deviceMAC, name = ServerAddress, isSDLAllowed = false}})
-        end)
-
-      EXPECT_HMICALL("BasicCommunication.PolicyUpdate",{})
-      :Do(function(_,_)
-          self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
-          if(device_consent ~= false) then
-            self:FailTestCase("Device is still consented after user didn't consent device.")
-          end
-        end)
-    end)
+function Test:TestStep_Check_Device_IsNotAllowed_snapshot()
+  self.hmiConnection:SendNotification("SDL.OnPolicyUpdate", {} )
+  EXPECT_HMICALL("BasicCommunication.PolicyUpdate",{})
+  :Do(function(_,_)
+    testCasesForPolicyTableSnapshot:extract_pts({self.applications[config.application1.registerAppInterfaceParams.appName]})
+    device_consent = testCasesForPolicyTableSnapshot:get_data_from_PTS("device_data."..config.deviceMAC..".user_consent_records.device.consent_groups.DataConsent-2")
+    self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+    if(device_consent ~= false) then
+      self:FailTestCase("Device is still consented after user didn't consent device.")
+    end
+  end)
 end
 
 --[[ Postconditions ]]
