b9d6f219535b2a5ba27c1d1fd93428d3b2667d10
Anna Rotar <ARotar@luxoft.com>
2016-11-29 16:19:35 +0200

    Added script covering [Policies] <device identifier> section creation. Connection of the new device with SDL-enabled applications

---
+++ test_scripts/Polices/user_consent_of_Policies/ATF_Device_identifier_creation_upon_connect_with_apps.lua
@@ -0,0 +1,103 @@
+---------------------------------------------------------------------------------------------
+-- Requirement summary:
+--     [Policies] <device identifier> section creation. Connection of the new device with SDL-enabled applications
+--
+-- Description:
+--    New device is connected over WiFi WITH SDL-enabled applications
+-- 1. Used preconditions:
+--    SDL and HMI are running
+--    Connect device not from LPT
+--   
+-- 2. Performed steps:
+--    Register app
+--
+-- Expected result:
+--    SDL must add new <device identifier> section in "device_data" section 
+---------------------------------------------------------------------------------------------
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local testCasesForPolicyTableSnapshot = require('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
+local mobile_session = require('mobile_session')
+
+--[[ Local variables ]]
+--local ServerAddress = commonFunctions:read_parameter_from_smart_device_link_ini("ServerAddress")
+testCasesForPolicyTable:Precondition_updatePolicy_By_overwriting_preloaded_pt("files/jsons/Policy/Related_HMI_API/OnAppPermissionConsent.json")
+
+--[[ General Precondition before ATF start ]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--[[ General Settings for configuration ]]
+Test = require('user_modules/connecttest_resumption')
+require('cardinalities')
+require('user_modules/AppTypes')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup("Preconditions")
+
+function Test:Precondition_Connect_device()
+  commonTestCases:DelayedExp(2000)
+  self:connectMobile()
+  EXPECT_HMICALL("BasicCommunication.UpdateDeviceList",
+  {
+    deviceList = {
+      {
+        id = config.deviceMAC,
+        name = "127.0.0.1",
+        transportType = "WIFI",
+        isSDLAllowed = false
+      }
+    }
+  }
+  ):Do(function(_,data)
+  self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+  end)
+  :Times(AtLeast(1))
+end
+
+function Test:Precondition_Register_app()
+  commonTestCases:DelayedExp(3000)
+  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession:StartService(7)
+  :Do(function()
+  local RequestIDRai1 = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
+  :Do(function(_,data)
+  self.HMIAppID = data.params.application.appID
+  end)
+  self.mobileSession:ExpectResponse(RequestIDRai1, { success = true, resultCode = "SUCCESS" })
+  self.mobileSession:ExpectNotification("OnHMIStatus", {hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
+  end)
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+
+function Test:TestStep_check_LocalPT_for_updates()
+  local is_test_fail = false
+    local device_data = testCasesForPolicyTableSnapshot:get_data_from_PTS("device_data")
+
+    print("device_data" ..tostring(device_data))
+
+    if(device_data ~= "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0") then
+      commonFunctions:printError("Error: device_data is missing")
+      is_test_fail = true
+    end
+
+    if(is_test_fail == true) then
+      self:FailTestCase("Test is FAILED. See prints.")
+    end 
+  
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+
+function Test.Postcondition_SDLForceStop()
+  commonFunctions:SDLForceStop()
+end
