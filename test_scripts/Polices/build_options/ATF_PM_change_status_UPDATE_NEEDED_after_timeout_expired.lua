55bb61e904150f586065f1e8aaf5404538d51265
Nikolaev <anikolaev@anikolaev-lws>
2016-11-30 17:31:45 +0200

    Added script for coverage req. APPLINK-18810

---
+++ test_scripts/Polices/build_options/ATF_PM_change_status_UPDATE_NEEDED_after_timeout_expired.lua
@@ -0,0 +1,152 @@
+-- UNREADY:
+-- Hello Iliana, in this script I don't know how to create - from "timeout_after_x_seconds" field of LocalPT and recieve:
+-- EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"}). Help please. Thanks a lot.
+
+---------------------------------------------------------------------------------------------
+-- Requirements summary:
+-- [PolicyTableUpdate][GENIVI] PoliciesManager changes status to “UPDATE_NEEDED”
+--
+-- Description:
+-- SDL should request PTU in case new application is registered and is not listed in PT
+-- 1. Used preconditions
+-- SDL is built with "-DEXTENDED_POLICY: PROPRIETARY" flag
+-- Connect mobile phone over WiFi.
+-- 2. Performed steps
+-- Register new application
+-- SDL-> <app ID> ->OnSystemRequest(params, url, )
+-- Timeout expires
+--
+-- Expected result:
+--SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
+---------------------------------------------------------------------------------------------
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+config.defaultProtocolVersion = 2
+
+--[[ Required Shared libraries ]]
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+-- local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+-- local testCasesForPolicyTableSnapshot = require ('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+
+--[[ General Precondition before ATF start ]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--[[ General Settings for configuration ]]
+Test = require('connecttest')
+require('cardinalities')
+require('user_modules/AppTypes')
+
+--[[ Local functions ]]
+
+local registerAppInterfaceParams =
+{
+  syncMsgVersion =
+  {
+    majorVersion = 3,
+    minorVersion = 0
+  },
+  appName = "Media Application",
+  isMediaApplication = true,
+  languageDesired = 'EN-US',
+  hmiDisplayLanguageDesired = 'EN-US',
+  appHMIType = {"NAVIGATION"},
+  appID = "MyTestApp",
+  deviceInfo =
+  {
+    os = "Android",
+    carrier = "Megafon",
+    firmwareRev = "Name: Linux, Version: 3.4.0-perf",
+    osVersion = "4.4.2",
+    maxNumberRFCOMMPorts = 1
+  }
+}
+
+local function policyUpdate(self)
+  local pathToSnaphot = nil
+  EXPECT_HMICALL ("BasicCommunication.PolicyUpdate")
+  :Do(function(_,data)
+      pathToSnaphot = data.params.file
+      self.hmiConnection:SendResponse(data.id, "BasicCommunication.PolicyUpdate", "SUCCESS", {})
+    end)
+  local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {url = "http://policies.telematics.ford.com/api/policies"}}})
+  :Do(function(_,_)
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
+        {
+          requestType = "PROPRIETARY",
+          url = "http://policies.telematics.ford.com/api/policies",
+          appID = self.applications ["Test Application"],
+          fileName = "sdl_snapshot.json"
+        },
+        pathToSnaphot
+      )
+    end)
+  EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "PROPRIETARY" })
+  :Do(function(_,_)
+      local CorIdSystemRequest = self.mobileSession:SendRPC ("SystemRequest",
+        {
+          requestType = "PROPRIETARY",
+          fileName = "sdl_snapshot.json"
+        },
+        pathToSnaphot
+      )
+      EXPECT_HMICALL("BasicCommunication.SystemRequest")
+
+      :Do(function(_,data)
+          self.hmiConnection:SendResponse(data.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
+        end)
+      EXPECT_RESPONSE(CorIdSystemRequest, {success = true, resultCode = "SUCCESS"})
+      :Do(function(_,_)
+          self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
+            {
+              policyfile = "/tmp/fs/mp/images/ivsu_cache/ptu.json"
+            })
+        end)
+      :Do(function(_,_)
+          EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"})
+        end)
+    end)
+end
+
+function Test:Preconditions_Update_Policy()
+  policyUpdate(self, "/tmp/fs/mp/images/ivsu_cache/ptu.json")
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup ("Test")
+function Test:TestStep_RegisterApplication_In_NewSession()
+  local corId = self.mobileSession2:SendRPC("RegisterAppInterface", registerAppInterfaceParams)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = "Media Application" }})
+  self.mobileSession2:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
+  self.mobileSession2:ExpectNotification("OnPermissionsChange")
+end
+
+function Test:TestStep_Start_New_PolicyUpdate_For_SecondApplication()
+  local pathToSnaphot = nil
+  EXPECT_HMICALL ("BasicCommunication.PolicyUpdate")
+  :Do(function(_,data)
+      pathToSnaphot = data.params.file
+      self.hmiConnection:SendResponse(data.id, "BasicCommunication.PolicyUpdate", "SUCCESS", {})
+    end)
+  local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {url = "http://policies.telematics.ford.com/api/policies"}}})
+  :Do(function(_,_)
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
+        {
+          requestType = "PROPRIETARY",
+          url = "http://policies.telematics.ford.com/api/policies",
+          appID = self.applications ["MyTestApp"],
+          fileName = "sdl_snapshot.json"
+        },
+        pathToSnaphot
+      )
+    end)
+end
+
+--- Run Timeout 60sec.
+-- :Do(function(_,_)
+-- EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
+-- end)
+
