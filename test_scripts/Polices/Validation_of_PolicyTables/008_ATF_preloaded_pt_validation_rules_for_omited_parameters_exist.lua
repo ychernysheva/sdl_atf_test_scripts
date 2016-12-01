a7a95cef5e94f5cdd0e10b797004c65c00e2b390
Alexandr Deriabin <aderiabin@luxoft.com>
2016-12-01 12:56:43 +0200

    Created test script for check validation rules for omitted parameters in PreloadedPT

--- test_scripts/Polices/Validation_of_PolicyTables/008_ATF_preloaded_pt_validation_rules_for_omited_parameters_exist.lua
+++ test_scripts/Polices/Validation_of_PolicyTables/008_ATF_preloaded_pt_validation_rules_for_omited_parameters_exist.lua
@@ -1,4 +1,7 @@
 ---------------------------------------------------------------------------------------------
+-- Requirement summary:
+-- [Policies]: PreloadedPolicyTable: Validation rules for omited parameters - exists
+--
 -- Description:
 -- Behavior of SDL during start SDL in case when PreloadedPT has omited parameters
 -- 1. Used preconditions:
@@ -7,20 +10,14 @@
 -- 2. Performed steps:
 -- Create PreloadedPolicyTable file with couple omited parameters
 -- Start SDL
-
--- Requirement summary:
--- [Policies]: PreloadedPolicyTable: Validation rules for omited parameters - exists
 --
 -- Expected result:
 -- PolicyManager shut SDL down
 ---------------------------------------------------------------------------------------------
---[[ General Settings for configuration ]]
+--[[ General configuration parameters ]]
 Test = require('connecttest')
 local config = require('config')
-
---[[ General configuration parameters ]]
 config.defaultProtocolVersion = 2
-local preloaded_pt_file_name = "sdl_preloaded_pt.json"
 
 --[[ Required Shared libraries ]]
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
@@ -28,19 +25,22 @@ local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local json = require("modules/json")
 local SDL = require('modules/SDL')
 
---[[ Preconditions ]]
-function Test.backup_preloaded_pt()
-  os.execute("cp " .. config.pathToSDL .. preloaded_pt_file_name .. ' ' .. config.pathToSDL .. "backup_" .. preloaded_pt_file_name)
+--[[ Local Variables ]]
+local PRELOADED_PT_FILE_NAME = "sdl_preloaded_pt.json"
+local testParameters = {vehicle_model = "Fiesta", vehicle_make = "Ford", vehicle_year = 2015}
+
+--[[ Local Functions ]]
+function Test.backupPreloadedPT()
+  os.execute("cp " .. config.pathToSDL .. PRELOADED_PT_FILE_NAME .. ' ' .. config.pathToSDL .. "backup_" .. PRELOADED_PT_FILE_NAME)
 end
 
-function Test.restore_preloaded_pt()
-  os.execute("mv " .. config.pathToSDL .. "backup_" .. preloaded_pt_file_name .. " " .. config.pathToSDL .. preloaded_pt_file_name)
+function Test.restorePreloadedPT()
+  os.execute("mv " .. config.pathToSDL .. "backup_" .. PRELOADED_PT_FILE_NAME .. " " .. config.pathToSDL .. PRELOADED_PT_FILE_NAME)
 end
 
-function Test.corrupt_preloaded_pt()
-  local omited_parameters = {vehicle_model = "Fiesta", vehicle_make = "Ford", vehicle_year = 2015}
+function Test.corruptPreloadedPT(omitedParameters)
 
-  local pathToFile = config.pathToSDL .. preloaded_pt_file_name
+  local pathToFile = config.pathToSDL .. PRELOADED_PT_FILE_NAME
 
   local file = io.open(pathToFile, "r")
   local json_data = file:read("*a")
@@ -48,7 +48,7 @@ function Test.corrupt_preloaded_pt()
 
   local data = json.decode(json_data)
   if data then
-    for key, value in pairs(omited_parameters) do
+    for key, value in pairs(omitedParameters) do
       data.policy_table.module_config[key] = value
     end
   end
@@ -59,7 +59,7 @@ function Test.corrupt_preloaded_pt()
   file:close()
 end
 
-function Test.check_sdl_shutdown()
+function Test.checkSdlShutdown()
   EXPECT_HMINOTIFICATION("BasicCommunication.OnSDLClose",{}):Times(1)
   local status = SDL:CheckStatusSDL()
   if status == SDL.RUNNING then
@@ -69,29 +69,33 @@ function Test.check_sdl_shutdown()
   return true
 end
 
+--[[ Preconditions ]]
 function Test:Precondition_stop_sdl()
   StopSDL(self)
 end
 
 function Test:Precondition()
   commonSteps:DeletePolicyTable()
-  self.backup_preloaded_pt()
-  self.corrupt_preloaded_pt()
+  self.backupPreloadedPT()
+  self.corruptPreloadedPT(testParameters)
 end
 
 --[[ Test ]]
-
 function Test:Test_start_sdl()
   StartSDL(config.pathToSDL, true, self)
 end
 
 function Test:Test()
-  self.check_sdl_shutdown()
+  self.checkSdlShutdown()
 end
 
 --[[ Postconditions ]]
 function Test:Postconditions()
-  self.restore_preloaded_pt()
+  self.restorePreloadedPT()
 end
 
-commonFunctions:SDLForceStop()
+function Test:Postcondition_StopSDL()
+  StopSDL(self)
+end
+
+return Test
