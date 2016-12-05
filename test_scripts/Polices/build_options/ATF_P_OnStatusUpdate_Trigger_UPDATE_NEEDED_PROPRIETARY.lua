90eadab2795f66f9414d74f7429101eb5c86726b
Nikolaev <anikolaev@anikolaev-lws>
2016-12-05 13:07:23 +0200

    Added scripts for coverage req. [PolicyTableUpdate] OnStatusUpdate trigger

---
+++ test_scripts/Polices/build_options/ATF_P_OnStatusUpdate_Trigger_UPDATE_NEEDED_PROPRIETARY.lua
@@ -0,0 +1,147 @@
+-- UNREADY:
+--Test:TestStep_PoliciesManager_changes_UPDATE_NEDDED
+--should be applicable for PROPRIETARY flag as well
+--function testCasesForPolicyTable.flow_PTU_SUCCEESS_PROPRIETARY should be added to testCasesForPolicyTable
+
+---------------------------------------------------------------------------------------------
+-- Requirements summary:
+-- [PolicyTableUpdate] OnStatusUpdate trigger
+-- c) UPDATE_NEDDED VC
+-- [HMI API] OnStatusUpdate
+-- [HMI API] OnReceivedPolicyUpdate notification
+--
+-- Description:
+--PoliciesManager must change the status to “UPDATE_NEDDED” and notify HMI with
+--OnStatusUpdate("UPDATE_NEDDED") right after successful validation of received PTU .
+-- 1. Used preconditions
+-- SDL is built with "-DEXTENDED_POLICY: PROPRIETARY" flag
+-- Application is registered.
+-- PTU is requested.
+-- SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
+-- SDL->HMI:SDL.PolicyUpdate(file, timeout, retry[])
+-- HMI -> SDL: SDL.GetURLs (<service>)
+-- HMI->SDL: BasicCommunication.OnSystemRequest ('url', requestType:PROPRIETARY)
+-- SDL->app: OnSystemRequest ('url', requestType:PROPRIETARY, fileType="JSON")
+-- app->SDL: SystemRequest(requestType=PROPRIETARY)
+-- SDL->HMI: SystemRequest(requestType=PROPRIETARY, fileName)
+-- HMI->SDL: SystemRequest(SUCCESS)
+-- 2. Performed steps
+-- HMI->SDL: OnReceivedPolicyUpdate(policy_file) according to data dictionary
+--
+-- Expected result:
+-- SDL->HMI: OnStatusUpdate(UPDATE_NEDDED)
+
+-------------------------------------------------------------------------------------------------
+--[[ General configuration parameters ]]
+config.defaultProtocolVersion = 2
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local mobile_session = require('mobile_session')
+
+--[[ Local Variables ]]
+--NewTestSuiteNumber = 0
+
+--[[ Local Functions ]]
+local registerAppInterfaceParams =
+{
+  syncMsgVersion =
+  {
+    majorVersion = 3,
+    minorVersion = 0
+  },
+  appName = "Media Application",
+  isMediaApplication = true,
+  languageDesired = 'EN-US',
+  hmiDisplayLanguageDesired = 'EN-US',
+  appHMIType = {"NAVIGATION"},
+  appID = "MyTestApp",
+  deviceInfo =
+  {
+    os = "Android",
+    carrier = "Megafon",
+    firmwareRev = "Name: Linux, Version: 3.4.0-perf",
+    osVersion = "4.4.2",
+    maxNumberRFCOMMPorts = 1
+  }
+}
+
+local function policyUpdate(self)
+  local pathToSnaphot = nil
+  EXPECT_HMICALL ("BasicCommunication.PolicyUpdate")
+  :Do(function(_,data)
+      pathToSnaphot = data.params.file
+      self.hmiConnection:SendResponse(data.id, "BasicCommunication.PolicyUpdate", "SUCCESS", {})
+    end)
+  local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {url = "http://policies.telematics.ford.com/api/policies"}}})
+  :Do(function(_,_)
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
+        {
+          requestType = "PROPRIETARY",
+          url = "http://policies.telematics.ford.com/api/policies",
+          appID = self.applications ["Test Application"],
+          fileName = "sdl_snapshot.json"
+        },
+        pathToSnaphot
+      )
+    end)
+  EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "PROPRIETARY" })
+  :Do(function(_,_)
+      local CorIdSystemRequest = self.mobileSession:SendRPC ("SystemRequest",
+        {
+          requestType = "PROPRIETARY",
+          fileName = "sdl_snapshot.json"
+        },
+        pathToSnaphot
+      )
+      EXPECT_HMICALL("BasicCommunication.SystemRequest")
+      :Do(function(_,data)
+          self.hmiConnection:SendResponse(data.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
+        end)
+      EXPECT_RESPONSE(CorIdSystemRequest, {success = true, resultCode = "SUCCESS"})
+      :Do(function(_,_)
+          self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
+            {
+              policyfile = "/tmp/fs/mp/images/ivsu_cache/ptu.json"
+            })
+        end)
+      :Do(function(_,_)
+          EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"})
+        end)
+    end)
+end
+
+--[[ General Precondition before ATF start]]
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--[[ General Settings for configuration ]]
+Test = require('connecttest')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup ("Preconditions")
+function Test:Preconditions_Update_Policy()
+  policyUpdate(self, "/tmp/fs/mp/images/ivsu_cache/ptu.json")
+end
+function Test:Precondition_OpenNewSession()
+  self.mobileSession2 = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession2:StartService(7)
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup ("Test")
+function Test:TestStep_RegisterApplication_In_NewSession_Expect_UPDATE_NEEDED()
+  local corId = self.mobileSession2:SendRPC("RegisterAppInterface", registerAppInterfaceParams)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = "Media Application" }})
+  self.mobileSession2:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
+  self.mobileSession2:ExpectNotification("OnPermissionsChange")
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+Test["StopSDL"] = function()
+  StopSDL()
+end
