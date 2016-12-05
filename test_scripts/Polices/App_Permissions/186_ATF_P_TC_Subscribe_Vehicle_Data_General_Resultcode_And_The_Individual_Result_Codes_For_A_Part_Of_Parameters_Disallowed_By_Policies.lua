0a93225f8ad00d0839697c7f310b19dc94b4a979
Georgi <istoimenova@luxoft.com>
2016-12-05 10:59:47 +0200

    Script is ready to be merged/

--- test_scripts/Polices/App_Permissions/186_ATF_P_TC_Subscribe_Vehicle_Data_General_Resultcode_And_The_Individual_Result_Codes_For_A_Part_Of_Parameters_Disallowed_By_Policies.lua
+++ test_scripts/Polices/App_Permissions/186_ATF_P_TC_Subscribe_Vehicle_Data_General_Resultcode_And_The_Individual_Result_Codes_For_A_Part_Of_Parameters_Disallowed_By_Policies.lua
@@ -1,6 +1,8 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
 -- [SubscribeVehicleData] General ResultCode and the individual result codes for a part of parameters disallowed by Policies
+-- [Mobile API] [GENIVI] SubscribeVehicleData request/response
+-- [HMI API] [GENIVI] VehicleInfo.SubscribeVehicleData request/response
 --
 -- Description:
 -- SDL must:
@@ -34,14 +36,40 @@
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-local testCasesForPolicyAppIdManagament = require("user_modules/shared_testcases/testCasesForPolicyAppIdManagament")
 local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
 local commonSteps = require("user_modules/shared_testcases/commonSteps")
-local testCasesForBuildingSDLPolicyFlag = require('user_modules/shared_testcases/testCasesForBuildingSDLPolicyFlag')
+local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+
+--[[ Local functions ]]
+local function UpdatePolicy()
+  local PermissionForSubscribeVehicleData =
+  [[
+  "SubscribeVehicleData": {
+    "hmi_levels": ["BACKGROUND",
+    "FULL",
+    "LIMITED",
+    "NONE"],
+    "parameters": ["rpm", "fuelLevel", "fuelLevel_State", "instantFuelConsumption", "speed"]
+  }
+  ]].. ", \n"
+  local PermissionForUnsubscribeVehicleData =
+  [[
+  "UnsubscribeVehicleData": {
+    "hmi_levels": ["BACKGROUND",
+    "FULL",
+    "LIMITED",
+    "NONE"],
+    "parameters": ["rpm", "fuelLevel", "fuelLevel_State", "instantFuelConsumption", "speed"]
+  }
+  ]].. ", \n"
+
+  local PermissionLinesForBase4 = PermissionForSubscribeVehicleData..PermissionForUnsubscribeVehicleData
+  local PTName = testCasesForPolicyTable:createPolicyTableFile_temp(PermissionLinesForBase4, nil, nil, {"SubscribeVehicleData","UnsubscribeVehicleData"})
+  testCasesForPolicyTable:Precondition_updatePolicy_By_overwriting_preloaded_pt(PTName)
+end
+UpdatePolicy()
 
 --[[ General Precondition before ATF start ]]
-testCasesForBuildingSDLPolicyFlag:CheckPolicyFlagAfterBuild("EXTERNAL_PROPRIETARY")
-commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFileAndPolicyTable()
 
 --[[ General Settings for configuration ]]
@@ -50,14 +78,13 @@ require("user_modules/AppTypes")
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
-
-function Test:UpdatePolicy()
-  testCasesForPolicyAppIdManagament:updatePolicyTable(self, "files/jsons/Policies/App_Permissions/ptu_017.json")
+function Test:Precondition_trigger_getting_device_consent()
+  testCasesForPolicyTable:trigger_getting_device_consent(self, config.application1.registerAppInterfaceParams.appName, config.deviceMAC)
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
-function Test:Test()
+function Test:TestStep_SubscribeVehicleData()
   local corId = self.mobileSession:SendRPC("SubscribeVehicleData",
     {
       speed = true,
@@ -86,11 +113,13 @@ function Test:Test()
       resultCode = "SUCCESS",
       speed = { dataType = "VEHICLEDATA_SPEED", resultCode = "SUCCESS" },
       rpm = { dataType = "VEHICLEDATA_RPM", resultCode = "SUCCESS" },
-      fuelLevel = { dataType = "VEHICLEDATA_FUELLEVEL", resultCode = "SUCCESS" },
-      fuelLevel_State = { dataType = "VEHICLEDATA_FUELLEVEL_STATE", resultCode = "DISALLOWED" },
-      instantFuelConsumption = { dataType = "VEHICLEDATA_FUELCONSUMPTION", resultCode = "DISALLOWED" },
-      info = "'fuelLevel_State', 'instantFuelConsumption' disallowed by policies."
+      fuelLevel = { dataType = "VEHICLEDATA_FUELLEVEL", resultCode = "SUCCESS" }
     })
 end
 
-return Test
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+testCasesForPolicyTable:Restore_preloaded_pt()
+function Test.Postcondition_StopSDL()
+  StopSDL()
+end
