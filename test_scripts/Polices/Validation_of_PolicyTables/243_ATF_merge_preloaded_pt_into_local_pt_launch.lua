0515b59941ea10bcb038affd01074741da16cab5
Georgi <istoimenova@luxoft.com>
2016-12-08 19:35:24 +0200

    Script is ready to be merged.

--- test_scripts/Polices/Validation_of_PolicyTables/243_ATF_merge_preloaded_pt_into_local_pt_launch.lua
+++ test_scripts/Polices/Validation_of_PolicyTables/243_ATF_merge_preloaded_pt_into_local_pt_launch.lua
@@ -16,20 +16,38 @@
 ---------------------------------------------------------------------------------------------
 
 --[[ General configuration parameters ]]
-Test = require('connecttest')
-local config = require('config')
-require('user_modules/AppTypes')
-config.defaultProtocolVersion = 2
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require ('user_modules/shared_testcases/commonSteps')
+local commonPreconditions = require ('user_modules/shared_testcases/commonPreconditions')
+local testCasesForPolicyTable = require ('user_modules/shared_testcases/testCasesForPolicyTable')
 local json = require("modules/json")
+
+config.defaultProtocolVersion = 2
+
 
 --[[ Local Variables ]]
 local TESTED_DATA = {
-  preloaded_date = {"2015-11-11","2015-12-12"},
+  {
+    key = "en-us",
+    label = "GPS and speed",
+    tts = "An app can access vehicle GPS and speed."
+  },
+  {
+    key = "de-de",
+    label = "GPS und Geschwindigkeit",
+    tts = "Eine App hat Zugriff auf die GPS-Daten und die Geschwindigkeit des Fahrzeugs."
+  },
+  {
+    key = "fr-fr",
+    label = "GPS et vitesse",
+    tts = "Une application peut accéder au GPS et à la vitesse du véhicule."
+  },
+  preloaded_date = {"1987-12-01","2012-05-02"}
 }
+
 local PRELOADED_PT_FILE_NAME = "sdl_preloaded_pt.json"
 
 local TestData = {
@@ -73,6 +91,72 @@ local TestData = {
 }
 
 --[[ Local Functions ]]
+local function updatePreloadedPt(updaters)
+  local pathToFile = config.pathToSDL .. PRELOADED_PT_FILE_NAME
+  local file = io.open(pathToFile, "r")
+  local json_data = file:read("*a")
+  file:close()
+
+  local data = json.decode(json_data)
+  if data then
+    for _, updateFunc in pairs(updaters) do
+      updateFunc(data)
+    end
+  end
+
+  local dataToWrite = json.encode(data)
+  file = io.open(pathToFile, "w")
+  file:write(dataToWrite)
+  file:close()
+end
+
+local function prepareInitialPreloadedPT()
+  local initialUpdaters = {
+    function(data)
+      for key, value in pairs(data.policy_table.functional_groupings) do
+        if not value.rpcs then
+          data.policy_table.functional_groupings[key] = nil
+        end
+      end
+    end,
+    function(data)
+      data.policy_table.module_config.preloaded_date = TESTED_DATA.preloaded_date[1]
+    end,
+    function(data)
+      for key,_ in pairs(data.policy_table.consumer_friendly_messages.messages.Location.languages) do
+        if key ~= TESTED_DATA[1].key then
+          data.policy_table.consumer_friendly_messages.messages.Location.languages[key] = nil
+        end
+      end
+    end
+  }
+  updatePreloadedPt(initialUpdaters)
+end
+
+local function prepareNewPreloadedPT()
+  local newUpdaters = {
+    function(data)
+      for key, value in pairs(data.policy_table.functional_groupings) do
+        if not value.rpcs then
+          data.policy_table.functional_groupings[key] = nil
+        end
+      end
+    end,
+    function(data)
+      data.policy_table.module_config.preloaded_date = TESTED_DATA.preloaded_date[2]
+    end,
+    function(data)
+      local obj
+      for i = 1, 3 do
+        obj = {label = TESTED_DATA[i].label, tts = TESTED_DATA[i].tts}
+        data.policy_table.consumer_friendly_messages.messages.Location.languages[TESTED_DATA[i].key] = obj
+      end
+
+    end
+  }
+  updatePreloadedPt(newUpdaters)
+end
+
 local function constructPathToDatabase()
   if commonSteps:file_exists(config.pathToSDL .. "storage/policy.sqlite") then
     return config.pathToSDL .. "storage/policy.sqlite"
@@ -134,6 +218,18 @@ local function isValuesCorrect(actualValues, expectedValues)
   return true
 end
 
+--[[ General Precondition before ATF start ]]
+config.defaultProtocolVersion = 2
+testCasesForPolicyTable.Delete_Policy_table_snapshot()
+commonSteps:DeleteLogsFileAndPolicyTable()
+commonPreconditions:BackupFile(PRELOADED_PT_FILE_NAME)
+prepareInitialPreloadedPT()
+
+--[[ General configuration parameters ]]
+Test = require('connecttest')
+local config = require('config')
+require('user_modules/AppTypes')
+
 function Test.checkLocalPT(checkTable)
   local expectedLocalPtValues
   local queryString
@@ -172,87 +268,28 @@ function Test.checkLocalPT(checkTable)
   return isTestPass
 end
 
-function Test.backupPreloadedPT(backupPrefix)
-  os.execute(table.concat({"cp ", config.pathToSDL, PRELOADED_PT_FILE_NAME, " ", config.pathToSDL, backupPrefix, PRELOADED_PT_FILE_NAME}))
-end
-
-function Test.restorePreloadedPT(backupPrefix)
-  os.execute(table.concat({"mv ", config.pathToSDL, backupPrefix, PRELOADED_PT_FILE_NAME, " ", config.pathToSDL, PRELOADED_PT_FILE_NAME}))
-end
-
-function Test.updatePreloadedPt(updaters)
-  local pathToFile = config.pathToSDL .. PRELOADED_PT_FILE_NAME
-  local file = io.open(pathToFile, "r")
-  local json_data = file:read("*a")
-  file:close()
-
-  local data = json.decode(json_data)
-  if data then
-    for _, updateFunc in pairs(updaters) do
-      updateFunc(data)
-    end
-  end
-
-  local dataToWrite = json.encode(data)
-  file = io.open(pathToFile, "w")
-  file:write(dataToWrite)
-  file:close()
-end
-
-function Test:prepareInitialPreloadedPT()
-  local initialUpdaters = {
-    function(data)
-      for key, value in pairs(data.policy_table.functional_groupings) do
-        if not value.rpcs then
-          data.policy_table.functional_groupings[key] = nil
-        end
-      end
-    end,
-    function(data)
-      data.policy_table.module_config.preloaded_date = TESTED_DATA.preloaded_date[1]
-    end
-  }
-  self.updatePreloadedPt(initialUpdaters)
-end
-
-function Test:prepareNewPreloadedPT()
-  local newUpdaters = {
-    function(data)
-      data.policy_table.module_config.preloaded_date = TESTED_DATA.preloaded_date[2]
-    end,
-  }
-  self.updatePreloadedPt(newUpdaters)
-end
-
---[[ Preconditions ]]
-commonFunctions:newTestCasesGroup("Preconditions")
-function Test:Precondition_StopSDL()
-  TestData:init()
-  StopSDL(self)
-end
-
-function Test:Precondition()
-  commonSteps:DeletePolicyTable()
-  self.backupPreloadedPT("backup_")
-
-  self:prepareInitialPreloadedPT()
-  TestData:store("Initial Preloaded PT is stored", config.pathToSDL .. PRELOADED_PT_FILE_NAME, "initial_" .. PRELOADED_PT_FILE_NAME)
-end
-
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 
-function Test:Test_FirstStartSDL()
-  StartSDL(config.pathToSDL, true, self)
-end
-
-function Test:Test_InitialLocalPT()
+function Test:TestStep_VerifyInitialLocalPT()
   os.execute("sleep 3")
   TestData:store("Initial Local PT is stored", constructPathToDatabase(), "initial_policy.sqlite")
   local checks = {
     {
       query = 'select preloaded_date from module_config',
       expectedValues = {TESTED_DATA.preloaded_date[1]}
+    },
+    {
+      query = 'select language_code from message where message_type_name = "Location"',
+      expectedValues = {TESTED_DATA[1].key}
+    },
+    {
+      query = 'select tts from message where message_type_name = "Location"',
+      expectedValues = {TESTED_DATA[1].tts}
+    },
+    {
+      query = 'select label from message where message_type_name = "Location"',
+      expectedValues = {TESTED_DATA[1].label}
     }
   }
   if not self.checkLocalPT(checks) then
@@ -260,26 +297,37 @@ function Test:Test_InitialLocalPT()
   end
 end
 
-function Test:Test_FirstStopSDL()
+function Test:TestStep_StopSDL()
   StopSDL(self)
 end
 
-function Test:Test_NewPreloadedPT()
-  self:prepareNewPreloadedPT()
-  TestData:store("New Preloaded PT is stored", config.pathToSDL .. PRELOADED_PT_FILE_NAME, "new_" .. PRELOADED_PT_FILE_NAME)
+function Test.TestStep_LoadNewPreloadedPT()
+  prepareNewPreloadedPT()
 end
 
-function Test:Test_SecondStartSDL()
+function Test:TestStep_StartSDL()
   StartSDL(config.pathToSDL, true, self)
 end
 
-function Test:Test_NewLocalPT()
+function Test:TestStep_VerifyNewLocalPT()
   os.execute("sleep 3")
   TestData:store("New Local PT is stored", constructPathToDatabase(), "new_policy.sqlite")
   local checks = {
     {
       query = 'select preloaded_date from module_config',
       expectedValues = {TESTED_DATA.preloaded_date[2]}
+    },
+    {
+      query = 'select language_code from message where message_type_name = "Location"',
+      expectedValues = {TESTED_DATA[1].key, TESTED_DATA[2].key, TESTED_DATA[3].key}
+    },
+    {
+      query = 'select tts from message where message_type_name = "Location"',
+      expectedValues = {TESTED_DATA[1].tts, TESTED_DATA[2].tts, TESTED_DATA[3].tts}
+    },
+    {
+      query = 'select label from message where message_type_name = "Location"',
+      expectedValues = {TESTED_DATA[1].label, TESTED_DATA[2].label, TESTED_DATA[3].label}
     }
   }
   if not self.checkLocalPT(checks) then
@@ -289,13 +337,9 @@ end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-
-function Test:Postcondition()
-  commonSteps:DeletePolicyTable()
-  self.restorePreloadedPT("backup_")
+testCasesForPolicyTable:Restore_preloaded_pt()
+function Test.Postcondition()
   StopSDL()
-  TestData:info()
 end
 
-commonFunctions:SDLForceStop()
 return Test
