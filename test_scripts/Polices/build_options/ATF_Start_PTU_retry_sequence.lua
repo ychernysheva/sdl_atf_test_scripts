8726c653e406ac5391b99a23c691cbf3f24ae251
Anna Rotar (GitHub) <ARotar@luxoft.com>
2016-12-12 16:13:18 +0200

    Update ATF_Start_PTU_retry_sequence.lua

--- test_scripts/Polices/build_options/ATF_Start_PTU_retry_sequence.lua
+++ test_scripts/Polices/build_options/ATF_Start_PTU_retry_sequence.lua
@@ -1,6 +1,3 @@
--- UNREADY:
---function Test:TestStep_Start_Retry_Sequence is not developed
---function testCasesForPolicyTable.flow_PTU_SUCCEESS_PROPRIETARY
 ---------------------------------------------------------------------------------------------
 -- Requirements summary:
 -- [PolicyTableUpdate] Local Policy Table retry sequence start
@@ -10,9 +7,7 @@
 --      "timeout_after_x_seconds" section of Local PT, it must start the retry sequence.
 -- 1. Used preconditions
 --      SDL is built with "-DEXTENDED_POLICY: PROPRIETARY" flag
---      Application is registered
---      PTU is requested
---      PTU omits "consumer_friendly_messages" section
+--      Application not in PT is registered -> PTU is triggered
 --      SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
 --      SDL->HMI:SDL.PolicyUpdate(file, timeout, retry[])
 --      HMI -> SDL: SDL.GetURLs (<service>)
@@ -31,59 +26,81 @@ config.defaultProtocolVersion = 2
 --[[ Required Shared libraries ]]
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local commonPreconditions = require ('user_modules/shared_testcases/commonPreconditions')
 local testCasesForPolicyTableSnapshot = require ('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
 
 --[[ General Precondition before ATF start ]]
 commonSteps:DeleteLogsFileAndPolicyTable()
+commonPreconditions:Connecttest_without_ExitBySDLDisconnect_WithoutOpenConnectionRegisterApp("connecttest_RAI.lua")
 
 --[[ General Settings for configuration ]]
-Test = require('connecttest')
+Test = require('user_modules/connecttest_RAI')
 require('cardinalities')
 require('user_modules/AppTypes')
+local mobile_session = require('mobile_session')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup("Preconditions")
+function Test:Precondition_Connect_mobile()
+  self:connectMobile()
+end
+
+function Test:Precondition_Start_new_session()
+  self.mobileSession = mobile_session.MobileSession( self, self.mobileConnection)
+  self.mobileSession:StartService(7)
+end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
---[TODO: Function should be implmented
-function Test:TestStep_Start_Retry_Sequence ()
-  testCasesForPolicyTable:flow_PTU_SUCCEESS_PROPRIETARY(self, app_id, device_id, hmi_app_id, ptu_file_path, ptu_file_name, ptu_file)
-  if (app_id == nil) then app_id = config.application1.registerAppInterfaceParams.appID end
-  if (device_id == nil) then device_id = config.deviceMAC end
-  if (hmi_app_id == nil) then hmi_app_id = self.applications[config.application1.registerAppInterfaceParams.appName] end
-  if (ptu_file_path == nil) then ptu_file_path = "files/" end
-  if (ptu_file_name == nil) then ptu_file_name = "PolicyTableUpdate" end
-  if (ptu_file == nil) then ptu_file = "ptu.json" end
-  --[[Start get data from PTS]]
-  --TODO(istoimenova): function for reading INI file should be implemented
-  --local SystemFilesPath = commonSteps:get_data_from_SDL_ini("SystemFilesPath")
-  local SystemFilesPath = "/tmp/fs/mp/images/ivsu_cache/"
+function Test:Start_Retry_Sequence_PROPRIETARY()
+  local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application1.appName } })
+    :Do(function(_,data)
+    local hmi_app_id = data.params.application.appID
+    EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
 
-  -- Check SDL snapshot is created correctly and get needed data
-  testCasesForPolicyTableSnapshot:verify_PTS(true, {app_id}, {device_id}, {hmi_app_id})
-  local endpoints = {}
-  for i = 1, #testCasesForPolicyTableSnapshot.pts_endpoints do
-    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "0x07") then
-      endpoints[1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
+    testCasesForPolicyTableSnapshot:create_PTS(true,
+    {config.application1.registerAppInterfaceParams.appID},
+    {config.deviceMAC},
+    {hmi_app_id})
+
+    local timeout_after_x_seconds = testCasesForPolicyTableSnapshot:get_data_from_PTS("module_config.timeout_after_x_seconds")
+    local seconds_between_retries = {}
+    for i = 1, #testCasesForPolicyTableSnapshot.pts_seconds_between_retries do
+      seconds_between_retries[i] = testCasesForPolicyTableSnapshot.pts_seconds_between_retries[i].value
     end
-  end
-  local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints} } )
-  :Do(function(_,_)
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
-        { requestType = "PROPRIETARY", fileName = ptu_file_name})
-      EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "PROPRIETARY"})
-      :Do(function(_,_)
-          EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
-            {status = "UPDATING"})
-            :Times(1)
-          local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "PROPRIETARY", fileName = ptu_file_name, appID = app_id}, ptu_file_path..ptu_file)
-          EXPECT_HMICALL("BasicCommunication.SystemRequest",{ requestType = "PROPRIETARY", fileName = SystemFilesPath..ptu_file_name })
-          :Do(function(_,_data1)
+    EXPECT_HMICALL("BasicCommunication.PolicyUpdate",
+    {
+      file = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate",
+      timeout = timeout_after_x_seconds,
+      retry = seconds_between_retries
+    })
+    :Do(function(_,_)
+      EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATING"})
+      :Times(1)
+      self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+        :Do(function(_,_)
+        local ptu_file_path = "files/"
+        local ptu_file_name = "PolicyTableUpdate"
+        local ptu_file = "ptu.json"
+        local SystemFilesPath = "/tmp/fs/mp/images/ivsu_cache/"
+        local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+        EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS", urls = {{url = "http://policies.telematics.ford.com/api/policies"}}}})
+          :Do(function(_,_)
+          self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",{ requestType = "PROPRIETARY", fileName = "PolicyTableUpdate"})
+          EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "PROPRIETARY"})
+            :Do(function(_,_)
+            local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "PROPRIETARY", fileName = "PolicyTableUpdate", hmi_app_id, ptu_file_path..ptu_file})
+            EXPECT_HMICALL("BasicCommunication.SystemRequest",{ requestType = "PROPRIETARY", fileName = SystemFilesPath..ptu_file_name })
+              :Do(function(_,_data1)
               self.hmiConnection:SendResponse(_data1.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
+              end)
+            EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
+            :Times(1)
             end)
-          EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
-            {status = "UPDATE_NEEDED"}):Times(1)
+          end)
         end)
+      end)
     end)
 end
 
