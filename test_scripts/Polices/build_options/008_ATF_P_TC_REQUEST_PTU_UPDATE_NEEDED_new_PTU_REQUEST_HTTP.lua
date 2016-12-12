81bf2d4842171177a85b5fe0238e38aa5cb64511
maria_mih <mmihaylova-banska@luxoft.com>
2016-12-12 17:19:40 +0200

    Script is updated with new postconditions, numbering and requirements

--- test_scripts/Polices/build_options/008_ATF_P_TC_REQUEST_PTU_UPDATE_NEEDED_new_PTU_REQUEST_HTTP.lua
+++ test_scripts/Polices/build_options/008_ATF_P_TC_REQUEST_PTU_UPDATE_NEEDED_new_PTU_REQUEST_HTTP.lua
@@ -1,9 +1,11 @@
 ---------------------------------------------------------------------------------------------
 -- Requirements summary:
 -- [PolicyTableUpdate] OnStatusUpdate(UPDATE_NEEDED) on new PTU request
+-- [HMI API] OnStatusUpdate
+-- [HMI API] UpdateSDL request/response
 --
 -- Description:
--- SDL should request PTU in case new application is registered and is not listed in PT
+-- PoliciesManager must notify HMI via SDL.OnStatusUpdate(UPDATE_NEEDED) on any PTU trigger.
 -- 1. Used preconditions
 -- SDL is built with "-DEXTENDED_POLICY: HTTP" flag
 -- Connect mobile phone over WiFi.
@@ -22,37 +24,17 @@ config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd40
 --[[ Required Shared libraries ]]
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
-local commonPreconditions = require ('user_modules/shared_testcases/commonPreconditions')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
 local testCasesForPolicyTableSnapshot = require ('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
 
 --[[ General Precondition before ATF start ]]
 commonSteps:DeleteLogsFileAndPolicyTable()
-commonPreconditions:Connecttest_without_ExitBySDLDisconnect_WithoutOpenConnectionRegisterApp("connecttest_RAI.lua")
-
---ToDo: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+--TODO: Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
 config.defaultProtocolVersion = 2
 
 --[[ General Settings for configuration ]]
-Test = require('user_modules/connecttest_RAI')
+Test = require('connecttest')
 require('cardinalities')
 require('user_modules/AppTypes')
-local mobile_session = require('mobile_session')
-
---[[ Preconditions ]]
-commonFunctions:newTestCasesGroup("Preconditions")
-function Test.Precondition_remove_user_connecttest()
-  os.execute( "rm -f ./user_modules/connecttest_RAI.lua" )
-end
-
-function Test:TestStep_ConnectMobile()
-  self:connectMobile()
-end
-
-function Test:TestStep_StartNewSession()
-  self.mobileSession = mobile_session.MobileSession( self, self.mobileConnection)
-  self.mobileSession:StartService(7)
-end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
@@ -62,12 +44,12 @@ function Test:TestStep_OnStatusUpdate_UPDATE_NEEDED_new_PTU_request()
 
   EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application1.appName } })
   :Do(function(_,data)
-    local hmi_app_id = data.params.application.appID
+      local hmi_app_id = data.params.application.appID
       EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
 
       testCasesForPolicyTableSnapshot:verify_PTS(true,
         {config.application1.registerAppInterfaceParams.appID},
-        {config.deviceMAC}, 
+        {config.deviceMAC},
         {hmi_app_id})
 
       local timeout_after_x_seconds = testCasesForPolicyTableSnapshot:get_data_from_PTS("module_config.timeout_after_x_seconds")
@@ -91,8 +73,9 @@ end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-function Test:Postcondition_SDLForceStop()
-  commonFunctions:SDLForceStop(self)
+
+function Test.Postcondition_Stop_SDL()
+  StopSDL()
 end
 
 return Test
