65cc99d0461b323a22ceed03aba6fa6236e1a14b
Anna Rotar (GitHub) <ARotar@luxoft.com>
2016-12-14 14:51:57 +0200

    Update ATF_Consent_status_not_allowed_on_device_connect.lua

--- test_scripts/Polices/user_consent_of_Policies/ATF_Consent_status_not_allowed_on_device_connect.lua
+++ test_scripts/Polices/user_consent_of_Policies/ATF_Consent_status_not_allowed_on_device_connect.lua
@@ -1,22 +1,22 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
---    [DeviceConsent] DataConsent status for each device is written in LocalP
+--    [DeviceConsent] DataConsent status for each device is written in LocalPT
 --
 -- Description:
 --     Providing the device`s DataConsent status (not allowed) to HMI upon device connection to SDL
 --     1. Used preconditions:
--- 	      Delete files and policy table from previous ignition cycle if any
---
+--        Delete files and policy table from previous ignition cycle if any
+--        Overwrite preloaded to ensure device is not consented
 --     2. Performed steps:
---        Connect device
+--        Connect device 
 --
 -- Expected result:
---     SDL must request DataConsent status of the corresponding device from the PoliciesManager to be taken from the Local PoliciesTable and provide it to HMI upon device connection
----------------------------------------------------------------------------------------------
+--     SDL/PoliciesManager must provide the device`s DataConsent status (not allowed) to HMI upon device`s connection->
+--     SDL must request DataConsent status of the corresponding device from the PoliciesManager 
+-------------------------------------------------------------------------------------------------
 --[[ General Settings for configuration ]]
 Test = require('user_modules/connecttest_resumption')
 require('cardinalities')
-require('mobile_session')
 
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
@@ -27,19 +27,60 @@ local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
 require('user_modules/AppTypes')
 
+--[[ Local Functions ]]
+local function Backup_preloaded()
+  os.execute('cp ' .. config.pathToSDL .. 'sdl_preloaded_pt.json' .. ' ' .. config.pathToSDL .. 'backup_sdl_preloaded_pt.json')
+  os.execute('rm ' .. config.pathToSDL .. 'policy.sqlite')
+end
+
+local function Restore_preloaded()
+  os.execute('rm ' .. config.pathToSDL .. 'sdl_preloaded_pt.json')
+  os.execute('cp ' .. config.pathToSDL .. 'backup_sdl_preloaded_pt.json' .. ' ' .. config.pathToSDL .. 'sdl_preloaded_pt.json')
+end
+
+local function Set_no_consent_for_device()
+  local pathToFile = config.pathToSDL .. 'sdl_preloaded_pt.json'
+  local file = io.open(pathToFile, "r")
+  local json_data = file:read("*all") -- may be abbreviated to "*a";
+  file:close()
+  local json = require("modules/json")
+  local data = json.decode(json_data)
+
+  if data.policy_table.functional_groupings["DataConsent-2"] then
+    data.policy_table.functional_groupings["DataConsent-2"] = nil
+  end
+    data.policy_table.app_policies["device"] = {
+    keep_context = false,
+    steal_focus = false,
+    priority = "NONE",
+    default_hmi = "NONE",
+    groups = {"Location-1"},
+    preconsented_groups = {"Base-4"}
+  }
+  data = json.encode(data)
+  file = io.open(pathToFile, "w")
+  file:write(data)
+  file:close()
+end
+
 --[[ Preconditions ]]
-commonSteps:DeleteLogsFileAndPolicyTable()
+commonFunctions:newTestCasesGroup("Preconditions")
+function Test.Precondition_DeleteLogsAndPolicyTable()
+  commonSteps:DeleteLogsFiles()
+  commonSteps:DeletePolicyTable()
+end
 
-function Test:Precondition_Close_current_connection()
-  self.mobileConnection:Close()
-  commonTestCases:DelayedExp(3000)
+function Test.Precondition_Backup_preloadedPT()
+  Backup_preloaded()
+end
 
+function Test.Precondition_Set_no_consent_for_device()
+  Set_no_consent_for_device()
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
-
-function Test:Consent_status_not_allowed_on_device_connect()
+function Test:Check_device_connects_as_not_consented()
   commonTestCases:DelayedExp(2000)
   self:connectMobile()
   EXPECT_HMICALL("BasicCommunication.UpdateDeviceList",
@@ -61,7 +102,9 @@ end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-
-function Test.Postcondition_SDLForceStop()
-  commonFunctions:SDLForceStop()
+function Test.Postcondition_SDLStop()
+  StopSDL()
+end
+function Test.Postcondition_Restore_preloadedPT()
+  Restore_preloaded()
 end
