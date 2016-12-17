053dde69ffeacf676bf96590bcf419114e7ee2e4
Alexandr Deriabin <aderiabin@luxoft.com>
2016-12-17 16:36:22 +0200

    Updated test script for check validation rules for RequestType section of PTU in case array contains one incorrect value, other are valid

--- test_scripts/Polices/Validation_of_PolicyTables/052_ATF_pt_update_validation_rules_request_type_array_has_only_one_value_is_invalid.lua
+++ test_scripts/Polices/Validation_of_PolicyTables/052_ATF_pt_update_validation_rules_request_type_array_has_only_one_value_is_invalid.lua
@@ -373,10 +373,8 @@ function Test:updatePolicyInDifferentSessions(PTName, appName, mobileSession)
       for _, updateFunc in pairs(updaters) do
         updateFunc(data)
       end
-      -- Workaround. null value in lua table == not existing value. But in json file it has to be
-      data.policy_table.functional_groupings["DataConsent-2"].rpcs = "tobedeletedinjsonfile"
+      data.policy_table.functional_groupings["DataConsent-2"].rpcs = json.null
       local dataToWrite = json.encode(data)
-      dataToWrite = string.gsub(dataToWrite, "\"tobedeletedinjsonfile\"", "null")
       file = io.open(pathToFile, "w")
       file:write(dataToWrite)
       file:close()
