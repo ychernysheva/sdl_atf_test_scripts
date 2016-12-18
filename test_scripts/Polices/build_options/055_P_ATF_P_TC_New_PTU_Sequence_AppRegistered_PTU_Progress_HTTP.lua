b02013faf6c5cce9ce3d3855f644c0407b56a58f
Georgi <istoimenova@luxoft.com>
2016-12-18 23:03:24 +0200

    Script is ready to be merged.

--- test_scripts/Polices/build_options/055_P_ATF_P_TC_New_PTU_Sequence_AppRegistered_PTU_Progress_HTTP.lua
+++ test_scripts/Polices/build_options/055_P_ATF_P_TC_New_PTU_Sequence_AppRegistered_PTU_Progress_HTTP.lua
@@ -1,17 +1,10 @@
---UNREADY:
---function to be developed for action:
--- In https://github.com/smartdevicelink/sdl_atf_test_scripts/pull/247/ there are
---attached ptu_prop.json at ptu.json that need to be renamed and
---copied at: /tmp/fs/mp/images/ivsu_cache/, for lines containing "/tmp/fs/mp/images/ivsu_cache/ptu.json"
--- functions in Test section need to be updated
-
-
+-------------------------------------------------------------------------------------------------------------------------------------
 -- Requirement summary:
 -- [PolicyTableUpdate] New PTU sequence initiation (app registering during PTU being in progress)
 --
 -- Description:
---PoliciesManager must initiate a new PTU sequence right after the previous PTUpdate is received, 
---validated and applied IN CASE a new app with appID that does not yet exist in Local PT registeres 
+--PoliciesManager must initiate a new PTU sequence right after the previous PTUpdate is received,
+--validated and applied IN CASE a new app with appID that does not yet exist in Local PT registeres
 --while PoliciesManager has sent the PT Snapshot and has not received the PT Update yet.
 -- 1. Used preconditions
 -- SDL is built with "-DEXTENDED_POLICY: HTTP" flag
@@ -24,7 +17,7 @@
 -- Expected result:
 -- 1. PoliciesManager validates the updated PT (policyFile) e.i. verifyes, saves the updated fields and everything that is defined with related requirements)
 -- 2. On validation success: SDL->HMI:OnStatusUpdate("UP_TO_DATE")
--- 3. SDL replaces the following sections of the Local Policy Table with the corresponding 
+-- 3. SDL replaces the following sections of the Local Policy Table with the corresponding
 -- sections from PTU:module_config, functional_groupings, app_policies
 -- 4. app_2 added to Local PT during PT Exchange process left after merge in LocalPT (not being lost on merge)
 -- 5. SDL creates the new snapshot and initiates the new PTU for the app_2 Policies obtaining: SDL-> HMI: SDL.PolicyUpdate()//new PTU sequence started
@@ -33,9 +26,10 @@
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
 local mobile_session = require('mobile_session')
+
 
 --[[ Local Functions ]]
 local registerAppInterfaceParams =
@@ -61,10 +55,10 @@ local registerAppInterfaceParams =
   }
 }
 
---[[ General Precondition before ATF start]]
+--[[ General Precondition before ATF start ]]
 commonSteps:DeleteLogsFileAndPolicyTable()
 
---ToDo: Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+--ToDo: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
 config.defaultProtocolVersion = 2
 
 --[[ General Settings for configuration ]]
@@ -74,110 +68,28 @@ require('user_modules/AppTypes')
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup ("Preconditions")
-function Test:Precondition_PolicyUpdateStarted()
-  local pathToSnaphot = nil
-  EXPECT_HMICALL ("BasicCommunication.PolicyUpdate")
-  :Do(function(_,data)
-      pathToSnaphot = data.params.file
-      self.hmiConnection:SendResponse(data.id, "BasicCommunication.PolicyUpdate", "SUCCESS", {})
-    end)
-  local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {url = "http://policies.telematics.ford.com/api/policies"}}})
-  :Do(function(_,_)
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
-        {
-          requestType = "HTTP",
-          url = "http://policies.telematics.ford.com/api/policies",
-          appID = self.applications ["Test Application"],
-          fileName = "sdl_snapshot.json"
-        },
-        pathToSnaphot
-      )
-    end)
-  EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "HTTP" })
-end
-
 function Test:Precondition_OpenNewSession()
   self.mobileSession2 = mobile_session.MobileSession(self, self.mobileConnection)
   self.mobileSession2:StartService(7)
 end
 
---[[ Test ]]
-commonFunctions:newTestCasesGroup ("Test")
-function Test:TestStep_RegisterApplication_In_NewSession()
+function Test:Precondition_RAI_NewSession()
   local corId = self.mobileSession2:SendRPC("RegisterAppInterface", registerAppInterfaceParams)
   EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = "Media Application" }})
   self.mobileSession2:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
   self.mobileSession2:ExpectNotification("OnPermissionsChange")
 end
 
-function Test.TestStep_FinishPTU_For_FirstApplication()
-  -- local CorIdSystemRequest = self.mobileSession:SendRPC ("SystemRequest",
-  --   {
-  --     requestType = "PROPRIETARY",
-  --     fileName = "ptu.json"
-  --   },
-  --   "/tmp/fs/mp/images/ivsu_cache/ptu.json"
-  -- )
-  -- EXPECT_HMICALL("BasicCommunication.SystemRequest")
-  -- :Do(function(_,data)
-  --     self.hmiConnection:SendResponse(data.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
-  --   end)
-  -- EXPECT_RESPONSE(CorIdSystemRequest, {success = true, resultCode = "SUCCESS"})
-  -- :Do(function(_,_)
-  --     os.execute("cp /home/anikolaev/OpenSDL_AUTOMATION/test_run/files/ptu.json /tmp/fs/mp/images/ivsu_cache/")
-  --     self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
-  --       {
-  --         policyfile = "/tmp/fs/mp/images/ivsu_cache/ptu.json"
-  --       })
-  --   end)
-  -- :Do(function(_,_)
-  --     EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"})
-  --   end)
-end
+--[[ Test ]]
+commonFunctions:newTestCasesGroup ("Test")
+function Test:TestStep_PoliciesManager_changes_UP_TO_DATE()
+  assert(commonFunctions:File_exists("files/ptu.json"))
+  local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest",
+    { requestType = "HTTP", fileName = "PolicyTableUpdate" },"files/ptu.json")
 
-function Test:TestStep_CheckThatAppID_Present_In_DataBase()
-  local PolicyDBPath = nil
-  if commonSteps:file_exists(tostring(config.pathToSDL) .. "/storage/policy.sqlite") == true then
-    PolicyDBPath = tostring(config.pathToSDL) .. "/storage/policy.sqlite"
-  end
-  if commonSteps:file_exists(tostring(config.pathToSDL) .. "/storage/policy.sqlite") == false then
-    commonFunctions:userPrint(31, "policy.sqlite file is not found")
-    self:FailTestCase("PolicyTable is not avaliable" .. tostring(PolicyDBPath))
-  end
-  os.execute(" sleep 2 ")
-  local AppId_2 = "sqlite3 " .. tostring(PolicyDBPath) .. "\"SELECT id FROM application WHERE id = '"..tostring(registerAppInterfaceParams.appID).."'\""
-  local bHandle = assert( io.popen(AppId_2, 'r'))
-  local AppIdValue_2 = bHandle:read( '*l' )
-  if AppIdValue_2 == nil then
-    self:FailTestCase("Value in DB is unexpected value " .. tostring(AppIdValue_2))
-  end
-end
-
-function Test:TestStep_Start_New_PolicyUpdate_For_SecondApplication()
-  local pathToSnaphot = nil
-  EXPECT_HMICALL ("BasicCommunication.PolicyUpdate")
-  :Do(function(_,data)
-      pathToSnaphot = data.params.file
-      self.hmiConnection:SendResponse(data.id, "BasicCommunication.PolicyUpdate", "SUCCESS", {})
-    end)
-  local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {url = "http://policies.telematics.ford.com/api/policies"}}})
-  :Do(function(_,_)
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
-        {
-          requestType = "HTTP",
-          url = "http://policies.telematics.ford.com/api/policies",
-          appID = self.applications ["MyTestApp"],
-          fileName = "sdl_snapshot.json"
-        },
-        pathToSnaphot
-      )
-    end)
-  EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "HTTP" })
-  :Do(function(_,_)
-      EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
-    end)
+  EXPECT_RESPONSE(CorIdSystemRequest, { success = true, resultCode = "SUCCESS"})
+  EXPECT_HMICALL("BasicCommunication.SystemRequest"):Times(0)
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"}, {"UPDATE_NEEDED"}):Times(2)
 end
 
 --[[ Postconditions ]]
