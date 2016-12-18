4689c61838bae6ee1a1266024a8629d874c6300a
Georgi <istoimenova@luxoft.com>
2016-12-18 11:22:38 +0200

    Script is ready to be merged.

--- test_scripts/Polices/build_options/042_ATF_P_TC_PTU_OnStatsusUpdate_Trigger_UPDATE_NEEDED_HTTP.lua
+++ test_scripts/Polices/build_options/042_ATF_P_TC_PTU_OnStatsusUpdate_Trigger_UPDATE_NEEDED_HTTP.lua
@@ -20,76 +20,57 @@
 -- Expected result:
 --SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
 ---------------------------------------------------------------------------------------------
-
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
-local testCasesForPolicyTableSnapshot = require ('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+local commonPreconditions = require ('user_modules/shared_testcases/commonPreconditions')
 
 --[[ General Precondition before ATF start ]]
 commonSteps:DeleteLogsFileAndPolicyTable()
-
---ToDo: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+commonPreconditions:Connecttest_without_ExitBySDLDisconnect_WithoutOpenConnectionRegisterApp("connecttest_ConnectMobile.lua")
+--TODO: Should be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
 config.defaultProtocolVersion = 2
 
 --[[ General Settings for configuration ]]
-Test = require('connecttest')
+Test = require('user_modules/connecttest_ConnectMobile')
 require('cardinalities')
 require('user_modules/AppTypes')
+local mobile_session = require('mobile_session')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup("Preconditions")
+
+function Test:Precondition_Connect_device()
+  self:connectMobile()
+end
+
+function Test:Precondition_StartSession()
+  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession:StartService(7)
+end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
-function Test:TestStep_PTU_UPDATED_NEEDED_Status ()
-  testCasesForPolicyTable:flow_PTU_SUCCEESS_HTTP(self, app_id, device_id, hmi_app_id, ptu_file_path, ptu_file_name, ptu_file)
-  if (app_id == nil) then app_id = config.application1.registerAppInterfaceParams.appID end
-  if (device_id == nil) then device_id = config.deviceMAC end
-  if (hmi_app_id == nil) then hmi_app_id = self.applications[config.application1.registerAppInterfaceParams.appName] end
-  if (ptu_file_path == nil) then ptu_file_path = "files/" end
-  if (ptu_file_name == nil) then ptu_file_name = "PolicyTableUpdate" end
-  if (ptu_file == nil) then ptu_file = "ptu.json" end
-  --[[Start get data from PTS]]
-  --TODO(istoimenova): function for reading INI file should be implemented
-  --local SystemFilesPath = commonSteps:get_data_from_SDL_ini("SystemFilesPath")
-  local SystemFilesPath = "/tmp/fs/mp/images/ivsu_cache/"
 
-  -- Check SDL snapshot is created correctly and get needed data
-  testCasesForPolicyTableSnapshot:verify_PTS(true, {app_id}, {device_id}, {hmi_app_id})
+function Test:TestStep_OnStatusUpdate_UPDATE_NEEDED_new_PTU_request()
+  local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
 
-  local endpoints = {}
-  for i = 1, #testCasesForPolicyTableSnapshot.pts_endpoints do
-    if (testCasesForPolicyTableSnapshot.pts_endpoints[i].service == "0x07") then
-      endpoints[1] = { url = testCasesForPolicyTableSnapshot.pts_endpoints[i].value, appID = nil}
-    end
-  end
-  local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints} } )
-  :Do(function(_,_)
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
-        { requestType = "HTTP", fileName = ptu_file_name})
-      EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "HTTP"})
-      :Do(function(_,_)
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", {application = { appName = config.application1.registerAppInterfaceParams.appName } })
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
+    { status = "UPDATE_NEEDED" }, {status = "UPDATING"}):Times(2)
+  EXPECT_NOTIFICATION("OnSystemRequest", {requestType = "HTTP"})
 
-          EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
-            {status = "UPDATING"}):Times(1)
-
-          local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "HTTP", fileName = ptu_file_name, appID = app_id}, ptu_file_path..ptu_file)
-          EXPECT_HMICALL("BasicCommunication.SystemRequest",{ requestType = "HTTP", fileName = SystemFilesPath..ptu_file_name })
-          :Do(function(_,_data1)
-              self.hmiConnection:SendResponse(_data1.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
-            end)
-          EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
-            {status = "UPDATE_NEEDED"}):Times(1)
-        end)
-    end)
+  self.mobileSession:ExpectResponse(correlationId, { success = true, resultCode = "SUCCESS" })
+  self.mobileSession:ExpectNotification("OnHMIStatus", {hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-function Test.Stop()
+
+function Test.Postcondition_Stop_SDL()
   StopSDL()
 end
 
