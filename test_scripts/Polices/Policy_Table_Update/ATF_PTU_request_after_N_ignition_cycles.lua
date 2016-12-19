17219c943fc50e4634a018640790b6e8093a0d40
ovikhrov <ovikhrov@luxoft.com>
2016-12-19 19:33:09 +0200

    Reworked. Prepared to merge. Opened question

--- test_scripts/Polices/Policy_Table_Update/ATF_PTU_request_after_N_ignition_cycles.lua
+++ test_scripts/Polices/Policy_Table_Update/ATF_PTU_request_after_N_ignition_cycles.lua
@@ -1,30 +1,37 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
---   [PolicyTableUpdate] Request to update PT - after "N" ignition cycles
+-- [PolicyTableUpdate] Request to update PT - after "N" ignition cycles
 --
 -- Description:
---     Triggering PTU on reaching 'ignition_cycles_since_last_exchange' value
---     1. Used preconditions:
---      Delete log file and policy table if any
---      Overwrite preloaded PT with "module_config"->"ignition_cycles_since_last_exchange" = 1
---      Start SDL and HMI
---      Register app on consented device
+-- Triggering PTU on reaching 'ignition_cycles_since_last_exchange' value
+-- 1. Used preconditions:
+-- Delete log file and policy table if any
+-- Register app and update policy
+-- Stop SDL
+-- Set ignition_cycles_since_last_exchange less on 1 from exchange_after_x_ignition_cycles
+-- Start SDL
+-- Register app
 --
---     2. Performed steps
---      Preform IGNOF
+-- 2. Performed steps
+-- Preform IGNOF
 --
 -- Expected result:
---      HMI->SDL:BasicCommunication.OnIgnitionCycleOver->
---      SDL must trigger a PolicyTableUpdate sequence
+-- HMI->SDL:BasicCommunication.OnIgnitionCycleOver->
+-- SDL must trigger a PolicyTableUpdate sequence
 ---------------------------------------------------------------------------------------------
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+config.application1.registerAppInterfaceParams.appHMIType = {"DEFAULT"}
 --[ToDo: should be removed when fixed: "ATF does not stop HB timers by closing session and connection"
 config.defaultProtocolVersion = 2
 
 --[[ Required Shared libraries ]]
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
+
+--[[ General Precondition before ATF start ]]
+commonSteps:DeleteLogsFiles()
+commonSteps:DeletePolicyTable()
 
 --[[ General Settings for configuration ]]
 Test = require('connecttest')
@@ -33,56 +40,72 @@ local mobile_session = require('mobile_session')
 require('user_modules/AppTypes')
 
 --[[ Local functions ]]
-local function Backup_preloaded()
-  os.execute('cp ' .. config.pathToSDL .. 'sdl_preloaded_pt.json' .. ' ' .. config.pathToSDL .. 'backup_sdl_preloaded_pt.json')
-  os.execute('rm ' .. config.pathToSDL .. 'policy.sqlite')
-end
-
-local function Restore_preloaded()
-  os.execute('rm ' .. config.pathToSDL .. 'sdl_preloaded_pt.json')
-  os.execute('cp ' .. config.pathToSDL .. 'backup_sdl_preloaded_pt.json' .. ' ' .. config.pathToSDL .. 'sdl_preloaded_pt.json')
-end
-
-local function Set_ignition_cycles_since_last_exchange(value)
-  local pathToFile = config.pathToSDL .. 'sdl_preloaded_pt.json'
-  local file = io.open(pathToFile, "r")
-  local json_data = file:read("*all")
-  file:close()
-  local json = require("modules/json")
-  local data = json.decode(json_data)
-
-  if data.policy_table.functional_groupings["DataConsent-2"] then
-    data.policy_table.functional_groupings["DataConsent-2"] = nil
-  end
-
-  data.policy_table.module_config.ignition_cycles_since_last_exchange = value
-  data = json.encode(data)
-  file = io.open(pathToFile, "w")
-  file:write(data)
-  file:close()
+local function SetIgnitionCyclesSinceLastExchange()
+  local pathToDB = config.pathToSDL .. "storage/policy.sqlite"
+  local sql_query = 'select exchange_after_x_ignition_cycles from module_config where rowid=1;'
+  local exchange_after_x_ignition_cycles = commonFunctions:get_data_policy_sql(pathToDB, sql_query)
+  local DBQuery = 'sqlite3 ' .. pathToDB .. ' \"UPDATE module_meta SET ignition_cycles_since_last_exchange = ' .. tonumber(exchange_after_x_ignition_cycles[1]) - 1 .. ' WHERE rowid = 1;\"'
+  os.execute(DBQuery)
+  os.execute(" sleep 1 ")
 end
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
 
-function Test.Precondition_StopSDL()
+function Test:Precondition_Activate_App_Consent_Device_And_Update_Policy()
+  local RequestId = self.hmiConnection:SendRequest("SDL.ActivateApp", {appID = self.applications["Test Application"]})
+  EXPECT_HMIRESPONSE(RequestId, {result = {code = 0, isSDLAllowed = false}, method = "SDL.ActivateApp"})
+  :Do(function(_,_)
+      local RequestIdGetUserFriendlyMessage = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = {"DataConsent"}})
+      EXPECT_HMIRESPONSE(RequestIdGetUserFriendlyMessage,{result = {code = 0, method = "SDL.GetUserFriendlyMessage"}})
+      :Do(function(_,_)
+          self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality", {allowed = true, source = "GUI", device = {id = config.deviceMAC, name = "127.0.0.1"}})
+          -- GetCurrentTimeStampDeviceConsent()
+          EXPECT_HMICALL("BasicCommunication.ActivateApp")
+          :Do(function(_,data1)
+              self.hmiConnection:SendResponse(data1.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
+              EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN"})
+              :Do(function()
+                end)
+            end)
+        end)
+    end)
+  EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
+  :Do(function(_,data)
+      pathToSnapshot = data.params.file
+      local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+      EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {{url = "http://policies.telematics.ford.com/api/policies"}}}})
+      :Do(function()
+          self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",{requestType = "PROPRIETARY", fileName = "filename"})
+          EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "PROPRIETARY" })
+          :Do(function()
+              local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {fileName = "PolicyTableUpdate", requestType = "PROPRIETARY"}, "files/ptu_general.json")
+              local systemRequestId
+              EXPECT_HMICALL("BasicCommunication.SystemRequest")
+              :Do(function(_,data1)
+                  systemRequestId = data1.id
+                  self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
+                    {
+                      policyfile = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate"
+                    })
+                  self.hmiConnection:SendResponse(systemRequestId, "BasicCommunication.SystemRequest", "SUCCESS", {})
+                  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"}):Timeout(500)
+                  self.mobileSession:ExpectResponse(CorIdSystemRequest, {success = true, resultCode = "SUCCESS"})
+                end)
+            end)
+        end)
+    end)
+end
+
+function Test:Precondition_StopSDL()
   StopSDL()
 end
 
-function Test.Precondition_DeleteLogsAndPolicyTable()
-  commonSteps:DeleteLogsFiles()
-  commonSteps:DeletePolicyTable()
+function Test:Precondition_SetIgnitionCyclesSinceLastExchange()
+  SetIgnitionCyclesSinceLastExchange()
 end
 
-function Test.Precondition_Backup_preloadedPT()
-  Backup_preloaded()
-end
-
-function Test.Precondition_Set_ignition_cycles_since_last_exchange()
-  Set_ignition_cycles_since_last_exchange(1)
-end
-
-function Test.Precondition_StartSDL()
+function Test:Precondition_StartSDL()
   StartSDL(config.pathToSDL, config.ExitOnCrash)
 end
 
@@ -99,36 +122,14 @@ function Test:Precondition_Register_app()
   self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
   self.mobileSession:StartService(7)
   :Do(function()
-  local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
-  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
-  :Do(function(_,data)
-  self.HMIAppID = data.params.application.appID
-  end)
-  self.mobileSession:ExpectResponse(correlationId, { success = true, resultCode = "SUCCESS" })
-  self.mobileSession:ExpectNotification("OnHMIStatus", {hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
-  end)
-end
-
-function Test:Precondition_Activate_app()
-  local RequestId = self.hmiConnection:SendRequest("SDL.ActivateApp", {appID = self.HMIAppID})
-  EXPECT_HMIRESPONSE(RequestId,{})
-  :Do(function(_,data)
-  if data.result.isSDLAllowed ~= true then
-    local RequestIdGetMes = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage",
-    {language = "EN-US", messageCodes = {"DataConsent"}})
-    EXPECT_HMIRESPONSE(RequestIdGetMes)
-    :Do(function()
-    self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality",
-    {allowed = true, source = "GUI", device = {id = config.deviceMAC, name = "127.0.0.1"}})
-    EXPECT_HMICALL("BasicCommunication.ActivateApp")
-    :Do(function()
-    self.hmiConnection:SendResponse(data.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
+      local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+      EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
+      :Do(function(_,data)
+          self.HMIAppID = data.params.application.appID
+        end)
+      self.mobileSession:ExpectResponse(correlationId, { success = true, resultCode = "SUCCESS" })
+      self.mobileSession:ExpectNotification("OnHMIStatus", {hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN"})
     end)
-    :Times(AtLeast(1))
-    end)
-  end
-  end)
-  EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN", audioStreamingState = "AUDIBLE"})
 end
 
 --[[ Test ]]
@@ -146,7 +147,4 @@ end
 commonFunctions:newTestCasesGroup("Postconditions")
 function Test.Postcondition_SDLStop()
   StopSDL()
-end
-function Test.Postcondition_Restore_preloaded()
-  Restore_preloaded()
 end
