72b361a829d93cc568f16008480c11c886e74295
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2016-12-20 22:40:47 +0200

    Prepare for merge

--- test_scripts/Polices/build_options/025_ATF_P_TC_PTU_Local_PT_Start_Retry_Sequence_HTTP.lua
+++ test_scripts/Polices/build_options/025_ATF_P_TC_PTU_Local_PT_Start_Retry_Sequence_HTTP.lua
@@ -6,6 +6,7 @@
 -- Description:
 -- In case PoliciesManager does not receive the Updated PT during time defined in
 -- "timeout_after_x_seconds" section of Local PT, it must start the retry sequence.
+--
 -- 1. Used preconditions
 -- SDL is built with "-DEXTENDED_POLICY: HTTP" flag
 -- Application is registered.
@@ -13,89 +14,141 @@
 -- PTU to be received satisfies data dictionary requirements
 -- PTU omits "consumer_friendly_messages" section
 -- SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
--- SDL->HMI:SDL.PolicyUpdate(file, timeout, retry[])
--- HMI -> SDL: SDL.GetURLs (<service>)
--- HMI->SDL: BasicCommunication.OnSystemRequest ('url', requestType:HTTP)
+--
 -- 2. Performed steps
 -- SDL->app: OnSystemRequest ('url', requestType:HTTP, fileType="JSON")
+--
 -- Expected result:
---Timeout expires and retry sequence started
---SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
-
+-- Timeout expires and retry sequence started
+-- SDL->HMI: SDL.OnStatusUpdate(UPDATE_NEEDED)
 ---------------------------------------------------------------------------------------------
-
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 
 --[[ Required Shared libraries ]]
-local commonSteps = require('user_modules/shared_testcases/commonSteps')
-local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
-local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
-local testCasesForPolicyTableSnapshot = require ('user_modules/shared_testcases/testCasesForPolicyTableSnapshot')
+local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
+local commonSteps = require("user_modules/shared_testcases/commonSteps")
+
+--[[ Local Variables ]]
+local sequence = { }
+local attempts = 15
+local timestamps = { }
+local r_expected = 61
+local r_actual
+local r_expected_status = { "UPDATE_NEEDED", "UPDATING", "UPDATE_NEEDED", "UPDATING" }
+local r_actual_status = { }
+
+--[[ Local Functions ]]
+local function timestamp()
+  local f = io.popen("date +%H:%M:%S.%3N")
+  local o = f:read("*all")
+  f:close()
+  return (o:gsub("\n", ""))
+end
+
+local function log(event, ...)
+  table.insert(sequence, { ts = timestamp(), e = event, p = {...} })
+end
+
+local function show_log()
+  print("--- Sequence -------------------------------------")
+  for k, v in pairs(sequence) do
+    local s = k .. ": " .. v.ts .. ": " .. v.e
+    for _, val in pairs(v.p) do
+      if val then s = s .. ": " .. val end
+    end
+    print(s)
+  end
+  print("--------------------------------------------------")
+end
+
+local function is_table_equal(t1, t2)
+  local ty1 = type(t1)
+  local ty2 = type(t2)
+  if ty1 ~= ty2 then return false end
+  if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
+  for k1, v1 in pairs(t1) do
+    local v2 = t2[k1]
+    if v2 == nil or not is_table_equal(v1, v2) then return false end
+  end
+  for k2, v2 in pairs(t2) do
+    local v1 = t1[k2]
+    if v1 == nil or not is_table_equal(v1, v2) then return false end
+  end
+  return true
+end
 
 --[[ General Precondition before ATF start ]]
+commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFileAndPolicyTable()
---TODO: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
-config.defaultProtocolVersion = 2
 
 --[[ General Settings for configuration ]]
-Test = require('connecttest')
+Test = require("connecttest")
 require('cardinalities')
-require('user_modules/AppTypes')
+require("user_modules/AppTypes")
+
+config.defaultProtocolVersion = 2
+
+--[[ Specific Notifications ]]
+EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate")
+:Do(function(_, d)
+    log("SDL->HMI: SDL.OnStatusUpdate", d.params.status)
+    table.insert(r_actual_status, d.params.status)
+  end)
+:Times(AnyNumber())
+:Pin()
+
+function Test:RegisterEvents()
+  self.mobileSession:ExpectNotification("OnSystemRequest")
+  :Do(function()
+      log("SDL->MOB: OnSystemRequest")
+      table.insert(timestamps, os.time())
+    end)
+  :Times(AnyNumber())
+  :Pin()
+end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 
-function Test:TestStep_ChangeStatus_Update_Needed()
-  local time_update_needed = {}
-  local time_system_request = {}
-  local endpoints = {}
-  local is_test_fail = false
-  local timeout_pts = testCasesForPolicyTableSnapshot:get_data_from_PTS("module_config.timeout_after_x_seconds")
-  local seconds_between_retries = {}
-  for i = 1, #testCasesForPolicyTableSnapshot.pts_seconds_between_retries do
-    seconds_between_retries[i] = testCasesForPolicyTableSnapshot.pts_seconds_between_retries[i].value
+Test["TestStep_Starting waiting cycle [" .. attempts * 5 .. "] sec"] = function() end
+
+for i = 1, attempts do
+  Test["Waiting " .. i * 5 .. " sec"] = function()
+    os.execute("sleep 5")
   end
-  local time_wait = (timeout_pts*seconds_between_retries[1]*1000)
+end
 
-  local function verify_retry_sequence()
-    time_update_needed[#time_update_needed + 1] = timestamp()
-    local time_1 = time_update_needed[#time_update_needed]
-    local time_2 = time_system_request[#time_system_request]
-    local timeout = (time_1 - time_2)
-    if( ( timeout > (timeout_pts*1000 + 2000) ) or ( timeout < (timeout_pts*1000 - 2000) )) then
-      is_test_fail = true
-      commonFunctions:printError("ERROR: timeout for first retry sequence is not as expected: "..timeout_pts.."msec(5sec tolerance). real: "..timeout.."ms")
-    else
-      print("timeout is as expected: "..tostring(timeout_pts*1000).."ms. real: "..timeout)
-    end
+function Test.TestStep_ShowTimeouts()
+  print("--- Seconds between retries ----------------------")
+  r_actual = timestamps[2] - timestamps[1]
+  print(r_actual)
+  print("--------------------------------------------------")
+end
+
+function Test:TestStep_Validate_Timeouts()
+  if (r_actual < r_expected - 5) or (r_actual > r_expected + 5) then
+    local msg = table.concat({ "Expected timeout: '", r_expected, "', got: '", r_actual, "'" })
+    self:FailTestCase(msg)
   end
+end
 
-  local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS", urls = endpoints} } )
-  :Do(function(_,_)
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",{ requestType = "HTTP", fileName = "PolicyTableUpdate" })
-
-      EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "HTTP", fileType = "JSON"}):Timeout(12000)
-      :Do(function(_,_) time_system_request[#time_system_request + 1] = timestamp() end)
-      EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATING"}, {status = "UPDATE_NEEDED"}):Times(2):Timeout(time_wait)
-      :Do(function(_,data)
-          if(data.params.status == "UPDATE_NEEDED" ) then
-            --first retry sequence
-            verify_retry_sequence()
-          end
-        end)
-    end)
-
-  commonTestCases:DelayedExp(time_wait)
-
-  if(is_test_fail == true) then
-    self:FailTestCase("Test is FAILED. See prints.")
+function Test:TestStep_Validate_Status()
+  if not is_table_equal(r_expected_status, r_actual_status) then
+    local msg = table.concat({
+        "\nExpected statuses:\n", commonFunctions:convertTableToString(r_expected_status, 1),
+        "\nActual:\n", commonFunctions:convertTableToString(r_actual_status, 1)})
+    self:FailTestCase(msg)
   end
+end
+
+function Test.Test_ShowSequence()
+  show_log()
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
+
 function Test.Postcondition_Stop_SDL()
   StopSDL()
 end
