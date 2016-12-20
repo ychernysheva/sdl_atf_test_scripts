636705db89821637e1db751a8f22ce9fae02f5a4
Anna Rotar (GitHub) <ARotar@luxoft.com>
2016-12-20 10:52:49 +0200

    Update 029_ATF_P_TC_Notifying_HMI_via_OnAppPermissionChanged.lua

--- test_scripts/Polices/Policy_Table_Update/029_ATF_P_TC_Notifying_HMI_via_OnAppPermissionChanged.lua
+++ test_scripts/Polices/Policy_Table_Update/029_ATF_P_TC_Notifying_HMI_via_OnAppPermissionChanged.lua
@@ -3,33 +3,34 @@
 -- [PolicyTableUpdate] Notifying HMI via OnAppPermissionChanged about the affected application
 --
 -- Description:
--- PoliciesManager must initiate sending SDL.OnAppPermissionChanged{appID}
--- notification to HMI IN CASE the Updated PT resulted any changes in the appID app`s policies.
--- Pre-conditions:
--- a. SDL and HMI are running
--- b. AppID_1 is connected to SDL.
--- c. The device the app is running on is consented
--- d. Policy Table Update procedure is on stage waiting for:
--- HMI->SDL: SDL.OnReceivedPolicyUpdate (policyfile)
--- e. 'policyfile' corresponds to PTU validation rules
+-- PoliciesManager must initiate sending SDL.OnAppPermissionChanged{appID} notification to HMI IN CASE the Updated PT resulted any changes in the appID app`s policies.
+-- Preconditions:
+--    1.SDL and HMI are running
+--    2.AppID_1 is connected to SDL.
+--    3.The device the app is running on is consented
+--    4.Policy Table Update procedure is on stage waiting for:
+--        HMI->SDL: SDL.OnReceivedPolicyUpdate (policyfile)
+--        'policyfile' corresponds to PTU validation rules
+-- Steps:
+--    Request policy update via HMI:
+--        HMI->SDL: SDL.OnReceivedPolicyUpdate (policyfile)
 
--- Action:
--- HMI->SDL: SDL.OnReceivedPolicyUpdate (policyfile)
-
--- Expected:
--- 1. PoliciesManager validates the updated PT (policyFile) e.i. verifyes, saves the updated fields and everything that is defined with related requirements)
--- 2. On validation success:
--- SDL->HMI:OnStatusUpdate("UP_TO_DATE")
--- 3. SDL replaces the following sections of the Local Policy Table with the corresponding sections from PTU:
--- module_config,
--- functional_groupings,
--- app_policies
--- 4. SDL removes 'policyfile' from the directory
--- 5. SDL->app: onPermissionChange(<permisssionItem>)
--- 6. SDL->HMI: SDL.OnAppPermissionChanged(<appID_1>, params)
-
+-- Expected result:
+--    1.PoliciesManager validates the updated PT (policyFile) e.i. verifyes, saves the updated fields and everything that is defined with related requirements)
+--    2.On validation success:
+--        SDL->HMI:OnStatusUpdate("UP_TO_DATE")
+--    3.SDL replaces the following sections of the Local Policy Table with the corresponding sections from PTU:
+--        module_config,
+--        functional_groupings,
+--        app_policies
+--    4.SDL removes 'policyfile' from the directory
+--    5.SDL->app: onPermissionChange(<permisssionItem>)
+--    6.SDL->HMI: SDL.OnAppPermissionChanged(<appID_1>, params)
+---------------------------------------------------------------------------------------------
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+--ToDo: shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+config.defaultProtocolVersion = 2
 
 --[[ Required Shared libraries ]]
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
@@ -38,9 +39,7 @@ local testCasesForPolicyTable = require('user_modules/shared_testcases/testCases
 local json = require('json')
 
 --[[ Local Variables ]]
--- Basic PTU file
 local basic_ptu_file = "files/ptu.json"
--- PTU for registered app
 local ptu_app_registered = "files/ptu_app.json"
 
 -- Prepare parameters for app to save it in json file
@@ -79,6 +78,12 @@ commonFunctions:newTestCasesGroup("Preconditions")
 function Test.Precondition_StopSDL()
   StopSDL()
 end
+
+function Test.Precondition_Delete_LogsAndPT()
+  commonSteps:DeleteLogsFiles()
+  commonSteps:DeletePolicyTable()
+end
+
 function Test.Precondition_StartSDL()
   StartSDL(config.pathToSDL, config.ExitOnCrash)
 end
@@ -102,24 +107,20 @@ end
 function Test.Precondition_PreparePTData()
   PrepareJsonPTU1(config.application1.registerAppInterfaceParams.appID, ptu_app_registered)
 end
---[[ end of Preconditions ]]
 
---[[ Test ]]
-commonFunctions:newTestCasesGroup("Test")
-function Test:RegisterApp()
+function Test:Precondition_RegisterApp()
   self.mobileSession:StartService(7)
   :Do(function (_,_)
       local correlationId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
-
       EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered")
       EXPECT_RESPONSE(correlationId, { success = true })
       EXPECT_NOTIFICATION("OnPermissionsChange")
     end)
 end
 
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
 function Test:UpdatePolicy_ExpectOnAppPermissionChangedWithAppID()
-  EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
-
   testCasesForPolicyTable:updatePolicyInDifferentSessions(Test, ptu_app_registered,
     config.application1.registerAppInterfaceParams.appName,
     self.mobileSession)
@@ -136,12 +137,10 @@ end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-
 function Test.Postcondition_RemovePTUfiles()
   os.remove(ptu_app_registered)
 end
+
 function Test.Postcondition_Stop_SDL()
   StopSDL()
 end
-
-return Test
