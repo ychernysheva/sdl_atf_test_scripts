cfaf294a8b44dff2821cc13823862d8cf750bad5
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2016-12-30 13:45:23 +0200

    Fix issues in script

--- test_scripts/Polices/Policy_Table_Update/050_ATF_P_User_Trigger_PTU_While_Another_Is_In_Progress.lua
+++ test_scripts/Polices/Policy_Table_Update/050_ATF_P_User_Trigger_PTU_While_Another_Is_In_Progress.lua
@@ -57,96 +57,57 @@ function Test:Precondition_Activate_App_And_Consent_Device_To_Start_PTU()
           :Do(function(_,data)
               self.hmiConnection:SendResponse(data.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
               EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN"})
+              :Do(function()
+
+                end)
             end)
           EXPECT_NOTIFICATION("OnPermissionsChange", {})
         end)
     end)
+  EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
 end
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 function Test:TestStep_Start_Update_First_Time_And_Trigger_New_PTU_Via_UpdateSDL()
-
-  local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {{url = "http://policies.telematics.ford.com/api/policies"}}}})
-  :Do(function()
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest", { requestType = "PROPRIETARY", fileName = "filename"} )
-      EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "PROPRIETARY" })
-
-      -- Reuest for new PTU
+  local RequestId_GetUrls = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
+  EXPECT_HMIRESPONSE(RequestId_GetUrls,{result = {code = 0, method = "SDL.GetURLS" } } )
+  :Do(function(_,_)
+      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",{ fileName = "PolicyTableUpdate", requestType = "PROPRIETARY"})
+      EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "PROPRIETARY", fileType = "JSON" } )
+      :Do(function(_,_)
+        -- Reuest for new PTU
       local RequestIdUpdateSDL = self.hmiConnection:SendRequest("SDL.UpdateSDL")
       EXPECT_HMIRESPONSE(RequestIdUpdateSDL,{result = {code = 0, method = "SDL.UpdateSDL"}})
 
-      local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest",
-        {
-          fileName = "PolicyTableUpdate",
-          requestType = "PROPRIETARY"
-        }, "files/ptu_general.json")
-      local systemRequestId
-      EXPECT_HMICALL("BasicCommunication.SystemRequest")
-      :Do(function(_,data)
-          systemRequestId = data.id
-          self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate", { policyfile = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate" })
+          local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest", {requestType = "PROPRIETARY", fileName = "PolicyTableUpdate"},
+            "files/ptu_general.json")
 
-          local function to_run()
-            self.hmiConnection:SendResponse(systemRequestId,"BasicCommunication.SystemRequest", "SUCCESS", {})
-          end
-          RUN_AFTER(to_run, 800)
+          EXPECT_HMICALL("BasicCommunication.SystemRequest",{
+              requestType = "PROPRIETARY",
+              fileName = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate"})
+          :Do(function(_,_data1)
+              self.hmiConnection:SendResponse(_data1.id,"BasicCommunication.SystemRequest", "SUCCESS", {})
+              self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
+                { policyfile = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate"})
 
-          self.mobileSession:ExpectResponse(CorIdSystemRequest, {success = true, resultCode = "SUCCESS"})
-          -- New PTU is triggered
-          :Do(function(_,_)
-
-            EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
-            :Do(function(_,_data1) self.hmiConnection:SendResponse(_data1.id, _data1.method, "SUCCESS", {}) end)
-
-            EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
-            :Do(function() end)
-          end)
-
-          EXPECT_NOTIFICATION("OnPermissionsChange", {})
+              EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"})
+            end)
+          EXPECT_RESPONSE(CorIdSystemRequest, { success = true, resultCode = "SUCCESS"})
         end)
     end)
 end
 
-function Test:TestStep_Second_Successful_Policy_Update()
-  local RequestIdGetURLS = self.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(RequestIdGetURLS,{result = {code = 0, method = "SDL.GetURLS", urls = {{url = "http://policies.telematics.ford.com/api/policies"}}}})
+function Test:Test_Verify_New_PTU_is_Triggered()
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", { status = "UPDATE_NEEDED" })
   :Do(function()
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
-        {
-          requestType = "PROPRIETARY",
-          fileName = "filename"
-        }
-      )
-      EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "PROPRIETARY" })
-      local CorIdSystemRequest = self.mobileSession:SendRPC("SystemRequest",
-        {
-          fileName = "PolicyTableUpdate",
-          requestType = "PROPRIETARY"
-        }, "files/ptu_general.json")
-      local systemRequestId
-      EXPECT_HMICALL("BasicCommunication.SystemRequest")
-      :Do(function(_,data)
-          systemRequestId = data.id
-          self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
-            {
-              policyfile = "/tmp/fs/mp/images/ivsu_cache/PolicyTableUpdate"
-            })
-          local function to_run()
-            self.hmiConnection:SendResponse(systemRequestId,"BasicCommunication.SystemRequest", "SUCCESS", {})
-          end
-          RUN_AFTER(to_run, 800)
-          self.mobileSession:ExpectResponse(CorIdSystemRequest, {success = true, resultCode = "SUCCESS"})
-          EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UP_TO_DATE"})
-        end)
-
-    end)
+    EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
+  end)
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
-testCasesForPolicyTable:Restore_preloaded_pt()
+
 function Test.Postcondition_Stop()
   StopSDL()
 end
