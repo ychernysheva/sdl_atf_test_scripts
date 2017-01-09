73bc6e82081118e3080e0c4b6e269a8ab56d55fe
istoimenova <istoimenova@luxoft.com>
2017-01-09 10:34:33 +0200

    Script is ready for review.

---
+++ test_scripts/API/RAI/048_ATF_N_HMI_Does_Not_Respond_WRONG_LANGUAGE_RAI_steeringWheelLocation.lua
@@ -0,0 +1,97 @@
+---------------------------------------------------------------------------------------------
+-- Requirement summary:
+-- [RegisterAppInterface] SDL must send "steeringWheelLocation" param to each app in response
+-- [GetCapabilities] Conditions for SDL to store value of "steeringWheelLocation" param
+-- [MOBILE_API] [HMI_API] The 'steeringWheelLocation' enum
+-- [MOBILE_API] The 'steeringWheelLocation' param
+-- [HMI_API] The 'steeringWheelLocation' parameter
+-- [RegisterAppInterface] WRONG_LANGUAGE languageDesired
+--
+-- Description:
+-- In case any SDL-enabled app sends RegisterAppInterface_request to SDL
+-- and SDL has the value of "steeringWheelLocation" stored internally
+-- SDL must provide this value of "steeringWeelLocation" via RegisterAppInterface_response to mobile app
+--
+-- 1. Used preconditions
+-- In InitHMI_OnReady HMI does not reply to UI.GetCapabilities
+--
+-- 2. Performed steps
+-- Register new applications with conditions for result WRONG_LANGUAGE
+--
+-- Expected result:
+-- SDL->mobile: RegisterAppInterface_response(WRONG_LANGUAGE, success: true) 
+-- steeringWeelLocation is equal to "HMI_capabilities.json"
+---------------------------------------------------------------------------------------------
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
+local testCasesForRAI = require('user_modules/shared_testcases/testCasesForRAI')
+local events = require("events")
+local mobile_session = require('mobile_session')
+
+--[[ Local variables ]]
+local value_steering_wheel_location = testCasesForRAI.get_data_steeringWheelLocation()
+
+--[[ General Precondition before ATF start ]]
+commonSteps:DeleteLogsFiles()
+commonSteps:DeletePolicyTable()
+testCasesForRAI:Connecttest_onlywith_StartSDL_InitHMIOnReady("connecttest_initHMI.lua")
+--TODO(istoimenova): shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
+config.defaultProtocolVersion = 2
+
+--[[ General Settings for configuration ]]
+Test = require('user_modules/connecttest_initHMI')
+require('user_modules/AppTypes')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup("Preconditions")
+
+function Test:Precondition_InitHMI_OnReady()
+	testCasesForRAI:InitHMI_onReady_without_UI_GetCapabilities(self)
+	local event = events.Event()
+	event.level = 2
+	event.matches = function(_, data) return data.method == "UI.GetCapabilities" end
+
+	EXPECT_HMIEVENT(event, "UI.GetCapabilities")
+	-- HMI does not reply to UI.GetCapabilities
+end
+
+function Test:Precondition_connectMobile()
+	self:connectMobile()
+end
+
+function Test:Precondition_StartSession()
+	self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+	self.mobileSession:StartService(7)
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+
+function Test:TestStep_RAI_RESUME_FAILED_steeringWheelLocation()
+	local CorIdRegister = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+	config.application1.registerAppInterfaceParams.languageDesired = "RU-RU"
+
+	EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application1.registerAppInterfaceParams.appName }})
+	--TODO(istoimenova): SDL returns SUCCESS, should be checked on propriate version
+	EXPECT_RESPONSE(CorIdRegister, { success = true, resultCode = "WRONG_LANGUAGE", steeringWheelLocation = value_steering_wheel_location })
+	EXPECT_NOTIFICATION("OnHMIStatus", {systemContext="MAIN", hmiLevel="NONE", audioStreamingState="NOT_AUDIBLE"} )
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+
+function Test.Postcondition_Restore_hmi_capabilities()
+	commonPreconditions:RestoreFile("hmi_capabilities.json")
+end
+
+function Test.Postcondition_Stop()
+  StopSDL()
+end
+
+return Test
