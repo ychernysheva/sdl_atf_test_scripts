b02382f15789b2cd4ab48075caf7abaa9c85a36f
Anna Rotar (GitHub) <ARotar@luxoft.com>
2017-01-10 10:23:56 +0200

    Update ATF_AddSubMenu_Negative_SubMenuIcon_OutUpperBound.lua

--- test_scripts/API/AddSubMenu/ATF_AddSubMenu_Negative_SubMenuIcon_OutUpperBound.lua
+++ test_scripts/API/AddSubMenu/ATF_AddSubMenu_Negative_SubMenuIcon_OutUpperBound.lua
@@ -1,26 +1,26 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
---		[GENIVI] AddSubMenu: SDL must support new "subMenuIcon" parameter 
---		[GeneralResultCodes] INVALID_DATA out of bounds
+--	[GENIVI] AddSubMenu: SDL must support new "subMenuIcon" parameter
+--	[GeneralResultCodes] INVALID_DATA out of bounds
 --
 -- Description:
--- 		Mobile app sends AddSubMenu with "subMenuIcon" with value is out upper bound
+-- 	Mobile app sends AddSubMenu with "subMenuIcon" with value is out upper bound
 -- 1. Used preconditions:
--- 		Delete files and policy table from previous ignition cycle if any
--- 		Start SDL and HMI
+-- 	Delete files and policy table from previous ignition cycle if any
+-- 	Start SDL and HMI
 --      Activate application
 -- 2. Performed steps:
--- 		Send AddSubMenu RPC with "subMenuIcon"  value is out upper bound
+-- 	Send AddSubMenu RPC with "subMenuIcon"  value is out upper bound
 --
 -- Expected result:
--- 		SDL must respond with INVALID_DATA and "success":"false"
+-- 	SDL must respond with INVALID_DATA and "success":"false"
 ---------------------------------------------------------------------------------------------
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 config.SDLStoragePath = config.pathToSDL .. "storage/"
 
 --[[ General configuration parameters ]]
-Test = require('connecttest')	
+Test = require('connecttest')
 require('cardinalities')
 
 --[[ Required Shared Libraries ]]
@@ -30,54 +30,53 @@ require('user_modules/AppTypes')
 
 --[[ Local variables ]]
 local strOutofBoundFileName = string.rep("a", 65532) .. ".png" --maxlength="65535"
-	
+
 --[[ Preconditions ]]
 commonSteps:DeleteLogsFileAndPolicyTable()
 commonFunctions:newTestCasesGroup("Preconditions")
 function Test:Precondition_ActivateApp()
-	local RequestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications["Test Application"]})
-	EXPECT_HMIRESPONSE(RequestId)
-	:Do(function(_,data)
-		if data.result.isSDLAllowed ~= true then
-			local RequestIdGetMes = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", 
-			{language = "EN-US", messageCodes = {"DataConsent"}})
-			EXPECT_HMIRESPONSE(RequestIdGetMes)
-			:Do(function()
-				self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality", 
-				{allowed = true, source = "GUI", device = {id = config.deviceMAC, name = "127.0.0.1"}})
-				EXPECT_HMICALL("BasicCommunication.ActivateApp")
-				:Do(function(_,data)
-					self.hmiConnection:SendResponse(data.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
-				end)
-				:Times(AtLeast(1)) 
-			end)
-		end
-	end)
-	EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN", audioStreamingState = "AUDIBLE"})	
+  local RequestId = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications["Test Application"]})
+  EXPECT_HMIRESPONSE(RequestId)
+  :Do(function(_,data)
+  if data.result.isSDLAllowed ~= true then
+    local RequestIdGetMes = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage",
+    {language = "EN-US", messageCodes = {"DataConsent"}})
+    EXPECT_HMIRESPONSE(RequestIdGetMes)
+    :Do(function()
+    self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality",
+    {allowed = true, source = "GUI", device = {id = config.deviceMAC, name = "127.0.0.1"}})
+    EXPECT_HMICALL("BasicCommunication.ActivateApp")
+    :Do(function(_,data)
+    self.hmiConnection:SendResponse(data.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
+    end)
+    :Times(AtLeast(1))
+    end)
+  end
+  end)
+  EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN", audioStreamingState = "AUDIBLE"})
 end
-
 commonSteps:PutFile("PutFile_strOutofBoundFileName", strOutofBoundFileName)
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 function Test:AddSubMenu_SubMenuIconOutUpperBound()
-	local cid = self.mobileSession:SendRPC("AddSubMenu",
-		{
-			menuID = 2000,
-			position = 200,
-			menuName ="SubMenu", 
-			subMenuIcon =
-			    {
-					imageType = "DYNAMIC",
-					value = strOutofBoundFileName } 
-		})	
-	EXPECT_RESPONSE(cid, { success = false, resultCode = "INVALID_DATA" }) 
-	EXPECT_NOTIFICATION("OnHashChange")
-	:Times(0)
-end
+  local cid = self.mobileSession:SendRPC("AddSubMenu",
+  {
+    menuID = 2000,
+    position = 200,
+    menuName ="SubMenu",
+    subMenuIcon =
+    {
+      imageType = "DYNAMIC",
+      value = strOutofBoundFileName }
+    })
+    EXPECT_RESPONSE(cid, { success = false, resultCode = "INVALID_DATA" })
+    EXPECT_NOTIFICATION("OnHashChange")
+    :Times(0)
+  end
 
---[[ Postconditions ]]
-commonFunctions:newTestCasesGroup("Postconditions")
-function Test.Postcondition_StopSDL()
-  StopSDL()
-end
+  --[[ Postconditions ]]
+  commonFunctions:newTestCasesGroup("Postconditions")
+  function Test.Postcondition_StopSDL()
+    StopSDL()
+  end
