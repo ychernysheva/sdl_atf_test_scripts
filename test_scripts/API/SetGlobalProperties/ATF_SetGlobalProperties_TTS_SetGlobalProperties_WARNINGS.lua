65d2cb603308077657241cc8d511677468dc0e20
valeriia <vvvakulenko@luxoft.com>
2017-01-11 17:59:20 +0200

    Update script

--- test_scripts/API/SetGlobalProperties/ATF_SetGlobalProperties_TTS_SetGlobalProperties_WARNINGS.lua
+++ test_scripts/API/SetGlobalProperties/ATF_SetGlobalProperties_TTS_SetGlobalProperties_WARNINGS.lua
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
--- SDL must send WARNINGS (success:true) to mobile app in case HMI respond WARNINGS at least to one component of RPC
+-- SDL must send WARNINGS (success:true) to mobile app in case HMI respond WARNINGS at least to one HMI-portions
 --
 -- Description:
 -- test is intended to check that SDL sends WARNINGS (success:true) to mobile app in case HMI respond WARNINGS to at least one HMI-portions
@@ -17,12 +17,15 @@
 
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+config.SDLStoragePath = config.pathToSDL .. "storage/"
 
 --[[ Required Shared libraries ]]
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require ('user_modules/shared_testcases/commonSteps')
 
 --[[ Local Variables ]]
-local storagePath = config.pathToSDL .. "storage/"
+local storagePath = config.SDLStoragePath..config.application1.registerAppInterfaceParams.appID.. "_" .. config.deviceMAC.. "/"
+local ServerAddress = commonFunctions.read_parameter_from_smart_device_link_ini("ServerAddress")
 
 --[[ General Precondition before ATF start ]]
 commonFunctions:SDLForceStop()
@@ -34,21 +37,20 @@ require('user_modules/AppTypes')
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
 
-function Test:Precondition_ActivationApp()			
-  local request_id = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications["Test Application"]})
+function Test:Precondition_ActivationApp()
+  local request_id = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications[config.application1.registerAppInterfaceParams.appName]})
   EXPECT_HMIRESPONSE(request_id)
   :Do(function(_,data)
-    if
-    data.result.isSDLAllowed ~= true then
+    if (data.result.isSDLAllowed ~= true) then
       local request_id1 = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = {"DataConsent"}})
       EXPECT_HMIRESPONSE(request_id1)
-      :Do(function(_,_)						
-        self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality", {allowed = true, source = "GUI", device = {id = config.deviceMAC, name = "127.0.0.1"}})
+      :Do(function(_,_)   
+        self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality", {allowed = true, source = "GUI", device = {id = config.deviceMAC, name = ServerAddress}})
         EXPECT_HMICALL("BasicCommunication.ActivateApp")
         :Do(function(_,_)
           self.hmiConnection:SendResponse(data.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
         end)
-        :Times(2)
+        :Times(1)
       end)
     end
   end)
@@ -56,7 +58,9 @@ function Test:Precondition_ActivationApp()
 end
 
 --[[ Test ]]
-function Test:SetGlobalProperties_PositiveCase_SUCCESS()
+commonFunctions:newTestCasesGroup("Test")
+
+function Test:TestSTep_SetGlobalProperties_WARNINGS()
   --mobile side: sending SetGlobalProperties request
   local cid = self.mobileSession:SendRPC("SetGlobalProperties",
   {
@@ -106,8 +110,9 @@ function Test:SetGlobalProperties_PositiveCase_SUCCESS()
         text = "Help prompt",
         type = "TEXT"
     }},
-    appID = self.applications["Test Application"]
+    appID = self.applications[config.application1.registerAppInterfaceParams.appName]
   })
+
   :Do(function(_,data)
     self.hmiConnection:SendResponse(data.id, "TTS.SetGlobalProperties", "UNSUPPORTED_RESOURCE", {})
   end)
