494ec52f78a5f7bfa907974fc76a802c8cf5c2ac
maria_mih <mmihaylova-banska@luxoft.com>
2017-01-11 10:33:44 +0200

    Function for upper bound added

--- user_modules/shared_testcases/testCasesForPerformAudioPassThru.lua
+++ user_modules/shared_testcases/testCasesForPerformAudioPassThru.lua
@@ -14,8 +14,14 @@ local testCasesForPerformAudioPassThru = {}
 function testCasesForPerformAudioPassThru:Check_audioPassThruIcon_Existence(self)
   --print("PathToAppFolder = "..PathToAppFolder)
   local result = commonSteps:file_exists(PathToAppFolder .. "icon.png")
+  local result1 = commonSteps:file_exists(PathToAppFolder .. string.rep ("a", 251).. ".png")
+  local result2 = commonSteps:file_exists(PathToAppFolder .. "")
   if(result == true) then
     print("audioPassThruIcon exists at application's sandbox")
+  elseif (result1 == true) then
+    print("audioPassThruIcon exists at application's sandbox") 
+  elseif (result2 == true) then
+    print("audioPassThruIcon exists at application's sandbox") 
   else
     self:FailTestCase ("audioPassThruIcon does not exist at application's sandbox!")
   end
@@ -66,12 +72,9 @@ function testCasesForPerformAudioPassThru:PerformAudioPassThru_AllParameters_SUC
       audioType = "PCM",
       muteAudio = true,
       audioPassThruIcon =
-      {
         { value = "icon.png",
           imageType = "STATIC"
         }
-
-      }
     })
 
   -- hmi expects TTS.Speak request
@@ -107,6 +110,93 @@ function testCasesForPerformAudioPassThru:PerformAudioPassThru_AllParameters_SUC
       maxDuration = 2000,
       muteAudio = true,
       audioPassThruIcon = { imageType = "STATIC", value = "icon.png"}
+
+    })
+  :Do(function(_,data)
+      local function UIPerformAoudioResponce()
+        self.hmiConnection:SendResponse(data.id, "UI.PerformAudioPassThru", "SUCCESS", {})
+      end
+
+      RUN_AFTER(UIPerformAoudioResponce, 1500)
+    end)
+
+  if
+  self.appHMITypes["NAVIGATION"] == true or
+  self.appHMITypes["COMMUNICATION"] == true or
+  self.isMediaApplication == true then
+    --mobile side: expect OnHMIStatus notification
+    EXPECT_NOTIFICATION("OnHMIStatus",
+      {hmiLevel = "FULL", audioStreamingState = "ATTENUATED", systemContext = "MAIN"},
+      {hmiLevel = "FULL", audioStreamingState = "AUDIBLE", systemContext = "MAIN"})
+    :Times(2)
+  else
+    EXPECT_NOTIFICATION("OnHMIStatus")
+    :Times(0)
+  end
+
+  self.mobileSession:ExpectResponse(CorIdPerformAudioPassThruAppParVD, { success = true, resultCode = "SUCCESS",
+    })
+
+  commonTestCases:DelayedExp(1500)
+
+end
+
+function testCasesForPerformAudioPassThru:PerformAudioPassThru_AllParameters_Upper_SUCCESS(self)
+  local CorIdPerformAudioPassThruAppParVD= self.mobileSession:SendRPC("PerformAudioPassThru",
+    {
+ 	  initialPrompt =
+				      {
+				        {
+				          text = string.rep("a", 500),
+				          type = "TEXT",
+				        }
+				      },
+      audioPassThruDisplayText1 = string.rep("a", 500),
+      audioPassThruDisplayText2 = string.rep("a", 500),
+      samplingRate = "44KHZ",
+      maxDuration = 1000000,
+      bitsPerSample = "16_BIT",
+      audioType = "PCM",
+      muteAudio = true,
+      audioPassThruIcon =
+        { value = string.rep ("a", 251).. ".png",
+          imageType = "STATIC"
+        }
+    })
+
+  -- hmi expects TTS.Speak request
+  EXPECT_HMICALL("TTS.Speak",
+    {
+      speakType = "AUDIO_PASS_THRU",
+      ttsChunks = { { text = string.rep("a", 500), type = "TEXT" } },
+      appID = self.applications[applicationName]
+    })
+  :Do(function(_,data)
+      -- send notification to start TTS.Speak
+      self.hmiConnection:SendNotification("TTS.Started",{ })
+
+      -- HMI sends TTS.Speak SUCCESS
+      local function ttsSpeakResponse()
+        self.hmiConnection:SendResponse (data.id, data.method, "SUCCESS", {})
+
+        -- HMI sends TTS.Stop
+        self.hmiConnection:SendNotification("TTS.Stopped")
+      end
+
+      RUN_AFTER(ttsSpeakResponse, 1000)
+    end)
+
+  -- hmi expects UI.PerformAudioPassThru request
+  EXPECT_HMICALL("UI.PerformAudioPassThru",
+    {
+      appID = self.applications[applicationName],
+      audioPassThruDisplayTexts = {
+        {fieldName = "audioPassThruDisplayText1", fieldText = string.rep("a", 500)},
+        {fieldName = "audioPassThruDisplayText2", fieldText = string.rep("a", 500)},
+      },
+      maxDuration = 1000000,
+      muteAudio = true,
+      audioPassThruIcon = { imageType = "STATIC", value = string.rep ("a", 251).. ".png"}
 
     })
   :Do(function(_,data)
