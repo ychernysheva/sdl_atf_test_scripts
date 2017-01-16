6467163a4267fb889ee6944948a7511d6fe3de25
valeriia <vvvakulenko@luxoft.com>
2017-01-16 15:05:41 +0200

    Update script due to comments of review iteration #1

--- test_scripts/API/DeleteCommand/ATF_DeleteCommand_VR_DeleteCommand_WARNINGS.lua
+++ test_scripts/API/DeleteCommand/ATF_DeleteCommand_VR_DeleteCommand_WARNINGS.lua
@@ -1,18 +1,18 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
--- SDL must send WARNINGS (success:true) to mobile app in case HMI respond WARNINGS at least to one HMI-portions
+-- SDL must send WARNINGS (success:true) to mobile app in case HMI respond WARNINGS at least to one component of RPC
 --
 -- Description:
 -- test is intended to check that SDL sends WARNINGS (success:true) to mobile app in case HMI respond WARNINGS to at least one HMI-portions
 -- in this test case when VR.DeleteCommand gets WARNINGS is checked
--- 1. Used preconditions: App is activated and registered SUCESSFULLY
+-- 1. Used preconditions: App is registered and activated SUCCESSFULLY. AddCommand is sent SUCCESSFULLY
 -- 2. Performed steps: 
 -- MOB -> SDL: sends DeleteCommand
--- SDL -> HMI: resends VR.DeleteCommand with imageType of cmdIcon which is not supported by HMI and UI.DeleteCommand with all valid params
+-- SDL -> HMI: resends VR.DeleteCommand
 -- HMI -> SDL: VR.DeleteCommand (WARNINGS), UI.DeleteCommand (SUCCESS)
 --
 -- Expected result:
--- SDL -> MOB: appID: (WARNINGS, success: true: DeleteCommand
+-- SDL -> MOB: DeleteCommand: (resultcode: WARNINGS, success: true)
 ---------------------------------------------------------------------------------------------
 
 --[[ General configuration parameters ]]
@@ -25,10 +25,12 @@ local commonSteps = require ('user_modules/shared_testcases/commonSteps')
 
 --[[ Local Variables ]]
 local storagePath = config.SDLStoragePath..config.application1.registerAppInterfaceParams.appID.. "_" .. config.deviceMAC.. "/"
-local ServerAddress = commonFunctions.read_parameter_from_smart_device_link_ini("ServerAddress")
+local ServerAddress = commonFunctions:read_parameter_from_smart_device_link_ini("ServerAddress")
 
 --[[ General Precondition before ATF start ]]
 commonFunctions:SDLForceStop()
+commonSteps:DeleteLogsFiles()
+commonSteps:DeletePolicyTable()
 
 --[[ General Settings for configuration ]]
 Test = require('connecttest')
@@ -47,47 +49,20 @@ function Test:Precondition_ActivationApp()
       :Do(function(_,_)   
         self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality", {allowed = true, source = "GUI", device = {id = config.deviceMAC, name = ServerAddress}})
         EXPECT_HMICALL("BasicCommunication.ActivateApp")
-        :Do(function(_,_)
-          self.hmiConnection:SendResponse(data.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
+        :Do(function(_,data1)
+          self.hmiConnection:SendResponse(data1.id,"BasicCommunication.ActivateApp", "SUCCESS", {})
         end)
-        :Times(1)
       end)
     end
   end)
   EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN"}) 
 end
 
-commonSteps:PutFile("Precondition_PutFile", "icon.png")
-
-function Test:Precondition_AddSubMenu()
-  --mobile side: sending AddSubMenu request
-  local cor_id_add_submenu = self.mobileSession:SendRPC("AddSubMenu",
-  {
-    menuID = 1000,
-    position = 0,
-    menuName ="Commandpositive"
-  })
-  --hmi side: expect UI.AddSubMenu request
-  EXPECT_HMICALL("UI.AddSubMenu", 
-  { 
-    menuID = 1000,
-    menuParams = {
-      position = 0,
-      menuName ="Commandpositive"
-    }
-  })
-  :Do(function(_,data)
-    --hmi side: sending UI.AddSubMenu response
-    self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
-  end)    
-  --mobile side: expect AddSubMenu response
-  EXPECT_RESPONSE(cor_id_add_submenu, { success = true, resultCode = "SUCCESS" })
-  --mobile side: expect OnHashChange notification
-  EXPECT_NOTIFICATION("OnHashChange")
-end
+function Test.Precondition_PutFile()
+  commonSteps:PutFile("Precondition_PutFile", "icon.png")
+end 
 
 function Test:Precondition_AddCommand()
-  --mobile side: sending AddCommand request
   local cid_add_cmd = self.mobileSession:SendRPC("AddCommand",
   {
     cmdID = 11,
@@ -108,7 +83,7 @@ function Test:Precondition_AddCommand()
       imageType ="DYNAMIC"
     }
   })
-  --hmi side: expect UI.AddCommand request
+
   EXPECT_HMICALL("UI.AddCommand", 
   { 
     cmdID = 11,
@@ -124,23 +99,29 @@ function Test:Precondition_AddCommand()
       menuName ="Commandpositive"
     }
   })
+
   :Do(function(_,data)
-    --hmi side: sending UI.AddCommand response
     self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
   end)  
-  --hmi side: expect VR.AddCommand request
+
   EXPECT_HMICALL("VR.AddCommand", 
   { 
     cmdID = 11,
     type = "Command",
-    vrCommands = 
-    {
-      "VRCommandonepositive", 
-      "VRCommandonepositivedouble"
-    }
+    vrCommands = { "VRCommandonepositive", "VRCommandonepositivedouble" }
   })
-  :Do(function(_,data)
-    self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+
+  :ValidIf(function(_,data)
+    local value_Icon = storagePath .. "action.png"
+    if (string.match(data.params.cmdIcon.value, "%S*" .. "("..string.sub(storagePath, 2).."action.png)" .. "$") == nil ) then
+      print("\27[31m value of menuIcon is WRONG. Expected: ~".. value_Icon .. "; Real: " .. data.params.cmdIcon.value .. "\27[0m")
+    return false
+    else
+    return true
+    end
+  end)
+
+  :Do(function(_,data) self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
   end)
   
   EXPECT_RESPONSE(cid_add_cmd, { success = true, resultCode = "SUCCESS" })
@@ -151,22 +132,20 @@ end
 commonFunctions:newTestCasesGroup("Test")
 
 function Test:TestStep_DeleteCommand_warning()
-  --mobile side: sending DeleteCommand request
   local cid_del_cmd = self.mobileSession:SendRPC("DeleteCommand",
   {
     cmdID = 11
   })  
-  --hmi side: expect UI.DeleteCommand request
+
   EXPECT_HMICALL("UI.DeleteCommand", 
   { 
     cmdID = 11,
     appID = self.applications["Test Application"]
   })
   :Do(function(_,data)
-    --hmi side: sending UI.DeleteCommand response
     self.hmiConnection:SendResponse(data.id, "UI.DeleteCommand", "SUCCESS", {})
   end)  
-  --hmi side: expect VR.DeleteCommand request
+
   EXPECT_HMICALL("VR.DeleteCommand", 
   { 
     cmdID = 11,
@@ -174,7 +153,6 @@ function Test:TestStep_DeleteCommand_warning()
     appID = self.applications[config.application1.registerAppInterfaceParams.appName]
   })
   :Do(function(_,data)
-    --hmi side: sending VR.DeleteCommand response
     self.hmiConnection:SendResponse(data.id, "VR.DeleteCommand", "WARNINGS", {})
   end)        
   EXPECT_RESPONSE(cid_del_cmd, { success = true, resultCode = "WARNINGS" })
