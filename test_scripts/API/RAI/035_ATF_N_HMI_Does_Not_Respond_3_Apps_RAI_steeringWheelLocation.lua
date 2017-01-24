3ac94863a075f5e31f00e8f401caeb364031771e
Iliyana Stoimenova(GitHub) <istoimenova@luxoft.com>
2017-01-24 09:10:28 +0200

    Update 035_ATF_N_HMI_Does_Not_Respond_3_Apps_RAI_steeringWheelLocation.lua
    Remove testCasesForRAI:Connecttest_onlywith_StartSDL_InitHMIOnReady, because using of static user_modules/connecttest_initHMI;
    update call of InitHMI_onReady_without_UI_GetCapabilities

--- test_scripts/API/RAI/035_ATF_N_HMI_Does_Not_Respond_3_Apps_RAI_steeringWheelLocation.lua
+++ test_scripts/API/RAI/035_ATF_N_HMI_Does_Not_Respond_3_Apps_RAI_steeringWheelLocation.lua
@@ -38,7 +38,7 @@ local value_steering_wheel_location = testCasesForRAI.get_data_steeringWheelLoca
 --[[ General Precondition before ATF start ]]
 commonSteps:DeleteLogsFiles()
 commonSteps:DeletePolicyTable()
-testCasesForRAI:Connecttest_onlywith_StartSDL_InitHMIOnReady("connecttest_initHMI.lua")
+
 --TODO(istoimenova): shall be removed when issue: "ATF does not stop HB timers by closing session and connection" is fixed
 config.defaultProtocolVersion = 2
 
@@ -50,7 +50,7 @@ require('user_modules/AppTypes')
 commonFunctions:newTestCasesGroup("Preconditions")
 
 function Test:Precondition_InitHMI_OnReady()
-	testCasesForRAI:InitHMI_onReady_without_UI_GetCapabilities(self)
+	testCasesForRAI.InitHMI_onReady_without_UI_GetCapabilities(self)
 	local event = events.Event()
 	event.level = 2
 	event.matches = function(_, data) return data.method == "UI.GetCapabilities" end
