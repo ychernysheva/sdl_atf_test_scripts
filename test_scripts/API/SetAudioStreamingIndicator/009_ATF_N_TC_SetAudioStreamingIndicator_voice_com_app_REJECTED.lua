a21a76f4355f86edda2adde8b774bd78e7389051
istoimenova <istoimenova@luxoft.com>
2017-01-27 15:10:12 +0200

    Updated with second application to check ismediaapplication = true or false

--- test_scripts/API/SetAudioStreamingIndicator/009_ATF_N_TC_SetAudioStreamingIndicator_voice_com_app_REJECTED.lua
+++ test_scripts/API/SetAudioStreamingIndicator/009_ATF_N_TC_SetAudioStreamingIndicator_voice_com_app_REJECTED.lua
@@ -11,25 +11,31 @@
 --
 -- 1. Used preconditions
 -- Allow SetAudioStreamingIndicator RPC by policy
--- Register and activate voice-com application
+-- Register and activate first voice-com application, isMediaApplication = false
+-- Register and activate second voice-com application, isMediaApplication = false
 --
 -- 2. Performed steps
 -- Send SetAudioStreamingIndicator(audioStreamingIndicator = "PLAY")
 --
 -- Expected result:
+-- For first and second application:
 -- SDL->mobile: SetAudioStreamingIndicator_response(REJECTED, success:false)
 -- SDL must NOT transfer this SetAudioStreamingIndicator_request to HMI
 ---------------------------------------------------------------------------------------------
 
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+config.application1.registerAppInterfaceParams.isMediaApplication = false
+config.application2.registerAppInterfaceParams.isMediaApplication = true
 config.application1.registerAppInterfaceParams.appHMIType = {"COMMUNICATION"}
+config.application2.registerAppInterfaceParams.appHMIType = {"COMMUNICATION"}
 
 --[[ Required Shared libraries ]]
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
 local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local mobile_session = require('mobile_session')
 
 --[[ General Precondition before ATF start ]]
 testCasesForPolicyTable:precondition_updatePolicy_AllowFunctionInHmiLeves({"BACKGROUND", "FULL", "LIMITED"}, "SetAudioStreamingIndicator")
@@ -47,14 +53,42 @@ function Test:Precondition_ActivateApp()
   EXPECT_NOTIFICATION("OnHMIStatus", {systemContext = "MAIN", hmiLevel = "FULL"})
 end
 
+function Test:Precondition_StartSecondSession()
+	self.mobileSession1 = mobile_session.MobileSession(self, self.mobileConnection)
+	self.mobileSession1:StartService(7)
+end
+
+function Test:Preconditon_RegisterSecondApplication()
+  local CorIdRegister = self.mobileSession1:SendRPC("RegisterAppInterface", config.application2.registerAppInterfaceParams)
+
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application2.registerAppInterfaceParams.appName }})
+  :Do(function(_,data) self.applications[config.application2.registerAppInterfaceParams.appName] = data.params.application.appID end)
+
+  self.mobileSession1:ExpectResponse(CorIdRegister, { success=true, resultCode = "SUCCESS"})
+	self.mobileSession1:ExpectNotification("OnHMIStatus", { systemContext = "MAIN", hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE"})
+end
+
+function Test:Precondition_ActivateSecondApp()
+  commonSteps:ActivateAppInSpecificLevel(self, self.applications[config.application2.registerAppInterfaceParams.appName])
+  EXPECT_NOTIFICATION("OnHMIStatus", {systemContext = "MAIN", hmiLevel = "BACKGROUND"})
+  self.mobileSession1:ExpectNotification("OnHMIStatus", {systemContext = "MAIN", hmiLevel = "FULL"})
+end
+
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 
-function Test:TestStep_SetAudioStreamingIndicator_REJECTED_PLAY()
+function Test:TestStep1_SetAudioStreamingIndicator_REJECTED_PLAY_ismedia_app_false()
   local corr_id = self.mobileSession:SendRPC("SetAudioStreamingIndicator", { audioStreamingIndicator = "PLAY" })
   EXPECT_HMICALL("UI.SetAudioStreamingIndicator",{}):Times(0)
 
   EXPECT_RESPONSE(corr_id, { success = false, resultCode = "REJECTED"})
+end
+
+function Test:TestStep2_SetAudioStreamingIndicator_REJECTED_PLAY_ismedia_app_true()
+  local corr_id = self.mobileSession1:SendRPC("SetAudioStreamingIndicator", { audioStreamingIndicator = "PLAY" })
+  EXPECT_HMICALL("UI.SetAudioStreamingIndicator",{}):Times(0)
+
+  self.mobileSession1:ExpectResponse(corr_id, { success = false, resultCode = "REJECTED"})
 end
 
 --[[ Postconditions ]]
