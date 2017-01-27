e4aa4655a691f6f32ffe5e5f901d57cf7a8e0940
Veronica Veremjova (GitHub) <vveremjova@luxoft.com>
2017-01-27 16:36:06 +0200

    Split tests for REVSDL-1800

---
+++ test_scripts/RC/REVSDL-1800/03_REVSDL-1800.lua
@@ -0,0 +1,129 @@
+local commonSteps = require("user_modules/shared_testcases/commonSteps")
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+revsdl = require("user_modules/revsdl")
+
+revsdl.AddUnknownFunctionIDs()
+revsdl.SubscribeToRcInterface()
+config.ValidateSchema = false
+config.application1.registerAppInterfaceParams.appHMIType = { "REMOTE_CONTROL" }
+
+Test = require('connecttest')
+require('cardinalities')
+local events = require('events')
+local mobile_session = require('mobile_session')
+
+
+--======================================REVSDL-1800========================================--
+---------------------------------------------------------------------------------------------
+-----------REVSDL-1800: Validation: RPC with mismatched control-params and-------------------
+----------------------moduleType from mobile app must get INVALID_DATA-----------------------
+---------------------------------------------------------------------------------------------
+--=========================================================================================--
+
+--=================================================BEGIN TEST CASES 3==========================================================--
+  --Begin Test suit CommonRequestCheck.3 for Req.#3
+
+  --Description: 3. In case application registered with REMOTE_CONTROL AppHMIType sends SetInteriorVehicleData RPC with "climateControlData" and RADIO moduleType
+              -- RSDL must respond with "resultCode: INVALID_DATA, success: false" to this mobile app, not transferring this RPC to the vehicle.
+
+  --Begin Test case CommonRequestCheck.3.1
+  --Description:  RSDL must respond with "resultCode: INVALID_DATA, success: false" to this mobile app, not transferring this RPC to the vehicle.
+
+    --Requirement/Diagrams id in jira:
+        --REVSDL-1800
+
+    --Verification criteria:
+        --In case application registered with REMOTE_CONTROL AppHMIType sends SetInteriorVehicleData RPC with "climateControlData" and RADIO moduleType
+
+    -----------------------------------------------------------------------------------------
+
+      --Begin Test case CommonRequestCheck.3.1.1
+      --Description: application sends SetInteriorVehicleData as Driver and ModuleType = RADIO
+        function Test:SetInterior_RADIO_WrongControlData()
+          --mobile sends request for precondition
+          local cid = self.mobileSession:SendRPC("SetInteriorVehicleData",
+          {
+            moduleData = {
+              moduleType = "RADIO",
+              moduleZone = {
+                col = 0,
+                colspan = 2,
+                level = 0,
+                levelspan = 1,
+                row = 0,
+                rowspan = 2
+              },
+              climateControlData =
+              {
+                fanSpeed = 50,
+                circulateAirEnable = true,
+                dualModeEnable = true,
+                currentTemp = 30,
+                defrostZone = "FRONT",
+                acEnable = true,
+                desiredTemp = 24,
+                autoModeEnable = true,
+                temperatureUnit = "CELSIUS"
+              }
+            }
+          })
+
+          --hmi side: not transferring this RPC to the vehicle.
+          EXPECT_HMICALL("RC.SetInteriorVehicleData")
+          :Times(0)
+
+          --RSDL must respond with "resultCode: INVALID_DATA, success: false" to this mobile app, not transferring this RPC to the vehicle.
+          EXPECT_RESPONSE(cid, { success = false, resultCode = "INVALID_DATA" })
+
+        end
+      --End Test case CommonRequestCheck.3.1.1
+
+    -----------------------------------------------------------------------------------------
+
+      --Begin Test case CommonRequestCheck.3.1.2
+      --Description: application sends SetInteriorVehicleData as Front Passenger and ModuleType = RADIO
+        function Test:SetInterior_RADIO_FrontClimateControlData()
+          --mobile sends request for precondition
+          local cid = self.mobileSession:SendRPC("SetInteriorVehicleData",
+          {
+            moduleData = {
+              moduleType = "RADIO",
+              moduleZone = {
+                col = 1,
+                colspan = 2,
+                level = 0,
+                levelspan = 1,
+                row = 0,
+                rowspan = 2
+              },
+              climateControlData =
+              {
+                fanSpeed = 50,
+                circulateAirEnable = true,
+                dualModeEnable = true,
+                currentTemp = 30,
+                defrostZone = "FRONT",
+                acEnable = true,
+                desiredTemp = 24,
+                autoModeEnable = true,
+                temperatureUnit = "CELSIUS"
+              }
+            }
+          })
+
+          --hmi side: not transferring this RPC to the vehicle.
+          EXPECT_HMICALL("RC.SetInteriorVehicleData")
+          :Times(0)
+
+          --RSDL must respond with "resultCode: INVALID_DATA, success: false" to this mobile app, not transferring this RPC to the vehicle.
+          EXPECT_RESPONSE(cid, { success = false, resultCode = "INVALID_DATA" })
+
+        end
+      --End Test case CommonRequestCheck.3.1.2
+
+    -----------------------------------------------------------------------------------------
+  --End Test case CommonRequestCheck.3.1
+
+
+--=================================================END TEST CASES 3==========================================================--
