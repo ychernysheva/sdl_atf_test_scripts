674489079d9d10568519f3114bdbd7409e463099
Nikolaev <anikolaev@anikolaev-lws>
2017-01-28 14:36:43 +0200

    Script for coverage when autoCompleteList param not present in PRC

---
+++ test_scripts/API/AutoCompleteList/002_ATF_SetGlobalProperties_P_AutoCompleteList_WithOutParam.lua
@@ -0,0 +1,82 @@
+--------------------------------------------------------------------------------------------------------------
+-- Requirement summary:
+-- [SetGlobalProperties] Conditions for SDL SDL respond <success = true, resultCode = "SUCCESS"> to mobile app
+--
+-- Description:
+-- SDL must: tranfer SetGlobalProperties_request without <autoCompleteList> param to HMI
+-- respond with <resultCode_received_from _HMI> to mobile app
+--
+-- Performed steps:
+-- 1. Register Application.
+-- 2. Mobile send RPC SetGlobalProperties without <autoCompleteList> param
+-- 3. SDL respond <success = true, resultCode = "SUCCESS"> to mobile app
+--------------------------------------------------------------------------------------------------------------
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+
+--[[ Required Shared libraries ]]
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+
+--[[ General Precondition before ATF start ]]
+config.defaultProtocolVersion = 2
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+--[[ General Settings for configuration ]]
+Test = require('connecttest')
+require('cardinalities')
+require('user_modules/AppTypes')
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+function Test:SetGlobalProperties_Without_autoCompleteList()
+--mobile side: sending SetGlobalProperties request
+local cid = self.mobileSession:SendRPC("SetGlobalProperties",
+    {
+      keyboardProperties =
+      {
+        keyboardLayout = "QWERTY",
+        keypressMode = "SINGLE_KEYPRESS",
+        limitedCharacterList =
+        {
+          "a"
+        },
+        language = "EN-US",
+        autoCompleteText = "Text_1, Text_2",
+      }
+    })
+  --hmi side: expect UI.SetGlobalProperties request
+  EXPECT_HMICALL("UI.SetGlobalProperties", 
+  {
+       keyboardProperties =
+      {
+        keyboardLayout = "QWERTY",
+        keypressMode = "SINGLE_KEYPRESS",
+        limitedCharacterList =
+        {
+          "a"
+        },
+        language = "EN-US",
+        autoCompleteText = "Text_1, Text_2",
+      }
+    })
+  :Do(function(_,data)
+    if (data.params.autoCompleteList == nil) then
+        self.hmiConnection:SendResponse (data.id, data.method, "SUCCESS", {})
+      return true
+    else
+      return false
+  end
+  end)
+  --mobile side: expect SetGlobalProperties response
+  EXPECT_RESPONSE(cid, { success = true, resultCode = "SUCCESS" })
+ --mobile side: expecting OnHashChange notification
+   EXPECT_NOTIFICATION("OnHashChange") 
+   :Times(1)
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+function Test.Postcondition_SDLStop()
+  StopSDL()
+end
