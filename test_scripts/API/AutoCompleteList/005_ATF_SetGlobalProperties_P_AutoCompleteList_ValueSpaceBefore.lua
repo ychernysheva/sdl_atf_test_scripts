817e407c6f28ab19bf991ce483e14e1a73d9406e
Nikolaev <anikolaev@anikolaev-lws>
2017-02-01 15:29:25 +0200

    Added changes after review

--- test_scripts/API/AutoCompleteList/005_ATF_SetGlobalProperties_P_AutoCompleteList_ValueSpaceBefore.lua
+++ test_scripts/API/AutoCompleteList/005_ATF_SetGlobalProperties_P_AutoCompleteList_ValueSpaceBefore.lua
@@ -1,15 +1,17 @@
 ----------------------------------------------------------------------------------------
 -- Requirement summary:
--- [SetGlobalProperties] Conditions for SDL SDL respond <success = true, resultCode = "SUCCESS"> to mobile app
+-- [SetGlobalProperties] Conditions for SDL  must transfer request in caes of valid "autoCompleteList" param
 --
 -- Description:
--- SDL must: tranfer SetGlobalProperties_request with <autoCompleteList> param param to HMI
--- respond with <resultCode_received_from _HMI> to mobile app
+-- Case when SDL tranfer SetGlobalProperties_request with <autoCompleteList> param param to HMI
+-- respond with <SUCCESS> to mobile app
 --
 -- Performed steps:
 -- 1. Register Application.
 -- 2. Mobile send RPC SetGlobalProperties with <autoCompleteList> with spaces before values.
--- 3. SDL respond <success = true, resultCode = "SUCCESS"> to mobile app
+-- 
+-- Expected result:
+-- SDL respond <success = true, resultCode = "SUCCESS"> to mobile app
 ----------------------------------------------------------------------------------------
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
@@ -43,7 +45,7 @@ local cid = self.mobileSession:SendRPC("SetGlobalProperties",
         },
         language = "EN-US",
         autoCompleteText = "Text_1, Text_2",
-        autoCompleteList = {" SpaceBefore", " SpaceBefore"}
+        autoCompleteList = {" SpaceBefore"}
       }
     })
   --hmi side: expect UI.SetGlobalProperties request
@@ -58,7 +60,7 @@ local cid = self.mobileSession:SendRPC("SetGlobalProperties",
           "a"
         },
         language = "EN-US",
-        autoCompleteList = {" SpaceBefore", " SpaceBefore"}
+        autoCompleteList = {" SpaceBefore"}
       }
     })
   :Do(function(_,data)
