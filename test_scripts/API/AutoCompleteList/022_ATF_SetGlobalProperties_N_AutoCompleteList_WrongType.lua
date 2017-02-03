e655b4afdee1e224d5f6de86fb5dab6acfa568af
Alexander Nikolaev (GitHub) <anikolaev@luxoft.com>
2017-02-03 12:06:38 +0200

    Update 022_ATF_SetGlobalProperties_N_AutoCompleteList_WrongType.lua

--- test_scripts/API/AutoCompleteList/022_ATF_SetGlobalProperties_N_AutoCompleteList_WrongType.lua
+++ test_scripts/API/AutoCompleteList/022_ATF_SetGlobalProperties_N_AutoCompleteList_WrongType.lua
@@ -4,6 +4,7 @@
 -- Description:
 -- Case when mobile send SetGlobalProperties request with <autoCompleteList> parm which have invalid type,
 -- SDL respond <success = false, resultCode = "INVALID_DATA"> to mobile app,
+--
 -- Perform steps:
 -- 1. Register Application.
 -- 2. Mobile send RPC SetGlobalProperties with <autoCompleteList> invalid type.
