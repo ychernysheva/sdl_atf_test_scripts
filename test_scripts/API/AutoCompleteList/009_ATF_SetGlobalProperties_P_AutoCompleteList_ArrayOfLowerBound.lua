e54c1c5469cea0219146425e488a85d25ddb89c9
Alexander Nikolaev (GitHub) <anikolaev@luxoft.com>
2017-02-03 11:32:09 +0200

    Update 009_ATF_SetGlobalProperties_P_AutoCompleteList_ArrayOfLowerBound.lua

--- test_scripts/API/AutoCompleteList/009_ATF_SetGlobalProperties_P_AutoCompleteList_ArrayOfLowerBound.lua
+++ test_scripts/API/AutoCompleteList/009_ATF_SetGlobalProperties_P_AutoCompleteList_ArrayOfLowerBound.lua
@@ -3,12 +3,13 @@
 -- [SetGlobalProperties] Conditions for SDL  must transfer request in case of valid "autoCompleteList" param 
 --
 -- Description:
--- SDL must: tranfer SetGlobalProperties_request with <autoCompleteList> param param to HMI
+-- Case when mobile send SetGlobalProperties request, SDL must tranfer SetGlobalProperties_request with <autoCompleteList> array is lower bound param to HMI,
 -- respond with <SUCCESS> to mobile app
 --
 -- Performed steps:
 -- 1. Register Application.
 -- 2. Mobile send RPC SetGlobalProperties with <autoCompleteList> array is lower bound.
+-- 3. HMI respond SUCCESS
 --
 -- Expected result:
 -- SDL respond <success = true, resultCode = "SUCCESS"> to mobile app
