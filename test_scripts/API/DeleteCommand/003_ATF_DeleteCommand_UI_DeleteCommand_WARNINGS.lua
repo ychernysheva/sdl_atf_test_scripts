9f295bc6e01baac7f74c21ac0870f0212d374c1c
valeriia <vvvakulenko@luxoft.com>
2017-02-06 14:25:38 +0200

    Update due to review comments

--- test_scripts/API/DeleteCommand/003_ATF_DeleteCommand_UI_DeleteCommand_WARNINGS.lua
+++ test_scripts/API/DeleteCommand/003_ATF_DeleteCommand_UI_DeleteCommand_WARNINGS.lua
@@ -6,7 +6,9 @@
 -- test is intended to check that SDL sends WARNINGS (success:true) to mobile app in case HMI respond WARNINGS to at least one HMI-portions
 -- in this test case when UI.DeleteCommand gets WARNINGS and VR.DeleteCommand gets ANY successfull result code is checked
 --
--- 1. Used preconditions: App is registered and activated SUCCESSFULLY. AddCommand is sent SUCCESSFULLY
+-- 1. Used preconditions:
+--      App is registered and activated SUCCESSFULLY
+--      AddCommand is sent SUCCESSFULLY
 -- 2. Performed steps:
 -- MOB -> SDL: sends DeleteCommand
 -- HMI -> SDL: UI.DeleteCommand (WARNINGS), VR.DeleteCommand (cyclically checked cases for result codes SUCCESS, WARNINGS, WRONG_LANGUAGE, RETRY, SAVED)
@@ -19,11 +21,10 @@
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 -- ToDo (vvvakulenko): remove after issue "ATF does not stop HB timers by closing session and connection" is resolved
 config.defaultProtocolVersion = 2
- 
+
 --[[ Required Shared libraries ]]
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require ('user_modules/shared_testcases/commonSteps')
-local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
 local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
 config.SDLStoragePath = commonPreconditions:GetPathToSDL() .. "storage/"
 local json = require('json')
@@ -31,42 +32,41 @@ local json = require('json')
 --[[ Local Variables ]]
 local storagePath = config.SDLStoragePath..config.application1.registerAppInterfaceParams.appID.. "_" .. config.deviceMAC.. "/"
 local ServerAddress = commonFunctions:read_parameter_from_smart_device_link_ini("ServerAddress")
- 
+
 --[[ Local Functions ]]
 local function update_ptu()
   commonPreconditions:BackupFile("sdl_preloaded_pt.json")
   local config_path = commonPreconditions:GetPathToSDL()
- 
+
   local pathToFile = config_path .. 'sdl_preloaded_pt.json'
   local file = io.open(pathToFile, "r")
   local json_data = file:read("*all")
   file:close()
- 
+
   local data = json.decode(json_data)
   if(data.policy_table.functional_groupings["DataConsent-2"]) then
     data.policy_table.functional_groupings["DataConsent-2"].rpcs = json.null
   end
- 
+
   data.policy_table.functional_groupings["Base-4"].rpcs.AddCommand = { hmi_levels = { "BACKGROUND", "FULL", "LIMITED" }}
   data.policy_table.functional_groupings["Base-4"].rpcs.DeleteCommand = { hmi_levels = { "BACKGROUND", "FULL", "LIMITED" }}
- 
+
   file = io.open(config_path .. 'sdl_preloaded_pt.json', "w")
   file:write(json.encode(data))
   file:close()
 end
- 
+
 update_ptu()
- 
+
 --[[ General Precondition before ATF start ]]
 commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFiles()
 commonSteps:DeletePolicyTable()
-testCasesForPolicyTable:precondition_updatePolicy_AllowFunctionInHmiLeves({"BACKGROUND", "FULL", "LIMITED"},"AddCommand")
 
 --[[ General Settings for configuration ]]
 Test = require('connecttest')
 require('user_modules/AppTypes')
- 
+
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
 
@@ -88,9 +88,9 @@ function Test:Precondition_ActivationApp()
   end)
   EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN"})
 end
- 
+
 commonSteps:PutFile("Precondition_PutFile", "icon.png")
- 
+
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
 
@@ -104,13 +104,13 @@ local resultCodes = {"SUCCESS", "WARNINGS", "WRONG_LANGUAGE", "RETRY", "SAVED"}
       vrCommands = { "VRCommandonepositive" .. tostring(i), "VRCommandonepositivedouble" .. tostring(i)},
       cmdIcon = { value ="icon.png", imageType ="DYNAMIC"}
     })
-  
+
     EXPECT_HMICALL("UI.AddCommand",
     {
       cmdID = i,
       menuParams = { parentID = 0, position = 0, menuName ="Commandpositive" .. tostring(i)},
       cmdIcon = { imageType ="DYNAMIC"}
-    })  
+    })
     :ValidIf(function(_,data)
       local value_Icon = storagePath .. "icon.png"
       if (string.match(data.params.cmdIcon.value, "%S*" .. "("..string.sub(storagePath, 2).."icon.png)" .. "$") == nil ) then
@@ -119,56 +119,56 @@ local resultCodes = {"SUCCESS", "WARNINGS", "WRONG_LANGUAGE", "RETRY", "SAVED"}
       else
         return true
       end
-    end)  
+    end)
     :Do(function(_,data) self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {}) end)
-  
+
     EXPECT_HMICALL("VR.AddCommand",
     {
       cmdID = i,
       type = "Command",
       vrCommands = { "VRCommandonepositive" .. tostring(i), "VRCommandonepositivedouble" .. tostring(i)}
-    })  
+    })
     :Do(function(_,data) self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {}) end)
-  
+
     EXPECT_RESPONSE(cor_id_add_cmd, { success = true, resultCode = "SUCCESS" })
     EXPECT_NOTIFICATION("OnHashChange")
   end
- 
+
   Test["TestStep_DelteCommand_UI_DelCmd_WARNINGS_and_VR_DelCmd_"..resultCodes[i]] = function(self)
     local cor_id_del_cmd = self.mobileSession:SendRPC("DeleteCommand",
     {
       cmdID = i
     })
-  
+
     EXPECT_HMICALL("UI.DeleteCommand",
     {
       cmdID = i,
-      appID = self.applications[config.application1.registerAppInterfaceParams.appName]
+      appID = self.applications[config.application1.registerAppInterfaceParams.appID]
     })
     :Do(function(_,data) self.hmiConnection:SendResponse(data.id, "UI.DeleteCommand", "WARNINGS", {}) end)
-  
+
     EXPECT_HMICALL("VR.DeleteCommand",
     {
       cmdID = i,
       type = "Command",
-      appID = self.applications[config.application1.registerAppInterfaceParams.appName]
+      appID = self.applications[config.application1.registerAppInterfaceParams.appID]
     })
     :Do(function(_,data) self.hmiConnection:SendResponse(data.id, "VR.DeleteCommand", resultCodes[i], {}) end)
-  
+
     EXPECT_RESPONSE(cor_id_del_cmd, { success = true, resultCode = "WARNINGS" })
     EXPECT_NOTIFICATION("OnHashChange")
   end
 end
- 
+
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
- 
+
 function Test.Postcondition_Restore_preloaded_file()
   commonPreconditions:RestoreFile("sdl_preloaded_pt.json")
 end
- 
+
 function Test.Postcondition_SDLStop()
   StopSDL()
 end
- 
+
 return Test
