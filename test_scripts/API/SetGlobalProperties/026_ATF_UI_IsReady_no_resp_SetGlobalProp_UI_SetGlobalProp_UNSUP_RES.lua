b92599974ff94dee0623c3c5c838a8942cb9a686
valeriia <vvvakulenko@luxoft.com>
2017-02-07 18:16:59 +0200

    Update script due to review comments

--- test_scripts/API/SetGlobalProperties/026_ATF_UI_IsReady_no_resp_SetGlobalProp_UI_SetGlobalProp_UNSUP_RES.lua
+++ test_scripts/API/SetGlobalProperties/026_ATF_UI_IsReady_no_resp_SetGlobalProp_UI_SetGlobalProp_UNSUP_RES.lua
@@ -1,27 +1,37 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
 -- SDL must send WARNINGS (success:true) to mobile app in case HMI respond WARNINGS at least to one component of RPC
+-- [UI Interface] HMI does NOT respond to IsReady and mobile app sends RPC that must be splitted
 --
 -- Description:
 -- test is intended to check that SDL sends UNSUPPORTED_RESOURCE (success:true) to mobile app in case HMI respond: WARNINGS one HMI-portion and UNSUPPORTED_RESOURCE to another one
--- in particular test it is checked case when UI.SetGlobalProperties gets UNSUPPORTED_RESOURCE and TTS.SetGlobalProperties gets WARNINGS
--- 1. Used preconditions: App is registered and activated SUCCESSFULLY
--- 2. Performed steps: 
+-- in particular test it is checked case when TTS.SetGlobalProperties with WARNINGS and to UI.SetGlobalProperties with UNSUPPORTED_RESOURCE (success:true)
+--
+-- 1. Used preconditions:
+-- HMI does not respont to UI.IaReady
+-- App is registered and activated SUCCESSFULLY
+-- 2. Performed steps:
 -- MOB -> SDL: sends SetGlobalProperties
--- SDL -> HMI: resends UI.SetGlobalProperties and TTS.SetGlobalProperties
 -- HMI -> SDL: UI.SetGlobalProperties (UNSUPPORTED_RESOURCE), TTS.SetGlobalProperties (WARNINGS)
 --
 -- Expected result:
+-- SDL -> HMI: resends UI.SetGlobalProperties and TTS.SetGlobalProperties
 -- SDL -> MOB: SetGlobalProperties (resultcode: UNSUPPORTED_RESOURCE, success: true)
 ---------------------------------------------------------------------------------------------
 
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
-config.SDLStoragePath = config.pathToSDL .. "storage/"
+-- ToDo (vvvakulenko): remove after issue "ATF does not stop HB timers by closing session and connection" is resolved
+config.defaultProtocolVersion = 2
 
 --[[ Required Shared libraries ]]
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require ('user_modules/shared_testcases/commonSteps')
+local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
+local testCasesForUI_IsReady = require('user_modules/IsReady_Template/testCasesForUI_IsReady')
+local mobile_session = require('mobile_session')
+config.SDLStoragePath = commonPreconditions:GetPathToSDL() .. "storage/"
 
 --[[ Local Variables ]]
 local storagePath = config.SDLStoragePath..config.application1.registerAppInterfaceParams.appID.. "_" .. config.deviceMAC.. "/"
@@ -31,13 +41,31 @@ local ServerAddress = commonFunctions:read_parameter_from_smart_device_link_ini(
 commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFiles()
 commonSteps:DeletePolicyTable()
+testCasesForPolicyTable:precondition_updatePolicy_AllowFunctionInHmiLeves({"BACKGROUND", "FULL", "LIMITED"},"SetGlobalProperties")
 
 --[[ General Settings for configuration ]]
-Test = require('connecttest')
+Test = require('user_modules/connecttest_initHMI')
 require('user_modules/AppTypes')
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
+
+function Test:Precondition_InitHMI_OnReady()
+  testCasesForUI_IsReady.InitHMI_onReady_without_UI_IsReady(self, 1)
+  EXPECT_HMICALL("UI.IsReady")
+  -- Do not send HMI response of UI.IsReady
+end
+
+function Test:Precondition_connectMobile()
+  self:connectMobile()
+end
+
+function Test:Precondition_StartSession()
+  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession:StartService(7)
+end
+
+commonSteps:RegisterAppInterface("Precondition_RegisterAppInterface")
 
 function Test:Precondition_ActivationApp()
   local request_id = self.hmiConnection:SendRequest("SDL.ActivateApp", { appID = self.applications[config.application1.registerAppInterfaceParams.appName]})
@@ -46,7 +74,7 @@ function Test:Precondition_ActivationApp()
     if (data.result.isSDLAllowed ~= true) then
       local request_id1 = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = {"DataConsent"}})
       EXPECT_HMIRESPONSE(request_id1)
-      :Do(function(_,_)   
+      :Do(function(_,_)
         self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality", {allowed = true, source = "GUI", device = {id = config.deviceMAC, name = ServerAddress}})
         EXPECT_HMICALL("BasicCommunication.ActivateApp")
         :Do(function(_,data1)
@@ -55,12 +83,10 @@ function Test:Precondition_ActivationApp()
       end)
     end
   end)
-  EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN"}) 
+  EXPECT_NOTIFICATION("OnHMIStatus", {hmiLevel = "FULL", systemContext = "MAIN"})
 end
 
-function Test.Precondition_PutFile()
-  commonSteps:PutFile("Precondition_PutFile", "action.png")
-end
+commonSteps:PutFile("Precondition_PutFile", "action.png")
 
 --[[ Test ]]
 commonFunctions:newTestCasesGroup("Test")
@@ -69,113 +95,64 @@ function Test:TestStep_SetGlobalProperties_WARNINGS_to_TTS_SGP_and_UNSUPPORTED_R
   local cid = self.mobileSession:SendRPC("SetGlobalProperties",
   {
     menuTitle = "Menu Title",
-    timeoutPrompt = 
-      {{
-        text = "Timeout prompt",
-        type = "TEXT"
-    }},
-    vrHelp = 
+    timeoutPrompt = {{ text = "Timeout prompt", type = "TEXT" }},
+    vrHelp =
       {{
         position = 1,
-        image = 
-        {
-          value = "action.png",
-          imageType = "DYNAMIC"
-        },
+        image = { value = "action.png", imageType = "DYNAMIC"},
         text = "VR help item"
     }},
-    menuIcon = 
-    {
-      value = "action.png",
-      imageType = "DYNAMIC"
-    },
-    helpPrompt = 
-      {{
-        text = "Help prompt",
-        type = "TEXT"
-    }},
+    menuIcon = { value = "action.png", imageType = "DYNAMIC"},
+    helpPrompt = {{ text = "Help prompt", type = "TEXT" }},
     vrHelpTitle = "VR help title",
-    keyboardProperties = 
-    {
-      keyboardLayout = "QWERTY",
-      language = "EN-US"
-    }
+    keyboardProperties = { keyboardLayout = "QWERTY", language = "EN-US"}
   })
 
   EXPECT_HMICALL("TTS.SetGlobalProperties",
   {
-    timeoutPrompt = 
-      {{
-        text = "Timeout prompt",
-        type = "TEXT"
-    }},
-    helpPrompt = 
-      {{
-        text = "Help prompt",
-        type = "TEXT"
-    }},
+    timeoutPrompt = {{ text = "Timeout prompt", type = "TEXT" }},
+    helpPrompt = {{ text = "Help prompt", type = "TEXT" }},
     appID = self.applications[config.application1.registerAppInterfaceParams.appName]
   })
-
-  :ValidIf(function(_,data)
-    local value_Icon = storagePath .. "action.png"
-    if (string.match(data.params.cmdIcon.value, "%S*" .. "("..string.sub(storagePath, 2).."action.png)" .. "$") == nil ) then
-      print("\27[31m value of menuIcon is WRONG. Expected: ~".. value_Icon .. "; Real: " .. data.params.cmdIcon.value .. "\27[0m")
-    return false
-    else
-    return true
-    end
-  end)
-
-  :Do(function(_,data) self.hmiConnection:SendResponse(data.id, "TTS.SetGlobalProperties", "WARNINGS", {})
-  end)
+  :Do(function(_,data) self.hmiConnection:SendResponse(data.id, "TTS.SetGlobalProperties", "WARNINGS", {}) end)
 
   EXPECT_HMICALL("UI.SetGlobalProperties",
   {
     vrHelpTitle = "VR help title",
-    vrHelp = 
+    vrHelp =
       {{
         position = 1,
-        image = 
-        {
-          imageType = "DYNAMIC",
-          value = storagePath .. "action.png"
-        },
+        image = { imageType = "DYNAMIC"},
         text = "VR help item"
     }},
     menuTitle = "Menu Title",
-    menuIcon = 
-    {
-      imageType = "DYNAMIC",
-      value = storagePath .. "action.png"
-    },
-    keyboardProperties = 
-    {
-      keyboardLayout = "QWERTY",
-      language = "EN-US"
-    },
-    appID = self.applications["Test Application"]
+    menuIcon = { imageType = "DYNAMIC" },
+    keyboardProperties = { keyboardLayout = "QWERTY", language = "EN-US"},
+    appID = self.applications[config.application1.registerAppInterfaceParams.appName]
   })
-
   :ValidIf(function(_,data)
     local value_Icon = storagePath .. "action.png"
-    if (string.match(data.params.cmdIcon.value, "%S*" .. "("..string.sub(storagePath, 2).."action.png)" .. "$") == nil ) then
-      print("\27[31m value of menuIcon is WRONG. Expected: ~".. value_Icon .. "; Real: " .. data.params.cmdIcon.value .. "\27[0m")
-    return false
+    if (string.match(params.vrHelp[1].image.value, "%S*" .. "("..string.sub(storagePath, 2).."action.png)" .. "$") == nil ) then
+      print("\27[31m value of vrHelp.Image is WRONG. Expected: ~".. value_Icon .. "; Real: " .. params.vrHelp[1].image.value .. "\27[0m")
+      return false
     else
-    return true
+      return true
     end
   end)
+  :Do(function(_,data) self.hmiConnection:SendResponse(data.id, "UI.SetGlobalProperties", "UNSUPPORTED_RESOURCE", {}) end)
 
-  :Do(function(_,data) self.hmiConnection:SendResponse(data.id, "UI.SetGlobalProperties", "UNSUPPORTED_RESOURCE", {})
-  end)
-  
+  EXPECT_HMIRESPONSE(RequestId,{result = {code = 0, method = "SDL.GetUserFriendlyMessage"}})
+
   EXPECT_RESPONSE(cid, { success = true, resultCode = "UNSUPPORTED_RESOURCE"})
   EXPECT_NOTIFICATION("OnHashChange")
 end
 
 --[[ Postconditions ]]
 commonFunctions:newTestCasesGroup("Postconditions")
+
+function Test.Postcondition_Restore_preloaded_file()
+  commonPreconditions:RestoreFile("sdl_preloaded_pt.json")
+end
 
 function Test.Postcondition_SDLStop()
   StopSDL()
