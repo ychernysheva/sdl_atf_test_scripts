d306bd9920f8df0377789fb867f2931f0e059dab
Alexandr Deriabin <aderiabin@luxoft.com>
2017-02-10 15:14:38 +0200

    Part of scripts were splitted

---
+++ test_scripts/RC/REVSDL-966/02.01_REVSDL-966.lua
@@ -0,0 +1,187 @@
+local commonSteps = require("user_modules/shared_testcases/commonSteps")
+commonSteps:CheckSDLPath()
+commonSteps:DeleteLogsFileAndPolicyTable()
+
+local commonPreconditions = require("user_modules/shared_testcases/commonPreconditions")
+commonPreconditions:BackupFile("sdl_preloaded_pt.json")
+commonPreconditions:ReplaceFile("sdl_preloaded_pt.json", "./files/jsons/RC/rc_sdl_preloaded_pt.json")
+
+local revsdl = require("user_modules/revsdl")
+
+revsdl.AddUnknownFunctionIDs()
+revsdl.SubscribeToRcInterface()
+config.ValidateSchema = false
+config.application1.registerAppInterfaceParams.appHMIType = { "REMOTE_CONTROL" }
+config.application1.registerAppInterfaceParams.appID = "8675311"
+
+Test = require('connecttest')
+require('cardinalities')
+
+--======================================Requirement=========================================--
+---------------------------------------------------------------------------------------------
+-----------Requirement: "Allow", "Ask Driver" or "Disallow" permissions - depending-----------
+------------------on zone value in RPC and this zone permissions in Policies-----------------
+---------------------------------------------------------------------------------------------
+--=========================================================================================--
+
+--=================================================BEGIN TEST CASES 2==========================================================--
+  --Begin Test suit CommonRequestCheck.2 for Req.#2
+
+  --Description: 2. In case the application sends a valid rc-RPC with <interiorZone>, <moduleType> and <params> allowed by app's assigned policies
+            --and "equipment" section of policies database contains this RPC name with <params> in <moduleType> in "driver_allow" sub-section of <interiorZone> section
+            --and driver's permission has not yet been obtained for this app&<interiorZone>&<moduleType>
+            --RSDL must send the RC.GetInteriorVehicleDataConsent for getting driver's allowance to the vehicle (HMI).
+
+
+  --Begin Test case CommonRequestCheck.2.1
+  --Description:  For ButtonPress
+
+    --Requirement/Diagrams id in jira:
+        --Requirement
+        --TC: Requirement
+
+    --Verification criteria:
+        --In case the application sends a valid rc-RPC with <interiorZone>, <moduleType> and <params> allowed by app's assigned policies
+
+    -----------------------------------------------------------------------------------------
+
+      --Begin Test case CommonRequestCheck.2.1.1
+      --Description: application sends ButtonPress as Front Passenger (col=1, row=0, level=0) and ModuleType = RADIO
+        function Test:ButtonPress_DriverAllowFrontRADIO()
+          --mobile side: In case the application sends a valid rc-RPC with <interiorZone>, <moduleType> and <params> allowed by app's assigned policies
+          local cid = self.mobileSession:SendRPC("ButtonPress",
+          {
+            zone =
+            {
+              colspan = 2,
+              row = 0,
+              rowspan = 2,
+              col = 1,
+              levelspan = 1,
+              level = 0
+            },
+            moduleType = "RADIO",
+            buttonPressMode = "LONG",
+            buttonName = "VOLUME_UP"
+          })
+
+          --hmi side: expect RC.GetInteriorVehicleDataConsent request from HMI
+          EXPECT_HMICALL("RC.GetInteriorVehicleDataConsent",
+                {
+                  appID = self.applications["Test Application"],
+                  moduleType = "RADIO",
+                  zone =
+                  {
+                    colspan = 2,
+                    row = 0,
+                    rowspan = 2,
+                    col = 1,
+                    levelspan = 1,
+                    level = 0
+                  }
+                })
+            :Do(function(_,data)
+              --hmi side: sending RC.GetInteriorVehicleDataConsent response to RSDL
+              self.hmiConnection:SendResponse(data.id, "RC.GetInteriorVehicleDataConsent", "SUCCESS", {allowed = true})
+
+              --hmi side: expect Buttons.ButtonPress request
+              EXPECT_HMICALL("Buttons.ButtonPress",
+                      {
+                        zone =
+                        {
+                          colspan = 2,
+                          row = 0,
+                          rowspan = 2,
+                          col = 1,
+                          levelspan = 1,
+                          level = 0
+                        },
+                        moduleType = "RADIO",
+                        buttonPressMode = "LONG",
+                        buttonName = "VOLUME_UP"
+                      })
+                :Do(function(_,data)
+                  --hmi side: sending Buttons.ButtonPress response
+                  self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+                end)
+          end)
+
+          EXPECT_RESPONSE(cid, { success = true, resultCode = "SUCCESS" })
+        end
+      --End Test case CommonRequestCheck.2.1.1
+
+    -----------------------------------------------------------------------------------------
+
+      --Begin Test case CommonRequestCheck.2.1.2
+      --Description: application sends ButtonPress as Left Rare Passenger (col=0, row=1, level=0) and ModuleType = CLIMATE
+        function Test:ButtonPress_DriverAllowLeftCLIMATE()
+          --mobile side: In case the application sends a valid rc-RPC with <interiorZone>, <moduleType> and <params> allowed by app's assigned policies
+          local cid = self.mobileSession:SendRPC("ButtonPress",
+          {
+            zone =
+            {
+              colspan = 2,
+              row = 1,
+              rowspan = 2,
+              col = 0,
+              levelspan = 1,
+              level = 0
+            },
+            moduleType = "CLIMATE",
+            buttonPressMode = "SHORT",
+            buttonName = "LOWER_VENT"
+          })
+
+          --hmi side: expect RC.GetInteriorVehicleDataConsent request from HMI
+          EXPECT_HMICALL("RC.GetInteriorVehicleDataConsent",
+                {
+                  appID = self.applications["Test Application"],
+                  moduleType = "CLIMATE",
+                  zone =
+                  {
+                    colspan = 2,
+                    row = 1,
+                    rowspan = 2,
+                    col = 0,
+                    levelspan = 1,
+                    level = 0
+                  }
+                })
+            :Do(function(_,data)
+              --hmi side: sending RC.GetInteriorVehicleDataConsent response to RSDL
+              self.hmiConnection:SendResponse(data.id, "RC.GetInteriorVehicleDataConsent", "SUCCESS", {allowed = true})
+
+              --hmi side: expect Buttons.ButtonPress request
+              EXPECT_HMICALL("Buttons.ButtonPress",
+                      {
+                        zone =
+                        {
+                          colspan = 2,
+                          row = 1,
+                          rowspan = 2,
+                          col = 0,
+                          levelspan = 1,
+                          level = 0
+                        },
+                        moduleType = "CLIMATE",
+                        buttonPressMode = "SHORT",
+                        buttonName = "LOWER_VENT"
+                      })
+                :Do(function(_,data)
+                  --hmi side: sending Buttons.ButtonPress response
+                  self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+                end)
+          end)
+
+          EXPECT_RESPONSE(cid, { success = true, resultCode = "SUCCESS" })
+        end
+      --End Test case CommonRequestCheck.2.1.2
+
+    -----------------------------------------------------------------------------------------
+  --End Test case CommonRequestCheck.2.1
+
+  --=================================================END TEST CASES 2==========================================================--
+
+function Test.PostconditionsRestoreFile()
+  commonPreconditions:RestoreFile("sdl_preloaded_pt.json")
+end
