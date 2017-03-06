0ff003f65367d1bef092238607ae85c841d6014b
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2017-03-06 16:04:44 +0200

    Update according to review comments 3

--- test_scripts/Polices/External_UCS/001_ATF_P_TC_LPT_Creation_PreloadedPT_without_external_consent_status_groups_struct.lua
+++ test_scripts/Polices/External_UCS/001_ATF_P_TC_LPT_Creation_PreloadedPT_without_external_consent_status_groups_struct.lua
@@ -13,119 +13,95 @@
 --
 -- Preconditions:
 -- 1. Stop SDL
--- 2. Modify PreloadedPolicyTable (add 'external_consent_status_groups' section)
--- 3. Start SDL
--- 4. Check SDL status => 0 (SDL is stopped)
+-- 2. Check that PreloadedPT doesn't contain 'external_consent_status_groups' section
 --
 -- Steps:
--- 1. Remove Local Policy Table
--- 2. Modify PreloadedPolicyTable (remove 'external_consent_status_groups' section)
--- 3. Start SDL
--- 4. Check SDL status
+-- 1. Start SDL
+-- 2. Check SDL status
 --
 -- Expected result:
 -- Status = 1 (SDL is running)
+--
+-- Note: Script is designed for EXTERNAL_PROPRIETARY flow
 ---------------------------------------------------------------------------------------------
 
 --[[ General configuration parameters ]]
-  config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
-  config.defaultProtocolVersion = 2
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+config.defaultProtocolVersion = 2
 
 --[[ Required Shared Libraries ]]
-  local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
-  local commonSteps = require('user_modules/shared_testcases/commonSteps')
-  local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
-  local sdl = require('SDL')
-  local testCasesForExternalUCS = require('user_modules/shared_testcases/testCasesForExternalUCS')
+local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
+local sdl = require('SDL')
+local testCasesForExternalUCS = require('user_modules/shared_testcases/testCasesForExternalUCS')
+
+--[[ Local variables ]]
+local checkedSection = "external_consent_status_groups"
 
 --[[ General Precondition before ATF start ]]
-  commonFunctions:SDLForceStop()
-  commonSteps:DeletePolicyTable()
-  commonSteps:DeleteLogsFiles()
-  commonPreconditions:BackupFile("sdl_preloaded_pt.json")
+commonFunctions:SDLForceStop()
+commonSteps:DeletePolicyTable()
+commonSteps:DeleteLogsFiles()
+commonPreconditions:BackupFile("sdl_preloaded_pt.json")
 
 --[[ General Settings for configuration ]]
-  Test = require("user_modules/connecttest_resumption")
-  require('user_modules/AppTypes')
+Test = require("user_modules/connecttest_resumption")
+require('user_modules/AppTypes')
 
 --[[ Preconditions ]]
-  commonFunctions:newTestCasesGroup("Preconditions")
+commonFunctions:newTestCasesGroup("Preconditions")
 
-  function Test:StopSDL()
-    testCasesForExternalUCS.ignitionOff(self)
-  end
+function Test:StopSDL()
+  testCasesForExternalUCS.ignitionOff(self)
+end
 
-  function Test:CheckSDLStatus_1_STOPPED()
-    testCasesForExternalUCS.checkSDLStatus(self, sdl.STOPPED)
-  end
+function Test:CheckSDLStatus_1_STOPPED()
+  testCasesForExternalUCS.checkSDLStatus(self, sdl.STOPPED)
+end
 
-  function Test.RemoveLPT()
-    testCasesForExternalUCS.removeLPT()
-  end
-
-  function Test.UpdatePreloadedPT_Add_section()
-    local updateFunc = function(preloadedTable)
-      preloadedTable.policy_table.device_data = {
-        [config.deviceMAC] = {
-          user_consent_records = {
-            [config.application1.registerAppInterfaceParams.appID] = {
-              external_consent_status_groups = {
-                Location = false
-              }
-            }
-          }
-        }
-      }
+function Test:CheckPreloadedPT()
+  local preloadedFile = commonPreconditions:GetPathToSDL() .. "sdl_preloaded_pt.json"
+  local preloadedTable = testCasesForExternalUCS.createTableFromJsonFile(preloadedFile)
+  local result = true
+  if preloadedTable
+  and preloadedTable.policy_table
+  and preloadedTable.policy_table.device_data
+  and preloadedTable.policy_table.device_data[config.deviceMAC]
+  and preloadedTable.policy_table.device_data[config.deviceMAC].user_consent_records
+  then
+    for _, v in pairs(preloadedTable.policy_table.device_data[config.deviceMAC].user_consent_records) do
+      if v[checkedSection] then
+        result = false
+      end
     end
-    testCasesForExternalUCS.updatePreloadedPT(updateFunc)
   end
-
-  function Test.StartSDL()
-    StartSDL(config.pathToSDL, config.ExitOnCrash)
-    os.execute("sleep 1")
+  if result == false then
+    self:FailTestCase("Section '" .. checkedSection .. "'' was found in PreloadedPT")
   end
-
-  function Test:CheckSDLStatus_2_STOPPED()
-    testCasesForExternalUCS.checkSDLStatus(self, sdl.STOPPED)
-  end
-
-  function Test:StopSDL()
-    testCasesForExternalUCS.ignitionOff(self)
-  end
-
-  function Test.RemoveLPT()
-    testCasesForExternalUCS.removeLPT()
-  end
-
-  function Test.UpdatePreloadedPT_Remove_section()
-    local updateFunc = function(preloadedTable)
-      -- whole 'device_data' section is not allowed in PreloadedPT according to data dictionary
-      preloadedTable.policy_table.device_data = nil
-    end
-    testCasesForExternalUCS.updatePreloadedPT(updateFunc)
-  end
+end
 
 --[[ Test ]]
-  commonFunctions:newTestCasesGroup("Test")
+commonFunctions:newTestCasesGroup("Test")
 
-  function Test.StartSDL()
-    StartSDL(config.pathToSDL, config.ExitOnCrash)
-    os.execute("sleep 1")
-  end
+function Test.StartSDL()
+  StartSDL(config.pathToSDL, config.ExitOnCrash)
+  os.execute("sleep 5")
+end
 
-  function Test:CheckSDLStatus_3_RUNNING()
-    testCasesForExternalUCS.checkSDLStatus(self, sdl.RUNNING)
-  end
+function Test:CheckSDLStatus_2_RUNNING()
+  testCasesForExternalUCS.checkSDLStatus(self, sdl.RUNNING)
+end
 
 --[[ Postconditions ]]
-  commonFunctions:newTestCasesGroup("Postconditions")
+commonFunctions:newTestCasesGroup("Postconditions")
 
-  function Test.StopSDL()
-    StopSDL()
-  end
+function Test.StopSDL()
+  StopSDL()
+end
 
-  function Test.RestorePreloadedFile()
-    commonPreconditions:RestoreFile("sdl_preloaded_pt.json")
-  end
+function Test.RestorePreloadedFile()
+  commonPreconditions:RestoreFile("sdl_preloaded_pt.json")
+end
 
 return Test
