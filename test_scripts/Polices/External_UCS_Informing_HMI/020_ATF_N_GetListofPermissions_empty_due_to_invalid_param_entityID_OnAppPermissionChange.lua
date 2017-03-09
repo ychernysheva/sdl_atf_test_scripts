17c7bfeb7147aeb4f4c46b8867936cdf4695876a
Valeriia Vakulenko (GitHub) <VVVakulenko@luxoft.com>
2017-03-09 19:18:08 +0200

    Update 020_ATF_N_GetListofPermissions_empty_due_to_invalid_param_entityID_OnAppPermissionChange.lua
    Update script due to first review iteration comments

--- test_scripts/Polices/External_UCS_Informing_HMI/020_ATF_N_GetListofPermissions_empty_due_to_invalid_param_entityID_OnAppPermissionChange.lua
+++ test_scripts/Polices/External_UCS_Informing_HMI/020_ATF_N_GetListofPermissions_empty_due_to_invalid_param_entityID_OnAppPermissionChange.lua
@@ -33,6 +33,20 @@ config.defaultProtocolVersion = 2
 local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
 local testCasesForPolicyTable = require('user_modules/shared_testcases/testCasesForPolicyTable')
+local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
+
+--[[Local Variables]]
+local params_invalid_data =
+{
+  {param_value = "invalidValue", comment = "String"}
+  {param_value = 1.32 comment = "Float"},
+  {param_value = {}, comment = "Empty table" },
+  {param_value = { entityType = 1, entityID = 1 }, comment = "Non-empty table"},
+  {param_value = -1, comment = "OutOfLowerBound"},
+  {param_value = 130, comment = "OutOfUpperBound" },
+  {param_value = "", comment = "Empty" },
+  {param_value = nil, desc = "Null" }
+}
 
 --[[ General Precondition before ATF start ]]
 commonFunctions:SDLForceStop()
@@ -51,53 +65,55 @@ function Test:Precondition_trigger_getting_device_consent()
   testCasesForPolicyTable:trigger_getting_device_consent(self, config.application1.registerAppInterfaceParams.appName, config.deviceMAC)
 end
 
-function Test:Precondition_PTU_and_OnAppPermissionConsent_Invalid_entityID()
-  local ptu_file_path = "files/jsons/Policies/Related_HMI_API/"
-  local ptu_file = "OnAppPermissionConsent_ptu.json"
-  
-  testCasesForPolicyTable:flow_SUCCEESS_EXTERNAL_PROPRIETARY(self, nil, nil, nil, ptu_file_path, nil, ptu_file)
-
-  EXPECT_HMINOTIFICATION("SDL.OnAppPermissionChanged",{ appID = self.applications[config.application1.registerAppInterfaceParams.appName]})
-  :Do(function(_,data)
+for i = 1, #params_invalid_data do
+  Test["Precondition_PTU_and_OnAppPermissionConsent_Invalid_"..params_invalid_data[i]] = function(self)
+    local ptu_file_path = "files/jsons/Policies/Related_HMI_API/"
+    local ptu_file = "OnAppPermissionConsent_ptu.json"
+    
+    testCasesForPolicyTable:flow_SUCCEESS_EXTERNAL_PROPRIETARY(self, nil, nil, nil, ptu_file_path, nil, ptu_file)
+    
+    EXPECT_HMINOTIFICATION("SDL.OnAppPermissionChanged",{ appID = self.applications[config.application1.registerAppInterfaceParams.appName]})
+    :Do(function(_,data)
       if (data.params.appPermissionsConsentNeeded== true) then
         local RequestIdListOfPermissions = self.hmiConnection:SendRequest("SDL.GetListOfPermissions", {appID = self.applications[config.application1.registerAppInterfaceParams.appName]})
-        EXPECT_HMIRESPONSE(RequestIdListOfPermissions,{result = {code = 0, method = "SDL.GetListOfPermissions",
-              -- allowed: If ommited - no information about User Consent is yet found for app.
-              allowedFunctions = {
-                { name = "Location", id = 156072572},
-                { name = "Notifications", id = 1809526495}
-              },
-              externalConsentStatus = {}
-            }
-          })
+          EXPECT_HMIRESPONSE(RequestIdListOfPermissions,{result = {code = 0, method = "SDL.GetListOfPermissions",
+            -- allowed: If ommited - no information about User Consent is yet found for app.
+            allowedFunctions = {
+              { name = "Location", id = 156072572},
+              { name = "Notifications", id = 1809526495}
+            },
+            externalConsentStatus = {}
+          }
+        })
         :Do(function()
-            local ReqIDGetUserFriendlyMessage = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage",
-              {language = "EN-US", messageCodes = {"AppPermissions"}})
-
-            EXPECT_HMIRESPONSE(ReqIDGetUserFriendlyMessage,
-              {result = {code = 0, messages = {{messageCode = "AppPermissions"}}, method = "SDL.GetUserFriendlyMessage"}})
-            :Do(function(_,_)
-                self.hmiConnection:SendNotification("SDL.OnAppPermissionConsent",
-                  {
-                    appID = self.applications[config.application1.registerAppInterfaceParams.appName],
-                    consentedFunctions = {
-                      { allowed = true, id = 156072572, name = "Location-1"},
-                      { allowed = true, id = 1809526495, name = "Notifications"}
-                    },
-                    externalConsentStatus = {
-                      {entityType = 13, entityID = "invalidID", status = "ON"}
-                    },
-                    source = "GUI"
-                  })
-                EXPECT_NOTIFICATION("OnPermissionsChange"):Times(0)
-                commonTestCases:DelayedExp(10000)
-              end)
+          local ReqIDGetUserFriendlyMessage = self.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage",
+          {language = "EN-US", messageCodes = {"AppPermissions"}})
+          
+          EXPECT_HMIRESPONSE(ReqIDGetUserFriendlyMessage,
+          {result = {code = 0, messages = {{messageCode = "AppPermissions"}}, method = "SDL.GetUserFriendlyMessage"}})
+          :Do(function(_,_)
+            self.hmiConnection:SendNotification("SDL.OnAppPermissionConsent",
+            {
+              appID = self.applications[config.application1.registerAppInterfaceParams.appName],
+              consentedFunctions = {
+                { allowed = true, id = 156072572, name = "Location-1"},
+                { allowed = true, id = 1809526495, name = "Notifications"}
+              },
+              externalConsentStatus = {
+                {entityType = 13, entityID = params_invalid_data[i], status = "ON"}
+              },
+              source = "GUI"
+            })
+            EXPECT_NOTIFICATION("OnPermissionsChange"):Times(0)
+            commonTestCases:DelayedExp(10000)
+          end)
         end)
       else
         commonFunctions:userPrint(31, "Wrong SDL bahavior: there are app permissions for consent, isPermissionsConsentNeeded should be true")
         return false
       end
-  end)
+    end)
+  end
 end
 
 --[[ Test ]]
@@ -105,12 +121,12 @@ commonFunctions:newTestCasesGroup("Test")
 
 function Test:TestStep_GetListofPermissions_entityID_invalid()
   local RequestIdListOfPermissions = self.hmiConnection:SendRequest("SDL.GetListOfPermissions", {appID = self.applications[config.application1.registerAppInterfaceParams.appName]})
-
+  
   EXPECT_HMIRESPONSE(RequestIdListOfPermissions, {
     code = "0",
     allowedFunctions = {
-    { name = "Location-1", id = 156072572},
-    { name = "Notifications", id = 1809526495}
+      { name = "Location", id = 156072572},
+      { name = "Notifications", id = 1809526495}
     },
     externalConsentStatus = {}
   })
