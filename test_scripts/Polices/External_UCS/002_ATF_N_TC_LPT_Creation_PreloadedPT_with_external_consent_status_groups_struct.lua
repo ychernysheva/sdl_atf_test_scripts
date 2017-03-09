248aef2149e05a217a563ca5e728d88f6faba2e3
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2017-03-09 19:45:16 +0200

    Updated according to fix implemented in ATF

--- test_scripts/Polices/External_UCS/002_ATF_N_TC_LPT_Creation_PreloadedPT_with_external_consent_status_groups_struct.lua
+++ test_scripts/Polices/External_UCS/002_ATF_N_TC_LPT_Creation_PreloadedPT_with_external_consent_status_groups_struct.lua
@@ -33,6 +33,7 @@
 --[[ General configuration parameters ]]
 config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
 config.defaultProtocolVersion = 2
+config.ExitOnCrash = false
 
 --[[ Required Shared Libraries ]]
 local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
