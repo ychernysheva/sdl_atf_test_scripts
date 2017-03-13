582a6179371bae9d10f0683e8c7e839847fe8356
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2017-03-13 15:51:03 +0200

    Updated according to review comments

--- test_scripts/Polices/External_UCS/016_ATF_N_TC_PTU_with_external_consent_status_groups_struct.lua
+++ test_scripts/Polices/External_UCS/016_ATF_N_TC_PTU_with_external_consent_status_groups_struct.lua
@@ -11,15 +11,11 @@
 -- a. consider this PTU as invalid
 -- b. do not merge this invalid PTU to LocalPT.
 --
--- Preconditions:
--- 1. Modify PreloadedPT by adding 'disallowed_by_external_consent_entities_on' section
---
 -- Steps:
 -- 1. Register app
--- 2. Send SDL.OnAppPermissionConsent (change ON -> OFF)
--- 3. Activate app
--- 4. Perform PTU
--- 5. Verify status of update
+-- 2. Activate app
+-- 3. Perform PTU (make sure 'external_consent_status_groups' exists in PTU file)
+-- 4. Verify status of update
 --
 -- Expected result:
 -- PTU fails with UPDATE_NEEDED status
@@ -34,45 +30,17 @@ config.defaultProtocolVersion = 2
 --[[ Required Shared Libraries ]]
 local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
-local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
-local json = require("modules/json")
 local testCasesForExternalUCS = require('user_modules/shared_testcases/testCasesForExternalUCS')
 
 --[[ Local variables ]]
 local appId = config.application1.registerAppInterfaceParams.appID
 local grpId = "Location-1"
 local checkedStatus = "UPDATE_NEEDED"
-
---[[ Local Functions ]]
-local function replaceSDLPreloadedPtFile()
-  local preloadedFile = commonPreconditions:GetPathToSDL() ..
-  commonFunctions:read_parameter_from_smart_device_link_ini("PreloadedPT")
-  local preloadedTable = testCasesForExternalUCS.createTableFromJsonFile(preloadedFile)
-  preloadedTable.policy_table.functional_groupings["DataConsent-2"].rpcs = json.null
-  --
-  preloadedTable.policy_table.app_policies[appId] = {
-    default_hmi = "NONE",
-    keep_context = false,
-    priority = "NONE",
-    steal_focus = false,
-    groups = {
-      "Base-4", grpId
-    }
-  }
-  preloadedTable.policy_table.functional_groupings[grpId].disallowed_by_external_consent_entities_on = {
-    {
-      entityID = 128,
-      entityType = 0
-    }
-  }
-  testCasesForExternalUCS.createJsonFileFromTable(preloadedTable, preloadedFile)
-end
+local checkedSection = "external_consent_status_groups"
 
 --[[ General Precondition before ATF start ]]
 commonFunctions:SDLForceStop()
 commonSteps:DeleteLogsFileAndPolicyTable()
-commonPreconditions:BackupFile("sdl_preloaded_pt.json")
-replaceSDLPreloadedPtFile()
 testCasesForExternalUCS.removePTS()
 
 --[[ General Settings for configuration ]]
@@ -97,15 +65,21 @@ function Test:RAI()
   testCasesForExternalUCS.registerApp(self, 1)
 end
 
-function Test:SendExternalConsent()
-  self.hmiConnection:SendNotification("SDL.OnAppPermissionConsent", { source = "GUI",
-      externalConsentStatus = {
-        { entityType = 0, entityID = 128, status = "OFF" }
-    } })
-end
-
 function Test:ActivateApp()
-  testCasesForExternalUCS.activateApp(self, 1, checkedStatus)
+  local updateFunc = function(pts)
+    pts.policy_table.device_data = {
+      [config.deviceMAC] = {
+        user_consent_records = {
+          [appId] = {
+            [checkedSection] = {
+              [grpId] = true
+            }
+          }
+        }
+      }
+    }
+  end
+  testCasesForExternalUCS.activateApp(self, 1, checkedStatus, updateFunc)
 end
 
 function Test:CheckStatus_UPDATE_NEEDED()
@@ -118,10 +92,6 @@ commonFunctions:newTestCasesGroup("Postconditions")
 
 function Test.StopSDL()
   StopSDL()
-end
-
-function Test.Postcondition_RestorePreloadedFile()
-  commonPreconditions:RestoreFile("sdl_preloaded_pt.json")
 end
 
 return Test
