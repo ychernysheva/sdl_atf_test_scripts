d0d5c9dbd2decda4e98d8e53d6e8ca1da08134b8
istoimenova <istoimenova@luxoft.com>
2017-03-14 09:49:16 +0200

    033_ATF_P_TC_navi_app_PTU_retry_sequence_finish_ForceProtectedService_OFF_RPC_encrypted_true

---
+++ test_scripts/Polices/Policies_Security/Trigger_PTU_NO_Certificate/033_ATF_P_TC_navi_app_PTU_retry_sequence_finish_ForceProtectedService_OFF_RPC_encrypted_true.lua
@@ -0,0 +1,163 @@
+---------------------------------------------------------------------------------------------
+-- Requirement summary:
+-- [PTU] [GENIVI] PolicyTableUpdate is failed by any reason and "ForceProtectedService"=OFF at .ini file
+-- [PTU] [GENIVI] SDL must start PTU for navi app right after app successfully registration
+--
+-- Description:
+-- In case SDL starts PolicyTableUpdate in case of no "certificate" at "module_config" section at LocalPT
+-- and PolicyTableUpdate is failed by any reason even after retry strategy
+-- and "ForceProtectedService" is OFF at .ini file
+-- and app sends StartService (<any_serviceType>, encypted=true) to SDL
+-- SDL must respond StartService (ACK, encrypted=false) to this mobile app
+--
+-- 1. Used preconditions:
+-- RPC SetAudioStreamingIndicator is allowed by policy
+-- ForceProtectedService is set to OFF in .ini file
+-- Navi app exists in LP, no certificate in module_config
+-- Register and activate navi application.
+-- -> SDL should trigger PTU: SDL.OnStatusUpdate(UPDATE_NEEDED)
+--
+-- 2. Performed steps
+-- 2.1 Wait PTU retry sequence to finish
+-- 2.2 Send RPC service.
+--
+-- Expected result:
+-- 1. SDL sends SDL.OnStatusUpdate(UPDATE_NEEDED)
+-- 2. SDL should return StartServiceACK, encrypt = false to RPC
+---------------------------------------------------------------------------------------------
+
+--[[ General configuration parameters ]]
+config.deviceMAC = "12ca17b49af2289436f303e0166030a21e525d266e209267433801a8fd4071a0"
+config.application1.registerAppInterfaceParams.appHMIType = {"NAVIGATION"}
+
+--[[ Required Shared libraries ]]
+local commonFunctions = require ('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
+local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
+local mobile_session = require('mobile_session')
+local testCasesForPolicyCeritificates = require('user_modules/shared_testcases/testCasesForPolicyCeritificates')
+local events = require('events')
+local Event = events.Event
+
+--[[ Local variables ]]
+local time_wait = (60 + 61 + 62 + 62 + 62 + 62)*1000
+
+--[[ General Precondition before ATF start ]]
+commonPreconditions:BackupFile("smartDeviceLink.ini")
+commonFunctions:write_parameter_to_smart_device_link_ini("ForceProtectedService", "Non")
+testCasesForPolicyCeritificates.update_preloaded_pt(config.application1.registerAppInterfaceParams.appID, false, {1,1,1,1,1})
+testCasesForPolicyCeritificates.create_ptu_certificate_exist(false, true)
+commonSteps:DeletePolicyTable()
+commonSteps:DeleteLogsFiles()
+
+--[[ General Settings for configuration ]]
+Test = require('user_modules/connecttest_resumption')
+require('user_modules/AppTypes')
+
+--[[ Preconditions ]]
+commonFunctions:newTestCasesGroup("Preconditions")
+
+function Test:Precondition_connectMobile()
+  self:connectMobile()
+end
+
+function Test:Precondition_StartSession()
+  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+  self.mobileSession:StartService(7)
+end
+
+function Test:Precondition_PTU_Trigger()
+  local CorIdRegister = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+
+  EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", { application = { appName = config.application1.registerAppInterfaceParams.appName }})
+  :Do(function(_,data) self.applications[config.application1.registerAppInterfaceParams.appName] = data.params.application.appID end)
+
+  EXPECT_RESPONSE(CorIdRegister, { success = true, resultCode = "SUCCESS" })
+  EXPECT_NOTIFICATION("OnHMIStatus", { systemContext = "MAIN", hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE"})
+
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status="UPDATE_NEEDED"}, {status = "UPDATING"}):Times(2)
+end
+
+function Test:Precondition_ActivateApp()
+  commonSteps:ActivateAppInSpecificLevel(self, self.applications[config.application1.registerAppInterfaceParams.appName])
+  EXPECT_NOTIFICATION("OnHMIStatus", {systemContext = "MAIN", hmiLevel = "FULL"})
+end
+
+--[[ Test ]]
+commonFunctions:newTestCasesGroup("Test")
+
+function Test.TestStep_PolicyTableUpdate_retry_sequence_elapse()
+  print("Wait retry sequence to elapse")
+
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
+    {status="UPDATE_NEEDED"}, {status = "UPDATING"},
+    {status="UPDATE_NEEDED"}, {status = "UPDATING"},
+    {status="UPDATE_NEEDED"}, {status = "UPDATING"},
+    {status="UPDATE_NEEDED"}, {status = "UPDATING"},
+    {status="UPDATE_NEEDED"})
+  :Times(9)
+  :Timeout(time_wait)
+  :Do(function(exp, data) print("exp: ".. exp.occurences) print("data = "..data.params.status)end)
+
+  commonTestCases:DelayedExp(time_wait)
+end
+
+function Test:TestStep_RPC_ACK_encrypt_false()
+  self.mobileSession.correlationId = self.mobileSession.correlationId + 1
+
+  local msg = {
+    serviceType = 7,
+    frameInfo = 0,
+    rpcType = 0,
+    rpcFunctionId = 48,
+    encryption = true,
+    rpcCorrelationId = self.mobileSession.correlationId,
+    payload = '{ "audioStreamingIndicator" : "PAUSE" }'
+  }
+  self.mobileSession:Send(msg)
+
+  local startserviceEvent = Event()
+  startserviceEvent.matches =
+  function(_, data)
+    return ( data.frameType == 0 and data.serviceType == 7)
+  end
+
+  self.mobileSession:ExpectEvent(startserviceEvent, "Service 7: StartServiceACK")
+  :ValidIf(function(_, data)
+      if data.frameInfo == 2 then
+        if(data.encryption == true) then
+          commonFunctions:printError("Service 7: StartService ACK, encryption: true is received")
+          return false
+        else
+          print("Service 7: StartServiceACK, encryption: false")
+          return true
+        end
+      elseif data.frameInfo == 3 then
+        commonFunctions:printError("Service 7: StartService NACK is received")
+        return false
+      else
+        commonFunctions:printError("Service 7: StartServiceACK/NACK is not received at all.")
+        return false
+      end
+    end)
+
+  EXPECT_HMICALL("UI.SetAudioStreamingIndicator"):Times(0)
+  EXPECT_RESPONSE(msg.rpcCorrelationId, { success = false, resultCode = "REJECTED"})
+
+end
+
+--[[ Postconditions ]]
+commonFunctions:newTestCasesGroup("Postconditions")
+
+function Test.Postcondition_Restore_files()
+  os.execute( " rm -f files/ptu_certificate_exist.json" )
+  commonPreconditions:RestoreFile("smartDeviceLink.ini")
+  commonPreconditions:RestoreFile("sdl_preloaded_pt.json")
+end
+
+function Test.Postcondition_Stop()
+  StopSDL()
+end
+
+return Test
