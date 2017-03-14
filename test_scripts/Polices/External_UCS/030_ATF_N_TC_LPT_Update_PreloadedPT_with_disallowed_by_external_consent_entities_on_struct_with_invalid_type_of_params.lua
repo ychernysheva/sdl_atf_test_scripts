17c69db47385240728c846e91b4f27ddce32efc1
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2017-03-14 17:11:29 +0200

    Update according to review comments

--- test_scripts/Polices/External_UCS/030_ATF_N_TC_LPT_Update_PreloadedPT_with_disallowed_by_external_consent_entities_on_struct_with_invalid_type_of_params.lua
+++ test_scripts/Polices/External_UCS/030_ATF_N_TC_LPT_Update_PreloadedPT_with_disallowed_by_external_consent_entities_on_struct_with_invalid_type_of_params.lua
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------------------------
 -- Requirement summary:
--- [Policies] External UCS: PreloadedPT without "disallowed_by_external_consent_entities_on" struct
+-- [Policies] External UCS: PreloadedPT with "disallowed_by_external_consent_entities_on" struct
 --
 -- Description:
 -- In case:
@@ -15,7 +15,7 @@
 -- Preconditions:
 -- 1. Stop SDL (Ignition Off)
 -- 2. Modify PreloadedPolicyTable (add 'disallowed_by_external_consent_entities_on' section)
--- Define not valid data type for entityType and entityId parameters
+-- Define not valid data type for entityType and/or entityId parameters
 -- 3. Initiate Local Policy Table update by setting 'preloaded_date' parameter
 --
 -- Steps:
