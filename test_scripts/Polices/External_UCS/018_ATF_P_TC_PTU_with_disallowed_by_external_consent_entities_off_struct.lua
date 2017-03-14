6bb3cbbfb2a4a522f30e2129db26c0eb52eaec37
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2017-03-14 16:19:18 +0200

    Update according to review comments

--- test_scripts/Polices/External_UCS/018_ATF_P_TC_PTU_with_disallowed_by_external_consent_entities_off_struct.lua
+++ test_scripts/Polices/External_UCS/018_ATF_P_TC_PTU_with_disallowed_by_external_consent_entities_off_struct.lua
@@ -39,6 +39,7 @@ config.defaultProtocolVersion = 2
 --[[ Required Shared Libraries ]]
 local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
 local commonSteps = require('user_modules/shared_testcases/commonSteps')
+local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
 local testCasesForExternalUCS = require('user_modules/shared_testcases/testCasesForExternalUCS')
 
 --[[ Local variables ]]
@@ -57,6 +58,25 @@ require('user_modules/AppTypes')
 
 --[[ Preconditions ]]
 commonFunctions:newTestCasesGroup("Preconditions")
+
+function Test:CheckPreloadedPT()
+  local preloadedFile = commonPreconditions:GetPathToSDL() .. "sdl_preloaded_pt.json"
+  local preloadedTable = testCasesForExternalUCS.createTableFromJsonFile(preloadedFile)
+  local result = true
+  if preloadedTable
+  and preloadedTable.policy_table
+  and preloadedTable.policy_table.functional_groupings
+  then
+    for _, v in pairs(preloadedTable.policy_table.functional_groupings) do
+      if v[checkedSection] then
+        result = false
+      end
+    end
+  end
+  if result == false then
+    self:FailTestCase("Section '" .. checkedSection .. "'' was found in PreloadedPT")
+  end
+end
 
 function Test:ConnectMobile()
   self:connectMobile()
