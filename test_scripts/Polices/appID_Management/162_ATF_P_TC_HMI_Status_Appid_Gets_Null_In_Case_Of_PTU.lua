5aea88e8c61a429a7cfa1fa0c5d245d1a9853277
pold500 <paveld500@gmail.com>
2017-04-11 12:16:27 +0300

    Fixed scripts due to code review comments

--- test_scripts/Polices/appID_Management/162_ATF_P_TC_HMI_Status_Appid_Gets_Null_In_Case_Of_PTU.lua
+++ test_scripts/Polices/appID_Management/162_ATF_P_TC_HMI_Status_Appid_Gets_Null_In_Case_Of_PTU.lua
@@ -77,8 +77,8 @@ end
 commonFunctions:newTestCasesGroup("Test")
 function Test:TestStep_UpdatePolicy()
   testCasesForPolicyAppIdManagament:updatePolicyTable(self, "files/jsons/Policies/appID_Management/ptu_013_2.json")
-  EXPECT_HMINOTIFICATION("SDL.OnAppPermissionChanged", { appID =  data.params.application.appID, appRevoked =  true})
-  EXPECT_HMICALL("BasicCommunication.ActivateApp")
+  EXPECT_HMINOTIFICATION("SDL.OnAppPermissionChanged", { appID =  HMIAppID, appRevoked =  true})
+  EXPECT_HMICALL("BasicCommunication.ActivateApp", { level = "NONE" })
 end
 
 function Test.Postcondition_Stop()
