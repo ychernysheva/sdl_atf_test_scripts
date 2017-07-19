ebfff36dabc7701ce18b223dff79b2d44b20b283
Dmytro Boltovskyi <dboltovskyi@luxoft.com>
2017-07-19 20:28:53 +0300

    ButtonPress: Add new scripts

---
+++ test_scripts/RC/ButtonPress/008.lua
@@ -0,0 +1,51 @@
+---------------------------------------------------------------------------------------------------
+-- RPC: ButtonPress
+-- Script: 008
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local commonRC = require('test_scripts/RC/commonRC')
+local json = require('modules/json')
+
+--[[ Local Variables ]]
+local modules = { "CLIMATE", "RADIO" }
+
+--[[ Local Functions ]]
+local function getDataForModule(pModuleType, self)
+  local cid = self.mobileSession:SendRPC("ButtonPress", {
+    moduleType = pModuleType,
+    buttonName = commonRC.getButtonNameByModule(pModuleType),
+    buttonPressMode = "SHORT"
+  })
+
+  EXPECT_HMICALL("Buttons.ButtonPress", {
+    appID = self.applications["Test Application"],
+    moduleType = pModuleType,
+    buttonName = commonRC.getButtonNameByModule(pModuleType),
+    buttonPressMode = "SHORT"
+  })
+  :Do(function(_, data)
+      self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+    end)
+
+  EXPECT_RESPONSE(cid, { success = true, resultCode = "SUCCESS" })
+end
+
+local function ptu_update_func(tbl)
+  tbl.policy_table.app_policies[config.application1.registerAppInterfaceParams.appID].moduleType = json.EMPTY_ARRAY
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", commonRC.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", commonRC.start)
+runner.Step("RAI, PTU", commonRC.rai_ptu, { ptu_update_func })
+
+runner.Title("Test")
+
+for _, mod in pairs(modules) do
+  runner.Step("ButtonPress " .. mod, getDataForModule, { mod })
+end
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", commonRC.postconditions)
---
+++ test_scripts/RC/ButtonPress/011.lua
@@ -0,0 +1,49 @@
+---------------------------------------------------------------------------------------------------
+-- RPC: ButtonPress
+-- Script: 011
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local commonRC = require('test_scripts/RC/commonRC')
+local commonTestCases = require('user_modules/shared_testcases/commonTestCases')
+
+--[[ Local Variables ]]
+local modules = { "CLIMATE", "RADIO" }
+
+--[[ Local Functions ]]
+local function getDataForModule(pModuleType, self)
+  local cid = self.mobileSession:SendRPC("ButtonPress", {
+    moduleType = pModuleType,
+    buttonName = commonRC.getButtonNameByModule(pModuleType),
+    buttonPressMode = "SHORT"
+  })
+
+  EXPECT_HMICALL("Buttons.ButtonPress", {
+    appID = self.applications["Test Application"],
+    moduleType = pModuleType,
+    buttonName = commonRC.getButtonNameByModule(pModuleType),
+    buttonPressMode = "SHORT"
+  })
+  :Do(function(_, _)
+    -- HMI does not respond
+    end)
+
+  EXPECT_RESPONSE(cid, { success = false, resultCode = "GENERIC_ERROR"})
+
+  commonTestCases:DelayedExp(11000)
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", commonRC.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", commonRC.start)
+runner.Step("RAI, PTU", commonRC.rai_ptu)
+
+runner.Title("Test")
+
+for _, mod in pairs(modules) do
+  runner.Step("ButtonPress " .. mod, getDataForModule, { mod })
+end
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", commonRC.postconditions)
