e2de9d52e2a1ce942034f440ca5b63c37e4dfce0
GetmanetsIrina <igetmanets@luxoft.com>
2017-12-04 14:46:52 +0200

    Script for reproducing 1880 issue

---
+++ test_scripts/Defects/4_5/1880_DefaultAndRPCTimeoutGENERIC_ERROR.lua
@@ -0,0 +1,375 @@
+---------------------------------------------------------------------------------------------
+-- In case
+-- SDL transfers *RPC with own timeout from mobile app to HMI (please see list with impacted *RPCs below)
+-- and HMI does NOT respond during <DefaultTimeout> + <*RPCs_own_timeout> (please see APPLINK-27495)
+-- SDL must:
+-- respond 'GENERIC_ERROR, success:false' to mobile app
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local commonDefects = require("test_scripts/Defects/4_5/commonDefects")
+
+--[[ Local Variables ]]
+local DefaultTimeout = 10000
+
+local AlertRequestParams = {
+  alertText1 = "alertText1",
+  duration = 7000
+}
+
+local AlertRequestParamsWithoutDuration = {
+  alertText1 = "alertText1",
+  ttsChunks = {
+    {
+      text = "TTSChunk",
+      type = "TEXT",
+    }
+  },
+}
+
+local SliderRequetsParams = {
+  numTicks = 3,
+  position = 2,
+  sliderHeader ="sliderHeader",
+  sliderFooter = {"1", "2", "3"},
+  timeout = 7000
+}
+
+local SliderRequetsParamsWithoutTimeout = {
+  numTicks = 3,
+  position = 2,
+  sliderHeader ="sliderHeader",
+  sliderFooter = {"1", "2", "3"},
+}
+
+local ScrollableMessageRequestParamsWithSoftButtons = {
+  scrollableMessageBody = "abc",
+  softButtons =
+  {
+    {
+      softButtonID = 1,
+      text = "Button1",
+      type = "IMAGE",
+      image =
+      {
+        value = "icon.png",
+        imageType = "DYNAMIC"
+      },
+      isHighlighted = false,
+      systemAction = "DEFAULT_ACTION"
+    },
+    {
+      softButtonID = 2,
+      text = "Button2",
+      type = "IMAGE",
+      image =
+      {
+        value = "icon.png",
+        imageType = "DYNAMIC"
+      },
+      isHighlighted = false,
+      systemAction = "DEFAULT_ACTION"
+    }
+  },
+  timeout = 7000
+}
+
+local ScrollableMessageRequestParamsWithoutSoftButtons = {
+  scrollableMessageBody = "abc",
+  timeout = 3000
+}
+
+local ScrollableMessageRequestParamsWithoutTimeout = {
+  scrollableMessageBody = "abc",
+}
+
+local PerformInteractionRequestParamsBOTH = {
+  initialText = "StartPerformInteraction",
+  initialPrompt = {
+    {
+      text = "Make your choice",
+      type = "TEXT"
+    }
+  },
+  interactionMode = "BOTH",
+  interactionChoiceSetIDList = {100},
+  helpPrompt = {
+    {
+      text = "Help Prompt",
+      type = "TEXT",
+    }
+  },
+  timeoutPrompt = {
+    {
+      text = " Time out ",
+      type = "TEXT",
+    }
+  },
+  timeout = 7000,
+  vrHelp = {
+    {
+      text = " New VRHelp ",
+      position = 1,
+      image = {
+        value = "icon.png",
+        imageType = "STATIC",
+      }
+    }
+  },
+  interactionLayout = "ICON_ONLY"
+}
+
+local PerformInteractionRequestParamsMANUAL = {
+  initialText = "StartPerformInteraction",
+  interactionMode = "MANUAL_ONLY",
+  initialPrompt = {
+    {
+      text = "Make your choice",
+      type = "TEXT"
+    }
+  },
+  interactionChoiceSetIDList = {100},
+  timeout = 8000,
+  interactionLayout = "LIST_ONLY"
+}
+
+local PerformInteractionRequestParamsVR = {
+  initialText = "StartPerformInteraction",
+  initialPrompt = {
+    {
+      text = "Make your choice",
+      type = "TEXT"
+    }
+  },
+  interactionMode = "VR_ONLY",
+  interactionChoiceSetIDList = {100},
+  helpPrompt = {
+    {
+      text = "Help Prompt",
+      type = "TEXT",
+    }
+  },
+  timeoutPrompt = {
+    {
+      text = "Time out",
+      type = "TEXT",
+    }
+  },
+  timeout = 12000,
+  vrHelp = {
+    {
+      text = "New VRHelp",
+      position = 1,
+      image = {
+        value = "icon.png",
+        imageType = "STATIC",
+      }
+    }
+  },
+  interactionLayout = "ICON_ONLY"
+}
+
+--[[ Local Functions ]]
+local function Alert(params, self)
+  local AlertDuration
+  if params.duration then
+    AlertDuration = params.duration
+  else
+    -- deration default valur from mobile API
+    AlertDuration = 5000
+  end
+  local RespTimeout = DefaultTimeout + AlertDuration
+  local RequestTime
+  local RespTime
+  local TimeBetweenReqRes
+
+  if params.ttsChunks then
+    EXPECT_HMICALL("TTS.Speak")
+    :Do(function(_,data)
+        local function SpeakResp()
+          self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+        end
+        RUN_AFTER(SpeakResp, 2000)
+      end)
+  end
+
+  local cid = self.mobileSession1:SendRPC("Alert", params)
+  RequestTime = timestamp()
+
+  EXPECT_HMICALL("UI.Alert")
+
+  self.mobileSession1:ExpectResponse(cid, { success = false, resultCode = "GENERIC_ERROR" })
+  :Timeout(RespTimeout + 1000)
+  :ValidIf(function()
+      RespTime = timestamp()
+      TimeBetweenReqRes = RespTime - RequestTime
+      if
+      TimeBetweenReqRes > RespTimeout - 1000 and
+      TimeBetweenReqRes < RespTimeout + 1000 then
+        return true
+      else
+        return false, "SDL triggers timeout earlier then expected(".. tostring(RespTimeout) .." sec), after " ..
+        tostring(TimeBetweenReqRes) ..
+        " sec.\n SDL must use Alert duration + default timeout in case of absence softButtons."
+      end
+    end)
+end
+
+local function Slider(params, self)
+  local SliderDuration
+  if params.timeout then
+    SliderDuration = params.timeout
+  else
+    -- timeout default value from mobile API
+    SliderDuration = 10000
+  end
+  local RespTimeout = DefaultTimeout + SliderDuration
+  local RequestTime
+  local RespTime
+  local TimeBetweenReqRes
+  local cid = self.mobileSession1:SendRPC("Slider", params)
+  RequestTime = timestamp()
+  EXPECT_HMICALL("UI.Slider")
+  self.mobileSession1:ExpectResponse(cid, {success = false, resultCode = "GENERIC_ERROR"})
+  :Timeout(RespTimeout + 1000)
+  :ValidIf(function()
+      RespTime = timestamp()
+      TimeBetweenReqRes = RespTime - RequestTime
+      if
+      TimeBetweenReqRes > RespTimeout - 1000 and
+      TimeBetweenReqRes < RespTimeout + 1000 then
+        return true
+      else
+        return false, "SDL triggers timeout earlier then expected(".. tostring(RespTimeout) .." sec), after " ..
+        tostring(TimeBetweenReqRes) .. " sec. \n SDL must use Slider timeout + default timeout."
+      end
+    end)
+end
+
+local function ScrollableMessage(params, self)
+  local ScrMesDuration
+  if params.timeout then
+    ScrMesDuration = params.timeout
+  else
+    -- timeout default value from mobile API
+    ScrMesDuration = 30000
+  end
+  local RespTimeout = DefaultTimeout + ScrMesDuration
+  local RequestTime
+  local RespTime
+  local TimeBetweenReqRes
+  local cid = self.mobileSession1:SendRPC("ScrollableMessage", params)
+  RequestTime = timestamp()
+  EXPECT_HMICALL("UI.ScrollableMessage")
+  self.mobileSession1:ExpectResponse(cid, {success = false, resultCode = "GENERIC_ERROR"})
+  :Timeout(RespTimeout + 1000)
+  :ValidIf(function()
+      RespTime = timestamp()
+      TimeBetweenReqRes = RespTime - RequestTime
+      if
+      TimeBetweenReqRes > RespTimeout - 1000 and
+      TimeBetweenReqRes < RespTimeout + 1000 then
+        return true
+      else
+        return false, "SDL triggers timeout earlier then expected(".. tostring(RespTimeout) .." sec), after " ..
+        tostring(TimeBetweenReqRes) .. " sec. \n SDL must use ScrollableMessage timeout + default timeout."
+      end
+    end)
+end
+
+local function CreateInteractionChoiceSet(self)
+  local cid = self.mobileSession1:SendRPC("CreateInteractionChoiceSet", {
+      interactionChoiceSetID = 100,
+      choiceSet = {
+        {
+          choiceID = 100,
+          menuName ="Choice100",
+          vrCommands = {
+            "Choice100",
+          }
+        }
+      }
+    })
+  EXPECT_HMICALL("VR.AddCommand")
+  :Do(function(_,data)
+      self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+    end)
+  self.mobileSession1:ExpectResponse(cid, {success = true, resultCode = "SUCCESS"})
+end
+
+local function PerformInteraction(params, self)
+  local PIDuration
+  local RequestTime
+  local RespTime
+  local TimeBetweenReqRes
+  local TimeToResponseForVR = 2000
+  local RespTimeout
+  if params.interactionMode ~= "VR_ONLY" then
+    if params.timeout then
+      PIDuration = params.timeout
+      RespTimeout = DefaultTimeout + 2*PIDuration
+    else
+      PIDuration = 10000
+      RespTimeout = DefaultTimeout + 2*PIDuration
+    end
+  else
+    RespTimeout = params.timeout
+  end
+  local cid = self.mobileSession1:SendRPC("PerformInteraction", params)
+  EXPECT_HMICALL("VR.PerformInteraction")
+  :Do(function(_,data)
+      local function RespVR()
+        self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+        RequestTime = timestamp()
+      end
+      RUN_AFTER(RespVR, TimeToResponseForVR)
+    end)
+  EXPECT_HMICALL("UI.PerformInteraction")
+  self.mobileSession1:ExpectResponse(cid, {success = false, resultCode = "GENERIC_ERROR"})
+  :Timeout(RespTimeout + 3000)
+  :ValidIf(function()
+      RespTime = timestamp()
+      TimeBetweenReqRes = RespTime - RequestTime
+      if
+      TimeBetweenReqRes > RespTimeout - 1000 and
+      TimeBetweenReqRes < RespTimeout + 1000 then
+        return true
+      else
+        return false, "SDL triggers timeout earlier then expected(".. tostring(RespTimeout) .." sec), after " ..
+        tostring(TimeBetweenReqRes) .. " sec. \n SDL must use PI timeout + default timeout."
+      end
+    end)
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", commonDefects.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", commonDefects.start)
+runner.Step("RAI, PTU", commonDefects.rai_ptu)
+runner.Step("Activate App", commonDefects.activate_app)
+runner.Step("Upload file", commonDefects.putFile, {"icon.png"})
+
+runner.Title("Test")
+runner.Step("Alert_default_timeout_and_Alert_timeout", Alert, { AlertRequestParams })
+runner.Step("Alert_default_timeout", Alert, { AlertRequestParamsWithoutDuration })
+
+runner.Step("Slider_default_timeout_and_Slider_timeout", Slider, { SliderRequetsParams })
+runner.Step("Slider_default_timeout", Slider, { SliderRequetsParamsWithoutTimeout })
+
+runner.Step("ScrollableMessage_default_timeout_and_ScrMes_timeout_with_softButtons", ScrollableMessage,
+  { ScrollableMessageRequestParamsWithSoftButtons })
+runner.Step("ScrollableMessage_default_timeout_and_ScrMes_timeout_without_softButtons", ScrollableMessage,
+  { ScrollableMessageRequestParamsWithoutSoftButtons })
+runner.Step("ScrollableMessage_default_timeout", ScrollableMessage,
+  { ScrollableMessageRequestParamsWithoutTimeout })
+
+runner.Step("CreateInteractionChoiceSet", CreateInteractionChoiceSet)
+runner.Step("PerformInteraction_default_timeout_and_PI_timeout_BOTH", PerformInteraction,
+  { PerformInteractionRequestParamsBOTH })
+runner.Step("PerformInteraction_default_timeout_and_PI_timeout_MANUAL", PerformInteraction,
+  { PerformInteractionRequestParamsMANUAL })
+runner.Step("PerformInteraction_timeout_VR", PerformInteraction, { PerformInteractionRequestParamsVR })
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", commonDefects.postconditions)
+
