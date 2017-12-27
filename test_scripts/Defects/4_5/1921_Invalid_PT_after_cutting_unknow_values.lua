8a081f94d518811469920f0e8f622813b02937db
GetmanetsIrina <igetmanets@luxoft.com>
2017-12-27 13:51:29 +0200

    Updated script

--- test_scripts/Defects/4_5/1921_Invalid_PT_after_cutting_unknow_values.lua
+++ test_scripts/Defects/4_5/1921_Invalid_PT_after_cutting_unknow_values.lua
@@ -13,23 +13,43 @@
 local runner = require('user_modules/script_runner')
 local commonDefects = require('test_scripts/Defects/4_5/commonDefects')
 local commonFunctions = require('user_modules/shared_testcases/commonFunctions')
+local commonSteps = require ('user_modules/shared_testcases/commonSteps')
+local json = require("modules/json")
 
 --[[ Local Functions ]]
-local isCuttingUnknownError = false
 local isError
 local ErrorMessage
+-- Path to policy table snapshot
+local pathToPTS = commonFunctions:read_parameter_from_smart_device_link_ini("SystemFilesPath") .. "/"
+.. commonFunctions:read_parameter_from_smart_device_link_ini("PathToSnapshot")
 
 --[[ Local Functions ]]
+--[[ @ptsToTable: decode snapshot from json to table
+--! @parameters:
+--! pts_f - file for decode
+--! @return: created table from file
+--]]
+local function ptsToTable(pts_f)
+  local f = io.open(pts_f, "r")
+  local content = f:read("*all")
+  f:close()
+  return json.decode(content)
+end
+
+--[[ @ptuUpdateFunc: update table for PTU
+--! @parameters:
+--! tbl - table for update
+--! @return: none
+--]]
 local function ptuUpdateFunc(tbl)
   local VDgroup = {
     rpcs = {
       GetVehicleData = {
         hmi_levels = {"BACKGROUND", "FULL", "LIMITED"},
-        parameters = {"gps"}
+        parameters = {"gps", "newParameter"}
       },
       SubscribeVehicleData = {
         hmi_levels = {"BACKGROUND", "FULL", "LIMITED"},
-        newParameter = {"value"}
       },
       UnknownAPI = {
         hmi_levels = {"BACKGROUND", "FULL", "LIMITED"},
@@ -45,16 +65,20 @@ local function ptuUpdateFunc(tbl)
   {"Base-4", "NewTestCaseGroup"}
 end
 
+--[[ @NotValidPtuUpdateFunc: update table for PTU with invalid content
+--! @parameters:
+--! tbl - table for update
+--! @return: none
+--]]
 local function NotValidPtuUpdateFunc(tbl)
   local VDgroup = {
     rpcs = {
       GetVehicleData = {
         hmi_levels = {"BACKGROUND", "FULL", "LIMITED"},
-        parameters = {"gps"}
+        parameters = {"gps", "newParameter"}
       },
       SubscribeVehicleData = {
         hmi_levels = {"BACKGROUND", "FULL", "LIMITED"},
-        newParameter = {"value"}
       },
       UnknownAPI = {
         hmi_levels = {"BACKGROUND", "FULL", "LIMITED"},
@@ -70,11 +94,22 @@ local function NotValidPtuUpdateFunc(tbl)
   {"Base-4", "NewTestCaseGroup"}
 end
 
+--[[ @ErrorOccurred: set error status and write Error message
+--! @parameters:
+--! Message - Error message
+--! @return: none
+--]]
 local function ErrorOccurred(Message)
   isError = true
   ErrorMessage = ErrorMessage .. Message
 end
 
+--[[ @CheckCuttingUnknowValues: Perform app registration, PTU and check absence of unknown values in
+--! OnPermissionsChange notification
+--! @parameters:
+--! self - test object
+--! @return: none
+--]]
 local function CheckCuttingUnknowValues(self)
   commonDefects.rai_ptu_n_without_OnPermissionsChange(1, ptuUpdateFunc,self)
   self.mobileSession1:ExpectNotification("OnPermissionsChange")
@@ -85,11 +120,10 @@ local function CheckCuttingUnknowValues(self)
       if #data.payload.permissionItem ~= 0 then
         for i=1, #data.payload.permissionItem do
           if data.payload.permissionItem[i].rpcName == "UnknownAPI" then
-            isCuttingUnknownError = true
             ErrorOccurred(" OnPermissionsChange contains unknown_RPC value.\n")
           end
-          if data.payload.permissionItem[i].newParameter then
-            isCuttingUnknownError = true
+          if data.payload.permissionItem[i].parameterPermissions.allowed["newParameter"] or
+          data.payload.permissionItem[i].parameterPermissions.userDisallowed["newParameter"] then
             ErrorOccurred(" OnPermissionsChange contains unknown_parameter value.\n")
           end
         end
@@ -102,18 +136,56 @@ local function CheckCuttingUnknowValues(self)
         return true
       end
     end)
-  commonFunctions:userPrint(33, "Check PTU content of PTU attempt in SDL log by message 'PTU content is'.\n"
-    .. "'policy_table' must not contain 'newParameter' and 'UnknownAPI' values.")
 end
 
+--[[ @removeSnapshotAndTriggerPTUFromHMI: Remove snapshot and trigger PTU from HMI for creation new snapshot,
+--! check absence of unknown parameters in snapshot
+--! @parameters:
+--! self - test object
+--! @return: none
+--]]
+local function removeSnapshotAndTriggerPTUFromHMI(self)
+  -- remove Snapshot
+  os.execute("rm -f " .. pathToPTS)
+  -- expect PolicyUpdate request on HMI side
+  EXPECT_HMICALL("BasicCommunication.PolicyUpdate", { file = pathToPTS })
+  :Do(function()
+      if (commonSteps:file_exists(pathToPTS) == false) then
+        self:FailTestCase(pathToPTS .. " is not created")
+      else
+        local isErrorInvalidPT = false
+        local ErrorMessageInvalidPT = ""
+        local pts = ptsToTable(pathToPTS)
+        local isUnknownAPI = pts.policy_table.functional_groupings.NewTestCaseGroup.rpcs["UnknownAPI"]
+        local isnewParameter = pts.policy_table.functional_groupings.NewTestCaseGroup.rpcs.GetVehicleData.parameters["newParameter"]
+        if isUnknownAPI then
+          isErrorInvalidPT = true
+          ErrorMessageInvalidPT = ErrorMessageInvalidPT .. "Snapshot contains UnknownAPI\n"
+        end
+        if isnewParameter then
+          isErrorInvalidPT = true
+          ErrorMessageInvalidPT = ErrorMessageInvalidPT .. "Snapshot contains newParameter for GetVehicleData RPC\n"
+        end
+        if isErrorInvalidPT == true then
+          self:FailTestCase(ErrorMessageInvalidPT)
+        end
+      end
+    end)
+  -- Sending OnPolicyUpdate notification form HMI
+  self.hmiConnection:SendNotification("SDL.OnPolicyUpdate", { })
+  -- Expect OnStatusUpdate notifications on HMI side
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
+    { status = "UPDATE_NEEDED" }, { status = "UPDATING" })
+  :Times(2)
+end
+
+--[[ @InvalidPTAfterCutingUnknownValues: Unsuccessful policy table update
+--! @parameters:
+--! self - test object
+--! @return: none
+--]]
 local function InvalidPTAfterCutingUnknownValues(self)
-  if isCuttingUnknownError == true then
-    self:FailTestCase("Unknown values are not cut from PT, so test case is not executed.")
-  else
-    commonDefects.UnsuccessPTU(NotValidPtuUpdateFunc,self)
-    commonFunctions:userPrint(33, "Check PTU content of second PTU attempt in SDL log by message" ..
-    " 'PTU content is'.\n'policy_table' must not contain 'newParameter' and 'UnknownAPI' values.")
-  end
+  commonDefects.UnsuccessPTU(NotValidPtuUpdateFunc,self)
 end
 
 --[[ Scenario ]]
@@ -123,8 +195,8 @@ runner.Step("Start SDL, HMI, connect Mobile, start Session", commonDefects.start
 
 runner.Title("Test")
 runner.Step("Check_cutting_unknown_values_from_PT", CheckCuttingUnknowValues)
+runner.Step("Remove Snapshot and trigger PTU, check new created PTS", removeSnapshotAndTriggerPTUFromHMI)
 runner.Step("Invalid_PTU_after_cuting_unknown_values", InvalidPTAfterCutingUnknownValues)
 
 runner.Title("Postconditions")
 runner.Step("Stop SDL", commonDefects.postconditions)
-
