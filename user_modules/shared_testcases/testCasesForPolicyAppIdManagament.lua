2619d6dc3806cc4b5924ed361bd1f0d2a5ae0aeb
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2018-02-28 17:58:30 +0200

    Change SDL protocol version to 2

--- user_modules/shared_testcases/testCasesForPolicyAppIdManagament.lua
+++ user_modules/shared_testcases/testCasesForPolicyAppIdManagament.lua
@@ -1,54 +1,53 @@
 ---------------------------------------------------------------------------------------------
 -- Policy: AppID Management common module
 ---------------------------------------------------------------------------------------------
+config.defaultProtocolVersion = 2
 
 local common = {}
-
 local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
 
-local policy_file_path = commonFunctions:read_parameter_from_smart_device_link_ini("SystemFilesPath")
-local policy_file_name = "PolicyTableUpdate"
-
-----------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
 -- The function is used only in case when PTU PROPRIETARY should have as result: UP_TO_DATE
 -- The funcion will be used when PTU is triggered.
--- 1. It is assumed that notification is recevied: EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
+-- 1. It is assumed that notification is recevied:
+-- EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", {status = "UPDATE_NEEDED"})
 -- 2. It is assumed that request/response is received: EXPECT_HMICALL("BasicCommunication.PolicyUpdate",
 -- 3. Function will use default endpoints
--- Difference with PROPRIETARY flow is clarified in "Can you clarify is PTU flows for External_Proprietary and Proprietary have differences?"
+-- Difference with PROPRIETARY flow is clarified in "Can you clarify is PTU flows for External_Proprietary
+-- and Proprietary have differences?"
 -- But this should be checked in appropriate scripts
 --TODO(istoimenova): functions with External_Proprietary should be merged at review of common functions.
 function common:updatePolicyTable(test, file)
   EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate", { status = "UPDATING" }, { status = "UP_TO_DATE" }):Times(2)
   local requestId = test.hmiConnection:SendRequest("SDL.GetURLS", { service = 7 })
-  EXPECT_HMIRESPONSE(requestId, {result = {code = 0, method = "SDL.GetURLS", urls = {{url = "http://policies.telematics.ford.com/api/policies"}}}})
-  :Do(function(_, _)
-      test.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest", { requestType = "PROPRIETARY", fileName = policy_file_name })
-
+  EXPECT_HMIRESPONSE(requestId, {
+      result = {
+        code = 0,
+        method = "SDL.GetURLS",
+        urls = {
+          {
+            url = "http://policies.telematics.ford.com/api/policies"
+          }
+        }
+      }
+    })
+  :Do(function()
+    local pts_file_name = commonFunctions:read_parameter_from_smart_device_link_ini("SystemFilesPath") .. "/"
+      .. commonFunctions:read_parameter_from_smart_device_link_ini("PathToSnapshot")
+      test.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest", {
+          requestType = "PROPRIETARY",
+          fileName = pts_file_name
+        })
       EXPECT_NOTIFICATION("OnSystemRequest", { requestType = "PROPRIETARY" })
-      :Do(function(_, _)
-
-          local corIdSystemRequest = test.mobileSession:SendRPC("SystemRequest",
-            {
-              requestType = "PROPRIETARY",
-              fileName = policy_file_name
-            },
-            file)
-          EXPECT_HMICALL("BasicCommunication.SystemRequest",{requestType = "PROPRIETARY", fileName = policy_file_path.."/"..policy_file_name },file)
+      :Do(function()
+          local corIdSystemRequest = test.mobileSession:SendRPC("SystemRequest", { requestType = "PROPRIETARY" }, file)
+          EXPECT_HMICALL("BasicCommunication.SystemRequest",{ requestType = "PROPRIETARY" }, file)
           :Do(function(_, data)
-              test.hmiConnection:SendResponse(data.id, "BasicCommunication.SystemRequest", "SUCCESS", {})
-              test.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
-                {
-                  policyfile = policy_file_path.."/"..policy_file_name
-                }
-              )
+              test.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", {})
+              test.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate", { policyfile = data.params.fileName } )
             end)
-
           EXPECT_RESPONSE(corIdSystemRequest, { success = true, resultCode = "SUCCESS"})
-          :Do(function(_, _)
-              -- requestId = test.hmiConnection:SendRequest("SDL.GetUserFriendlyMessage", {language = "EN-US", messageCodes = {"StatusUpToDate"}})
-              -- EXPECT_HMIRESPONSE(requestId)
-            end)
+          EXPECT_HMICALL("VehicleInfo.GetVehicleData", { odometer = true })
         end)
 
     end)
