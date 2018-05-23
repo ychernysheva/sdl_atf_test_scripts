430bc45c56bd00010bb505e29c2ee091c8fdde0d
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2018-05-23 11:48:27 +0300

    Update script

--- test_scripts/Defects/4_6/842_App_does_not_activate_when_policies_are_disabled.lua
+++ test_scripts/Defects/4_6/842_App_does_not_activate_when_policies_are_disabled.lua
@@ -1,9 +1,9 @@
 --------------------------------------------------------------------------------
--- User story: https://github.com/smartdevicelink/sdl_core/issues/842
+-- Issue: https://github.com/smartdevicelink/sdl_core/issues/842
 
 -- Pre-conditions:
 -- 1. SDL is started (EnablePolicy = false)
--- 2. HMI is started 
+-- 2. HMI is started
 
 -- Steps to reproduce:
 -- 1. Activate App
@@ -14,68 +14,47 @@
 
 --[[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
-local common = require('test_scripts/Defects/commonDefects')
-local mobile_session = require("mobile_session")
-local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
+local common = require("user_modules/sequences/actions")
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
 
 --[[ Local Variables ]]
-local kSuccess = 0
-local hmiAppIds = {}
+local hmiAppId
 
 --[[ Local Functions ]]
-local function rai(self)
-    self, id = common.getSelfAndParams(1, self)
-    if not id then id = 1 end
-    self["mobileSession" .. id] = mobile_session.MobileSession(self, self.mobileConnection)
-    self["mobileSession" .. id]:StartService(7)
-    :Do(function()
-        local corId = self["mobileSession" .. id]:SendRPC
-        ("RegisterAppInterface", config["application" .. id].registerAppInterfaceParams)
-        EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered",
-            { application = { appName = config["application" .. id].registerAppInterfaceParams.appName } })
-        :Do(function(_, d1)
-            hmiAppIds[config["application" .. id].registerAppInterfaceParams.appID] = d1.params.application.appID
-            end)
-        self["mobileSession" .. id]:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
-        :Do(function()
-            self["mobileSession" .. id]:ExpectNotification("OnHMIStatus",
-                { hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN" })
-            :Times(AtLeast(1))
-            end)
-        end)
-end    
-
-local function disablingPolicy()
-    enablePolicyBackup = commonFunctions:read_parameter_from_smart_device_link_ini("EnablePolicy")
-    if not commonFunctions:write_parameter_to_smart_device_link_ini("EnablePolicy", "false") then
-        test:FailTestCase("Value is not set.")
-    end
+local function disablePolicy()
+  common.setSDLIniParameter("EnablePolicy", "false")
 end
 
-local function activateApp(expectedCode, self)
-    local requestId = self.hmiConnection:SendRequest("SDL.ActivateApp", 
-        { appID = hmiAppIds[config["application" .. id].registerAppInterfaceParams.appID] })
-    EXPECT_HMIRESPONSE(requestId)
-    :ValidIf(function(_, data)
-        local actualCode = 
-            (data.error ~= nil and data.error.code or data.result.code)
-        if(expectedCode == actualCode) then
-            return true
-        end
-        return false, "Expected value:" .. tostring(expectedCode) .. 
-            "\nActual value: " .. tostring(actualCode)
+local function registerApp()
+  common.getMobileSession():StartService(7)
+  :Do(function()
+      local corId = common.getMobileSession():SendRPC("RegisterAppInterface", common.getConfigAppParams())
+      common.getHMIConnection():ExpectNotification("BasicCommunication.OnAppRegistered")
+      :Do(function(_, d1)
+          hmiAppId = d1.params.application.appID
+        end)
+      common.getMobileSession():ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
     end)
+end
+
+local function activateApp()
+  local requestId = common.getHMIConnection():SendRequest("SDL.ActivateApp", { appID = hmiAppId })
+  common.getHMIConnection():ExpectResponse(requestId)
+  common.getMobileSession():ExpectNotification("OnHMIStatus", {
+    hmiLevel = "FULL", audioStreamingState = "AUDIBLE", systemContext = "MAIN" })
 end
 
 --[[ Scenario ]]
 runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
-runner.Step("Disabling Policy", disablingPolicy)
+runner.Step("Disabling Policy", disablePolicy)
 runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
-runner.Step("RAI", rai)
+runner.Step("Register App", registerApp)
 
 runner.Title("Test")
-runner.Step("ActivateApp", activateApp, { kSuccess })
+runner.Step("Activate App", activateApp)
 
 runner.Title("Postconditions")
 runner.Step("Stop SDL", common.postconditions)
