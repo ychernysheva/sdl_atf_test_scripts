3f2f8324776b91df4f83803e9056f9ddd8949b15
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2018-05-25 12:54:21 +0300

    Add scripts to check sending of RPC in Protected mode

---
+++ test_scripts/Security/007_Navi_Predefined_cert_missing_PTU_cert_expired_NACK.lua
@@ -0,0 +1,46 @@
+---------------------------------------------------------------------------------------------------
+-- Issues:
+--   https://github.com/smartdevicelink/sdl_core/issues/2190
+--   https://github.com/smartdevicelink/sdl_core/issues/2191
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local common = require("test_scripts/Security/common")
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
+config.application1.registerAppInterfaceParams.appHMIType = { "NAVIGATION" }
+
+--[[ Local Variables ]]
+
+--[[ Local Functions ]]
+local function startServiceSecuredNACK()
+  local serviceId = 7
+  common.getMobileSession():StartSecureService(serviceId)
+  common.getMobileSession():ExpectControlMessage(serviceId, {
+    frameInfo = common.frameInfo.START_SERVICE_NACK,
+    encryption = false
+  })
+  common.getMobileSession():ExpectHandshakeMessage()
+  :Times(0)
+  local function ptUpdate(pTbl)
+    local filePath = "./files/Security/client_credential_expired.pem"
+    local crt = common.readFile(filePath)
+    pTbl.policy_table.module_config.certificate = crt
+  end
+  common.policyTableUpdateSuccess(ptUpdate)
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Init SDL certificates", common.initSDLCertificates, { "./files/Security/client_credential_expired.pem", false })
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+
+runner.Title("Test")
+runner.Step("Register App", common.registerApp)
+runner.Step("PolicyTableUpdate", common.policyTableUpdate)
+runner.Step("Switch RPC Service to Protected mode NACK", startServiceSecuredNACK)
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL, clean-up certificates", common.postconditions)
---
+++ test_scripts/Security/common.lua
@@ -0,0 +1,185 @@
+---------------------------------------------------------------------------------------------------
+-- Common module
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local actions = require("user_modules/sequences/actions")
+local security = require("user_modules/sequences/security")
+local utils = require("user_modules/utils")
+local test = require("user_modules/dummy_connecttest")
+local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
+local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
+
+--[[ General configuration parameters ]]
+config.SecurityProtocol = "DTLS"
+config.application1.registerAppInterfaceParams.appName = "server"
+config.application1.registerAppInterfaceParams.appID = "SPT"
+
+--[[ Module ]]
+local m = actions
+
+m.frameInfo = security.frameInfo
+m.readFile = utils.readFile
+
+--[[ Functions ]]
+local function getSystemTimeValue()
+  local dd = os.date("*t")
+  return {
+    millisecond = 0,
+    second = dd.sec,
+    minute = dd.min,
+    hour = dd.hour,
+    day = dd.day,
+    month = dd.month,
+    year = dd.year,
+    tz_hour = 2,
+    tz_minute = 0
+  }
+end
+
+local function registerGetSystemTimeResponse()
+  actions.getHMIConnection():ExpectRequest("BasicCommunication.GetSystemTime")
+  :Do(function(_, data)
+      actions.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", { systemTime = getSystemTimeValue() })
+    end)
+  :Pin()
+  :Times(AnyNumber())
+end
+
+function m.allowSDL()
+  actions.getHMIConnection():SendNotification("SDL.OnAllowSDLFunctionality", {
+    allowed = true,
+    source = "GUI",
+    device = {
+      id = utils.getDeviceMAC(),
+      name = utils.getDeviceName()
+    }
+  })
+end
+
+function m.start()
+  test:runSDL()
+  commonFunctions:waitForSDLStart(test)
+  :Do(function()
+      test:initHMI()
+      :Do(function()
+          local rid = actions.getHMIConnection():SendRequest("MB.subscribeTo", {
+            propertyName = "BasicCommunication.OnSystemTimeReady" })
+          actions.getHMIConnection():ExpectResponse(rid)
+          :Do(function()
+              utils.cprint(35, "HMI initialized")
+              test:initHMI_onReady()
+              :Do(function()
+                  utils.cprint(35, "HMI is ready")
+                  actions.getHMIConnection():SendNotification("BasicCommunication.OnSystemTimeReady")
+                  registerGetSystemTimeResponse()
+                  test:connectMobile()
+                  :Do(function()
+                      utils.cprint(35, "Mobile connected")
+                      m.allowSDL()
+                    end)
+                end)
+            end)
+        end)
+    end)
+end
+
+function m.sendAddCommandProtected()
+  local params = {
+    cmdID = 1,
+    menuParams = {
+      position = 1,
+      menuName = "Command_1"
+    }
+  }
+  local cid = m.getMobileSession():SendEncryptedRPC("AddCommand", params)
+  m.getHMIConnection():ExpectRequest("UI.AddCommand", params)
+  :Do(function(_, data)
+      m.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", { })
+    end)
+  m.getMobileSession():ExpectEncryptedResponse(cid, { success = true, resultCode = "SUCCESS" })
+  m.getMobileSession():ExpectEncryptedNotification("OnHashChange")
+end
+
+function m.activateAppProtected()
+  local rid = m.getHMIConnection():SendRequest("SDL.ActivateApp", { appID = m.getHMIAppId() })
+  m.getHMIConnection():ExpectResponse(rid)
+  m.getMobileSession():ExpectEncryptedNotification("OnHMIStatus", {
+    hmiLevel = "FULL", audioStreamingState = "AUDIBLE", systemContext = "MAIN" })
+end
+
+local function saveFile(pContent, pFileName)
+  local f = io.open(pFileName, "w")
+  f:write(pContent)
+  f:close()
+end
+
+local function getAllCrtsFromPEM(pCrtsFileName)
+  local crts = utils.readFile(pCrtsFileName)
+  local o = {}
+  local i = 1
+  local s = crts:find("-----BEGIN RSA PRIVATE KEY-----", i, true)
+  local _, e = crts:find("-----END RSA PRIVATE KEY-----", i, true)
+  o.key = crts:sub(s, e) .. "\n"
+  for _, v in pairs({ "crt", "rootCA", "issuingCA" }) do
+    i = e
+    s = crts:find("-----BEGIN CERTIFICATE-----", i, true)
+    _, e = crts:find("-----END CERTIFICATE-----", i, true)
+    o[v] = crts:sub(s, e) .. "\n"
+  end
+  return o
+end
+
+local function createCrtHashes()
+  local sdlBin = commonPreconditions:GetPathToSDL()
+  os.execute("cd " .. sdlBin .. " && c_rehash .")
+end
+
+local function updateSDLIniFile()
+  m.setSDLIniParameter("KeyPath", "module_key.pem")
+  m.setSDLIniParameter("CertificatePath", "module_crt.pem")
+end
+
+function m.initSDLCertificates(pCrtsFileName, pIsModuleCrtDefined)
+  if pIsModuleCrtDefined == nil then pIsModuleCrtDefined = true end
+  local allCrts = getAllCrtsFromPEM(pCrtsFileName)
+  local sdlBin = commonPreconditions:GetPathToSDL()
+  saveFile(allCrts.rootCA, sdlBin .. "rootCA.pem")
+  saveFile(allCrts.issuingCA, sdlBin .. "issuingCA.pem")
+  createCrtHashes()
+  if pIsModuleCrtDefined then
+    saveFile(allCrts.key, sdlBin .. "module_key.pem")
+    saveFile(allCrts.crt, sdlBin .. "module_crt.pem")
+  end
+  updateSDLIniFile()
+end
+
+function m.cleanUpCertificates()
+  local sdlBin = commonPreconditions:GetPathToSDL()
+  os.execute("cd " .. sdlBin .. " && find . -type l -exec rm -f {} \\;")
+  os.execute("cd " .. sdlBin .. " && rm -rf *.pem")
+end
+
+local preconditionsOrig = m.preconditions
+local postconditionsOrig = m.postconditions
+
+function m.preconditions()
+  preconditionsOrig()
+  m.cleanUpCertificates()
+end
+
+function m.postconditions()
+  postconditionsOrig()
+  m.cleanUpCertificates()
+end
+
+function m.policyTableUpdateSuccess(pPTUpdateFunc)
+  local function expNotificationFunc()
+    m.getHMIConnection():ExpectNotification("SDL.OnStatusUpdate",
+      { status = "UPDATE_NEEDED" }, { status = "UPDATING" }, { status = "UP_TO_DATE" })
+    :Times(3)
+    m.getHMIConnection():ExpectRequest("VehicleInfo.GetVehicleData", { odometer = true })
+  end
+  m.policyTableUpdate(pPTUpdateFunc, expNotificationFunc)
+end
+
+return m
