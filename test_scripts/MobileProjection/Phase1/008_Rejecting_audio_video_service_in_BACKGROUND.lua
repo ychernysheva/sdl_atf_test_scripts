c3162b3a534de6c6fff8fe38bd6d4e218352bcee
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2018-06-21 14:38:07 +0300

    Initial scripts for checking happy path

---
+++ test_scripts/MobileProjection/Phase1/008_Rejecting_audio_video_service_in_BACKGROUND.lua
@@ -0,0 +1,56 @@
+---------------------------------------------------------------------------------------------------
+-- User story: TBD
+-- Use case: TBD
+--
+-- Requirement summary:
+-- TBD
+--
+-- Description:
+-- In case:
+-- 1) Application is registered with PROJECTION appHMIType
+-- 2) set in BACKGROUND HMI level
+-- 3) and starts audio/video service
+-- SDL must:
+-- 1) reject audio/video service in BACKGROUND HMI level
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require('test_scripts/MobileProjection/Phase1/common')
+local runner = require('user_modules/script_runner')
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
+
+--[[ Local Variables ]]
+local appHMIType = "PROJECTION"
+
+--[[ General configuration parameters ]]
+config.application1.registerAppInterfaceParams.appHMIType = { appHMIType }
+config.application2.registerAppInterfaceParams.appHMIType = { appHMIType }
+
+--[[ Local Functions ]]
+local function ptUpdate(pTbl)
+  pTbl.policy_table.app_policies[common.getConfigAppParams().appID].AppHMIType = { appHMIType }
+end
+
+local function BringAppToBackground()
+  common.activateApp(2)
+  common.getMobileSession():ExpectNotification("OnHMIStatus",
+	{ hmiLevel = "BACKGROUND", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN" })
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+runner.Step("Register App", common.registerApp)
+runner.Step("PolicyTableUpdate with HMI types", common.policyTableUpdate, { ptUpdate })
+runner.Step("Activate App", common.activateApp)
+runner.Step("Register second projection application", common.registerApp, { 2 })
+runner.Step("Bring first app to BACKGROUND", BringAppToBackground)
+
+runner.Title("Test")
+runner.Step("Reject video service in BACKGROUND", common.RejectingServiceStart, { 11 })
+runner.Step("Reject audio service in BACKGROUND", common.RejectingServiceStart, { 10 })
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", common.postconditions)
---
+++ test_scripts/MobileProjection/Phase1/013_Rejecting_StartAudioStream_during_audio_service_start.lua
@@ -0,0 +1,80 @@
+---------------------------------------------------------------------------------------------------
+-- User story: TBD
+-- Use case: TBD
+--
+-- Requirement summary:
+-- TBD
+--
+-- Description:
+-- In case:
+-- 1) Application is registered with PROJECTION appHMIType
+-- 2) and starts audio services
+-- 3) HMI rejects StartAudioStream
+-- SDL must:
+-- 1) end service
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require('test_scripts/MobileProjection/Phase1/common')
+local runner = require('user_modules/script_runner')
+local events = require('events')
+local constants = require('protocol_handler/ford_protocol_constants')
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
+
+--[[ Local Variables ]]
+local appHMIType = "PROJECTION"
+
+--[[ General configuration parameters ]]
+config.application1.registerAppInterfaceParams.appHMIType = { appHMIType }
+
+--[[ Local Functions ]]
+local function ptUpdate(pTbl)
+  pTbl.policy_table.app_policies[common.getConfigAppParams().appID].AppHMIType = { appHMIType }
+end
+
+local function startService()
+  common.getMobileSession():StartService(10)
+  local EndServiceEvent = events.Event()
+  EndServiceEvent.matches =
+  function(_, data)
+    return data.frameType == constants.FRAME_TYPE.CONTROL_FRAME and
+    data.serviceType == constants.SERVICE_TYPE.PCM and
+    data.sessionId == common.getMobileSession().sessionId and
+    data.frameInfo == constants.FRAME_INFO.END_SERVICE
+  end
+  common.getMobileSession():ExpectEvent(EndServiceEvent, "Expect EndServiceEvent")
+  :Do(function()
+    common.getMobileSession():Send({
+      frameType = constants.FRAME_TYPE.CONTROL_FRAME,
+      serviceType = constants.SERVICE_TYPE.PCM,
+      frameInfo = constants.FRAME_INFO.END_SERVICE_ACK
+    })
+  end)
+  common.getHMIConnection():ExpectRequest("Navigation.StartAudioStream")
+  :Do(function(_, data)
+    local function response()
+      common.getHMIConnection():SendError(data.id, data.method, "REJECTED", "Request is rejected")
+    end
+    RUN_AFTER(response, 100)
+  end)
+  :Times(4)
+  common.getHMIConnection():ExpectRequest("Navigation.StopAudioStream")
+  :Do(function(_,data)
+    common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", { })
+  end)
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+runner.Step("Register App", common.registerApp)
+runner.Step("PolicyTableUpdate with HMI types", common.policyTableUpdate, { ptUpdate })
+runner.Step("Activate App", common.activateApp)
+
+runner.Title("Test")
+runner.Step("Stop audio service by rejecting StartAudioStream", startService)
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", common.postconditions)
