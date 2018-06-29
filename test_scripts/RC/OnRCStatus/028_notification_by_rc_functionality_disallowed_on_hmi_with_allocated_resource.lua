d35886504b6f4823cecc4056ed1af6666482b627
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2018-06-29 10:40:19 +0300

    Add additional script

---
+++ test_scripts/RC/OnRCStatus/028_notification_by_rc_functionality_disallowed_on_hmi_with_allocated_resource.lua
@@ -0,0 +1,63 @@
+---------------------------------------------------------------------------------------------------
+-- User story: TBD
+-- Use case: TBD
+--
+-- Requirement summary:
+-- [SDL_RC] TBD
+--
+-- Description:
+-- In case:
+-- 1) RC functionality is allowed on HMI
+-- 2) RC app is registered
+-- 3) RC app allocates module via SetInteriorVehicleData
+-- 4) RC functionality is disallowed on HMI
+-- SDL must:
+-- 1) SDL sends an OnRCStatus notification to the HMI (allocatedModules=[], freeModules=[x,y,z], due to resource freed)
+-- 2) SDL sends OnRCStatus notifications to the already registered RC apps (allowed=false, allocatedModules=[], freeModules=[])
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local common = require('test_scripts/RC/OnRCStatus/commonOnRCStatus')
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
+config.application1.registerAppInterfaceParams.appHMIType = { "REMOTE_CONTROL" }
+
+--[[ Local Variables ]]
+local freeModules = common.getAllModules()
+local allocatedModules = {
+	[1] = {}
+}
+
+--[[ Local Functions ]]
+local function disableRCFromHMI()
+  common.getHMIconnection():SendNotification("RC.OnRemoteControlSettings", { allowed = false })
+  common.getMobileSession():ExpectNotification("OnRCStatus",
+	{ allowed = false, freeModules = {}, allocatedModules = {} })
+  local pModuleStatusHMI = {
+    freeModules = common.getModulesArray(common.getAllModules()),
+    allocatedModules = { }
+  }
+  common.validateOnRCStatusForHMI(1, { pModuleStatusHMI })
+end
+
+local function setVehicleData(pModuleType)
+	local pModuleStatus = common.setModuleStatus(freeModules, allocatedModules, pModuleType)
+  common.rpcAllowed(pModuleType, 1, "SetInteriorVehicleData")
+	common.validateOnRCStatusForApp(1, pModuleStatus)
+	common.validateOnRCStatusForHMI(1, { pModuleStatus })
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+runner.Step("RC app registration", common.registerRCApplication)
+runner.Step("Activate App", common.activateApp)
+runner.Step("SetInteriorVehicleData RADIO", setVehicleData, { "RADIO" })
+
+runner.Title("Test")
+runner.Step("RC functionality is disallowed from HMI", disableRCFromHMI)
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", common.postconditions)
