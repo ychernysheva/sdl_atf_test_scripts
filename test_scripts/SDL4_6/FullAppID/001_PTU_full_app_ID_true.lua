5f784bd4cc9514724a29d8e50417e4d2ad735169
Conlain Kelly <conlain.k@gmail.com>
2018-08-09 13:28:44 -0400

    Add new tests for full app id functionality

---
+++ test_scripts/SDL4_6/FullAppID/001_PTU_full_app_ID_true.lua
@@ -0,0 +1,40 @@
+---------------------------------------------------------------------------------------------
+-- Script verifies that a PT snapshot contains the correct full_app_id_supported flag
+-- Supports PROPRIETARY 
+---------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
+local commonPreconditions = require("user_modules/shared_testcases/commonPreconditions")
+local mobile_session = require("mobile_session")
+local actions = require("user_modules/sequences/actions")
+local json = require("modules/json")
+local atf_logger = require("atf_logger")
+local sdl = require("SDL")
+local commonSteps = require("user_modules/shared_testcases/commonSteps")
+local commonSmoke = require('test_scripts/Smoke/commonSmoke')
+
+local common = require("test_scripts/SDL4_6/FullAppID/common")
+
+local full_app_id_supported = "true"
+
+-- This is the id in the policy table
+common.policy_app_id = config.application1.registerAppInterfaceParams.fullAppID
+
+runner.Title("Preconditions " .. full_app_id_supported)
+-- Stop SDL if process is still running, delete local policy table and log files
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Set UseFullAppID to true", actions.setSDLIniParameter, {"UseFullAppID", full_app_id_supported})
+-- Start SDL and HMI, establish connection between SDL and HMI, open mobile connection via TCP
+runner.Step("Start SDL, HMI, connect Mobile", common.start)
+-- Pring in terminal build options(RC status and policy slow)
+runner.Step("SDL Configuration", common.printSDLConfig)
+
+runner.Title("Test " .. full_app_id_supported)
+-- create mobile session, register application, perform PTU wit PT
+runner.Step("RAI, PTU", common.raiPTU)
+-- Check that PTU is performed successful
+runner.Step("Check Status", common.checkPTUStatus)
+
+runner.Title("Postconditions " .. full_app_id_supported)
+runner.Step("Restore ini and stop SDL", common.postconditions)
