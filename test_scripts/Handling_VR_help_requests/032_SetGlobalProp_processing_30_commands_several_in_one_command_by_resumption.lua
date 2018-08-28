34deedd5b08b580308782c379f0c52f40a9c0721
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2018-08-28 16:22:14 +0300

    Updating scripts according to changes in proposal

--- test_scripts/Handling_VR_help_requests/032_SetGlobalProp_processing_30_commands_several_in_one_command_by_resumption.lua
+++ test_scripts/Handling_VR_help_requests/032_SetGlobalProp_processing_30_commands_several_in_one_command_by_resumption.lua
@@ -7,14 +7,17 @@
 --
 -- Description:
 -- In case:
--- 1. Mobile application sets 29 command one by one
+-- 1. Mobile application sets 28 command one by one
 -- 2. Perform reconnect
 -- 3. SDL resumes commands
--- 4. Mobile application sets 2 commands in one request
+-- 4. Mobile application sets 1 command in one request (29) with 2 synonyms
+-- 5. Mobile application sets 1 command in one request (30)
+-- 6. Mobile application sets 1 command in one request (31)
 -- SDL does:
--- 1. send SetGlobalProperties with full list of command values for vrHelp and helpPrompt parameters for 30 VR commands
--- and exclude 31th VR command from list
--- 2. not send SetGlobalProperties after added 31 command
+-- 1. send SetGlobalProperties with full list of command values for vrHelp and helpPrompt parameters for 28 VR commands
+-- 4. send SetGlobalProperties with constructed the vrHelp and helpPrompt parameters using 1st synonym from list
+-- 5. send SetGlobalProperties with constructed the vrHelp and helpPrompt parameters
+-- 6. not send SetGlobalProperties after added 31 command
 ---------------------------------------------------------------------------------------------------
 --[[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
@@ -24,7 +27,7 @@ local common = require('test_scripts/Handling_VR_help_requests/commonVRhelp')
 runner.testSettings.isSelfIncluded = false
 
 --[[ Local Variables ]]
-local AddCommandParams = common.getAddCommandParams(30)
+local AddCommandParams = common.getAddCommandParams(29)
 AddCommandParams.vrCommands = { "Command_30_1", "Command_30_2" }
 
 --[[ Scenario ]]
@@ -36,7 +39,7 @@ runner.Step("Pin OnHashChange", common.pinOnHashChange)
 runner.Step("App activation", common.activateApp)
 
 runner.Title("Test")
-for i = 1, 29 do
+for i = 1, 28 do
   runner.Step("SetGlobalProperties from SDL after added command " .. i, common.addCommandWithSetGP, { i })
 end
 runner.Step("App reconnect", common.reconnect)
@@ -44,7 +47,8 @@ runner.Step("App resumption", common.registrationWithResumption,
   { 1, common.resumptionLevelFull, common.resumptionDataAddCommands })
 runner.Step("SetGlobalProperties after AddCommand with several vrCommand", common.addCommandWithSetGP,
   { nil, AddCommandParams })
-runner.Step("Absence SetGlobalProperties from SDL after adding 32 VR command", common.addCommandWithoutSetGP, { 31 })
+runner.Step("SetGlobalProperties from SDL after added command 30", common.addCommandWithSetGP, { 30 })
+runner.Step("Absence SetGlobalProperties from SDL after adding 31 VR command", common.addCommandWithoutSetGP, { 31 })
 
 runner.Title("Postconditions")
 runner.Step("Stop SDL", common.postconditions)
