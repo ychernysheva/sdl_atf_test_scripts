20db55e658cf8ba6863901b2f31f3e9d901f7ae0
Oleg Krupenich <okrupenich@luxoft.com>
2018-10-26 17:15:47 +0300

    Update: description fix and PTU added

--- test_scripts/Defects/5_0/1846_Mismatch_for_mandatory_parameter_between_MOBILE_API_and_HMI_API.lua
+++ test_scripts/Defects/5_0/1846_Mismatch_for_mandatory_parameter_between_MOBILE_API_and_HMI_API.lua
@@ -10,9 +10,24 @@
 --     distanceToManeuverScale
 -- The MOBILE_API is reflecting that these parameters are not mandatory, whereas the HMI_API 
 -- reflects that they are mandatory.
--- 
--- WARNING: you should include "Navigation-1" group in "sdl_preloaded_pt.json" file before run 
--- this test!
+--
+-- Preconditions:
+-- 1) Clear environment
+-- 2) SDL started, HMI and mobile session connected
+-- 3) Registered and activated app
+-- 5) PTU
+--
+-- Steps:
+-- 1) send mobile RPC "ShowConstantTBT" without "distanceToManeuver" param
+--    and recieve resultCode = "INVALID_DATA"
+-- 2) send mobile RPC "ShowConstantTBT" without "distanceToManeuverScale" param
+--    and recieve resultCode = "INVALID_DATA"
+-- 3) send mobile RPC "ShowConstantTBT" with "distanceToManeuver" and 
+--    "distanceToManeuverScale" params and recieve resultCode = "SUCCESS"
+--
+-- Expected:
+-- Params "distanceToManeuver" and "distanceToManeuverScale" of the mobile RPC "ShowConstantTBT" 
+-- should be mandatory 
 ---------------------------------------------------------------------------------------------------
 
 --[[ Required Shared libraries ]]
@@ -23,6 +38,10 @@ local common = require('user_modules/sequences/actions')
 runner.testSettings.isSelfIncluded = false
 
 --[[ Local Functions ]]
+local function ptuFunc(tbl)
+	tbl.policy_table.app_policies["0000001"].groups = {"Base-4", "Navigation-1"}
+end
+
 local function checkShowConstantTBT(pParams, pResultTable)
 	local cid = common.getMobileSession():SendRPC("ShowConstantTBT", pParams)
 	common.getMobileSession():ExpectResponse(cid, pResultTable)
@@ -34,20 +53,16 @@ runner.Step("Clean environment", common.preconditions)
 runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
 runner.Step("RAI", common.registerApp)
 runner.Step("Activate App", common.activateApp)
+runner.Step("PTU", common.policyTableUpdate, { ptuFunc })
 
 runner.Title("Test")
 runner.Step("Negative check mandatory parameter distanceToManeuver", checkShowConstantTBT, {{
-		appID = common.getHMIAppId(1),
-		--distanceToManeuver = 50.1,
 	 	distanceToManeuverScale = 100.2
 	}, {success = false, resultCode = "INVALID_DATA"}})
 runner.Step("Negative check mandatory parameter distanceToManeuverScale", checkShowConstantTBT, {{
-		appID = common.getHMIAppId(1),
 		distanceToManeuver = 50.1,
-	 	--distanceToManeuverScale = 100.2
 	}, {success = false, resultCode = "INVALID_DATA"}})
 runner.Step("Positive case for ShowConstantTBT", checkShowConstantTBT, {{
-		appID = common.getHMIAppId(1),
 		distanceToManeuver = 50.1,
 	 	distanceToManeuverScale = 100.2
 	}, {success = true, resultCode = "SUCCESS"}})
