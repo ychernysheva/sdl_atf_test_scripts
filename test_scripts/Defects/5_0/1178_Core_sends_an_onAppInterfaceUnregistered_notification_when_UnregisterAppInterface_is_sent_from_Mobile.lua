8c1099fdaf4be8b0cd0be4443a4dac9bab5b7a21
Oleg Krupenich <okrupenich@luxoft.com>
2018-10-29 11:16:17 +0200

    Script description and onAppInterfaceUnregistered mobile notification check

--- test_scripts/Defects/5_0/1178_Core_sends_an_onAppInterfaceUnregistered_notification_when_UnregisterAppInterface_is_sent_from_Mobile.lua
+++ test_scripts/Defects/5_0/1178_Core_sends_an_onAppInterfaceUnregistered_notification_when_UnregisterAppInterface_is_sent_from_Mobile.lua
@@ -2,6 +2,8 @@
 -- User story: https://github.com/SmartDeviceLink/sdl_core/issues/1178
 --
 -- Description:
+-- Core sends an onAppInterfaceUnregistered notification when UnregisterAppInterface is sent from Mobile
+--
 -- It seems as though we have an issue with core sending both an onAppInterfaceUnregistered 
 -- notification and an UnregisterAppInterface response when the mobile proxy sends over 
 -- UnregisterAppInterface. This should not be the case, as the notification should only be sent 
@@ -9,13 +11,23 @@
 -- The notification also has a reason of -1, which is not a valid value.
 -- We should remove the notification from being sent when the UnregisterAppInterface RPC is sent.
 --
--- Preconditions: N/A
+-- Preconditions:
+-- 1) Clear environment
+-- 2) SDL, HMI, Mobile session started
+-- 3) Registered app
+-- 4) Activated app
 --
--- Steps: N/A
+-- Steps: 
+-- 1) Send "UnregisterAppInterface" mobile RPC
+--   
+-- Postconditions:
+-- Stop SDL
 --
--- Expected result: N/A
+-- Expected:
+-- 1) "onAppInterfaceUnregistered" notification was NOT recieved by mobile
+-- 2) "UnregisterAppInterface" response was recieved by mobile with resultCode = "SUCCESS"
+-- 3) "OnAppUnregistered" was recieved by HMI with unexpectedDisconnect = false 
 ---------------------------------------------------------------------------------------------------
-
 --[[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
 local common = require('user_modules/sequences/actions')
@@ -26,9 +38,14 @@ runner.testSettings.isSelfIncluded = false
 --[[ Local Functions ]]
 local function unregisterAppInterface()
 	local cid = common.getMobileSession():SendRPC("UnregisterAppInterface", { })
+	common.getMobileSession():ExpectNotification("onAppInterfaceUnregistered", { })
+	:Times(0)
+	common.getMobileSession():ExpectResponse(cid, 
+		{ success = true, resultCode = "SUCCESS" }
+	)
 	common.getHMIConnection():ExpectNotification("BasicCommunication.OnAppUnregistered", 
-		{ appID = common.getHMIAppId(), unexpectedDisconnect = false})
-	common.getMobileSession():ExpectResponse(cid, { success = true, resultCode = "SUCCESS" })
+		{ appID = common.getHMIAppId(), unexpectedDisconnect = false}
+	)
 end
 
 --[[ Scenario ]]
