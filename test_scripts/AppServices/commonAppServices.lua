f588fea6e059631368d55b65b47d34028e6adb4f
Shobhit Adlakha <ShobhitAd@users.noreply.github.com>
2019-03-26 16:11:09 -0400

    GetFile RPC ATF tests (#2166)
    * commonAppServices class and PublishAppService for Mobile ASPs
    
    * Implemented GetFile RPC tests
    
    * Implemented GetFile RPC tests
    
    * Added GetFile functions to CommonAppServices and fixed HMI_ASP test case
    
    * Modified getFilCRC32 function and updated test descriptions
    
    * Addressed review comments
    
    * Indentation fix

--- test_scripts/AppServices/commonAppServices.lua
+++ test_scripts/AppServices/commonAppServices.lua
@@ -1,4 +1,3 @@
-
 local actions = require("user_modules/sequences/actions")
 
 local commonAppServices = actions
@@ -190,5 +189,87 @@ function commonAppServices.getAppServiceID(app_id)
   if not app_id then app_id = 1 end
   return serviceIDs[app_id]
 end
+
+--[[ GetFile ]]
+
+local function file_check(file_name)
+  local file_found=io.open(file_name, "r")
+  return file_found~=nil
+end
+
+local function getFileCRC32(fileName)
+  pFile = "files/"..fileName
+  local cmd = "cat " .. pFile .. " | gzip -1 | tail -c 8 | head -c 4"
+  local handle = io.popen(cmd)
+  local crc = handle:read("*a")
+  handle:close()
+  local function bytesToInt(pStr)
+    local t = { string.byte(pStr, 1, -1) }
+    local n = 0
+    for k = 1, #t do
+      n = n + t[k] * 2 ^ ((k - 1) * 8)
+    end
+    return n
+  end
+  return bytesToInt(crc)
+end
+
+local function getATFPath()
+  local handle = io.popen("echo $(pwd)")
+  local result = handle:read("*a")
+  handle:close()
+  return result:sub(0, -2)
+end
+
+function commonAppServices.getFileFromStorage(app_id, request_params, response_params)
+  local mobileSession = commonAppServices.getMobileSession(app_id)
+  if file_check("files/"..request_params.fileName) and response_params.crc == nil then
+    local file_crc = getFileCRC32(request_params.fileName)   
+    if response_params.success then
+      response_params.crc = file_crc
+    end
+  end
+  --mobile side: sending GetFile request
+  local cid = mobileSession:SendRPC("GetFile", request_params)
+  --mobile side: expected GetFile response   
+  mobileSession:ExpectResponse(cid, response_params)
+end
+
+function commonAppServices.getFileFromService(app_id, asp_app_id, request_params, response_params)
+  local mobileSession = commonAppServices.getMobileSession(app_id)
+  if file_check("files/"..request_params.fileName) and response_params.crc == nil then
+    local file_crc = getFileCRC32(request_params.fileName)   
+    if response_params.success then
+      response_params.crc = file_crc
+    end
+  end
+
+  request_params.appServiceId = commonAppServices.getAppServiceID(asp_app_id)
+
+  --mobile side: sending GetFile request
+  local cid = mobileSession:SendRPC("GetFile", request_params)
+  if asp_app_id == 0 then 
+    --EXPECT_HMICALL
+    commonAppServices.getHMIConnection():ExpectRequest("BasicCommunication.GetFilePath")
+    :Do(function(_, d2)
+      local cwd = getATFPath()
+      file_path = cwd.."/files/"..request_params.fileName
+      commonAppServices.getHMIConnection():SendResponse(d2.id, d2.method, "SUCCESS", {filePath = file_path})
+    end) 
+  end
+
+  --mobile side: expected GetFile response   
+  mobileSession:ExpectResponse(cid, response_params)
+end
+
+function commonAppServices.putFileInStorage(app_id, request_params, response_params)
+  local mobileSession = commonAppServices.getMobileSession(app_id)      
+  --mobile side: sending PutFile request
+  local cid = mobileSession:SendRPC("PutFile", request_params, "files/"..request_params.syncFileName)
+  --mobile side: expected PutFile response
+  mobileSession:ExpectResponse(cid, response_params)
+end
+
+
 
 return commonAppServices
