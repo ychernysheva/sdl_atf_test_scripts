4d90a3cb76ad7aac5d2e0ffe1ff29270c575c407
Shobhit Adlakha <ShobhitAd@users.noreply.github.com>
2019-03-29 12:53:31 -0400

    Update test_scripts/AppServices/AppServiceActivation/003_two_apps_activate_success.lua
    Co-Authored-By: JackLivio <jack@livio.io>

--- test_scripts/AppServices/AppServiceActivation/003_two_apps_activate_success.lua
+++ test_scripts/AppServices/AppServiceActivation/003_two_apps_activate_success.lua
@@ -2,7 +2,7 @@
 --  Precondition: 
 --  1) Application 1 with <appID> is registered on SDL.
 --  2) Application 2 with <appID> is registered on SDL.
---  3) Specific permissions for App 1 are assigned for <appID> with PublishAppService
+--  3) AppServiceProvider permissions for App 1 are assigned for <appID> with PublishAppService
 --  4) Specific permissions for App 2 are assigned for <appID> with PublishAppService
 --
 --  Steps:
