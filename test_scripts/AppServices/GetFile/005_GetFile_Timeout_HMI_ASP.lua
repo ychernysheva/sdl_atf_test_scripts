9634e4399c66ffbe45192773662fb37a18948567
Shobhit Adlakha <ShobhitAd@users.noreply.github.com>
2019-03-29 09:17:09 -0400

    Forwarded AppService requests timeout ATF tests (#2169)
    * commonAppServices class and PublishAppService for Mobile ASPs
    
    * Implemented GetFile RPC tests
    
    * Implemented GetFile RPC tests
    
    * Added GetFile functions to CommonAppServices and fixed HMI_ASP test case
    
    * Modified getFilCRC32 function and updated test descriptions
    
    * Forwarded request timeout check for GetFile
    
    * Added timeout tests for GetAppServiceData
    
    * Getting default timeout from runner testSettings
    
    * Added timeout tests for PerformAppServiceInteraction
    
    * Updated timeout tests
    
    * Moved timeout tests to appropriate folders
    
    * Increased response delay buffer

--- test_scripts/AppServices/GetFile/005_GetFile_Timeout_HMI_ASP.lua
+++ test_scripts/AppServices/GetFile/005_GetFile_Timeout_HMI_ASP.lua
@@ -18,11 +18,6 @@ local common = require('test_scripts/AppServices/commonAppServices')
 --[[ Test Configuration ]]
 runner.testSettings.isSelfIncluded = false
 
---[[ Local functions ]]
-local function PTUfunc(tbl)
-    tbl.policy_table.app_policies[common.getConfigAppParams(1).fullAppID] = common.getAppServiceConsumerConfig(1);
-end
-
 --[[ Local variables ]]
 local manifest = {
   serviceName = config.application1.registerAppInterfaceParams.appName,
@@ -44,6 +39,39 @@ local getFileParams = {
 
 local result = { success = true, resultCode = "SUCCESS"}
 
+
+--[[ Local functions ]]
+local function PTUfunc(tbl)
+    tbl.policy_table.app_policies[common.getConfigAppParams(1).fullAppID] = common.getAppServiceConsumerConfig(1);
+end
+
+local function getATFPath()
+  local handle = io.popen("echo $(pwd)")
+  local result = handle:read("*a")
+  handle:close()
+  return result:sub(0, -2)
+end
+
+local function getFileFromService(app_id, asp_app_id, request_params, response_params)
+  local mobileSession = common.getMobileSession(app_id)
+  request_params.appServiceId = common.getAppServiceID(asp_app_id)
+  --mobile side: sending GetFile request
+  local cid = mobileSession:SendRPC("GetFile", request_params)
+  if asp_app_id == 0 then 
+    --EXPECT_HMICALL
+    common.getHMIConnection():ExpectRequest("BasicCommunication.GetFilePath")
+    :Do(function(_, d2)
+      local cwd = getATFPath()
+      file_path = cwd.."/files/"..request_params.fileName
+      RUN_AFTER((function() 
+        common.getHMIConnection():SendResponse(d2.id, d2.method, "SUCCESS", {filePath = file_path})      
+      end), runner.testSettings.defaultTimeout + 2000)
+    end) 
+  end
+  --mobile side: expected GetFile response   
+  mobileSession:ExpectResponse(cid, response_params):Timeout(runner.testSettings.defaultTimeout + common.getRpcPassThroughTimeoutFromINI())
+end
+
 --[[ Scenario ]]
 runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
@@ -53,9 +81,8 @@ runner.Step("RAI App 1", common.registerApp)
 runner.Step("PTU", common.policyTableUpdate, { PTUfunc })
 runner.Step("Activate App", common.activateApp)
 
-runner.Title("Test")
-runner.Step("Getfile", common.getFileFromService, {1, 0, getFileParams, result})
+runner.Title("Test")    
+runner.Step("Getfile", getFileFromService, {1, 0, getFileParams, result})
 
 runner.Title("Postconditions")
 runner.Step("Stop SDL", common.postconditions)
-
