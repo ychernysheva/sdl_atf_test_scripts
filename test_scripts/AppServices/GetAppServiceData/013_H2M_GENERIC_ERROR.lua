0412e9deb1787fac29a0742919856427f1eabf19
jacobkeeler <jacob.keeler@livioradio.com>
2019-04-01 17:32:31 -0400

    Add GENERIC_ERROR timeout tests for app service RPCs

---
+++ test_scripts/AppServices/GetAppServiceData/013_H2M_GENERIC_ERROR.lua
@@ -0,0 +1,95 @@
+---------------------------------------------------------------------------------------------------
+--  Precondition: 
+--  1) Application with <appID> is registered on SDL.
+--  2) Specific permissions are assigned for <appID> with GetAppServiceData
+--  3) Application has published a MEDIA service
+--
+--  Steps:
+--  1) HMI sends a AppService.GetAppServiceData RPC request with serviceType MEDIA
+--
+--  Expected:
+--  1) SDL forwards the GetAppServiceData request to Application as GetAppServiceData
+--  2) Application sends a GetAppServiceData response (SUCCESS) to Core with its own serviceData
+--  3) SDL forwards the response to HMI as AppService.GetAppServiceData
+---------------------------------------------------------------------------------------------------
+
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local common = require('test_scripts/AppServices/commonAppServices')
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
+
+--[[ Local Variables ]]
+local manifest = {
+  serviceName = config.application1.registerAppInterfaceParams.appName,
+  serviceType = "MEDIA",
+  allowAppConsumers = true,
+  rpcSpecVersion = config.application1.registerAppInterfaceParams.syncMsgVersion,
+  mediaServiceManifest = {}
+}
+
+local appServiceData = {
+  serviceType = manifest.serviceType,
+  mediaServiceData = {
+    mediaType = "MUSIC",
+    mediaTitle = "Song name",
+    mediaArtist = "Band name",
+    mediaAlbum = "Album name",
+    playlistName = "Good music",
+    isExplicit = false,
+    trackPlaybackProgress = 200,
+    trackPlaybackDuration = 300,
+    queuePlaybackProgress = 2200,
+    queuePlaybackDuration = 4000,
+    queueCurrentTrackNumber = 12,
+    queueTotalTrackCount = 20
+  }
+}
+
+local rpc = {
+  name = "GetAppServiceData",
+  hmiName = "AppService.GetAppServiceData",
+  params = {
+    serviceType = manifest.serviceType
+  }
+}
+
+local expectedResponse = {
+  code = 22,
+  data = {
+    method = rpc.hmiName
+  }
+}
+
+local function PTUfunc(tbl)
+  tbl.policy_table.app_policies[common.getConfigAppParams(1).fullAppID] = common.getAppServiceProducerConfig(1);
+end
+
+--[[ Local Functions ]]
+local function processRPCSuccess(self)
+  local mobileSession = common.getMobileSession()
+  local cid = common.getHMIConnection():SendRequest(rpc.hmiName, rpc.params)
+  local service_id = common.getAppServiceID()
+  -- Do not respond to request
+  mobileSession:ExpectRequest(rpc.name, rpc.params)
+
+  EXPECT_HMIRESPONSE(cid, {
+    error = expectedResponse
+  }):Timeout(runner.testSettings.defaultTimeout + common.getRpcPassThroughTimeoutFromINI() + 1000)
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+runner.Step("RAI", common.registerApp)
+runner.Step("PTU", common.policyTableUpdate, { PTUfunc })
+runner.Step("Activate App", common.activateApp)
+runner.Step("Publish App Service", common.publishMobileAppService, { manifest })
+
+runner.Title("Test")
+runner.Step("RPC " .. rpc.name .. "_resultCode_GENERIC_ERROR", processRPCSuccess)
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", common.postconditions)
