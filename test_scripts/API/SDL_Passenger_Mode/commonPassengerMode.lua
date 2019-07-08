a2052be3d2402480bfbe476214330ed4a3974efb
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2019-07-08 11:10:34 -0400

    Update common module

--- test_scripts/API/SDL_Passenger_Mode/commonPassengerMode.lua
+++ test_scripts/API/SDL_Passenger_Mode/commonPassengerMode.lua
@@ -11,12 +11,14 @@ local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
 local commonPreconditions = require('user_modules/shared_testcases/commonPreconditions')
 local utils = require("user_modules/utils")
 local json = require("modules/json")
+local test = require("user_modules/dummy_connecttest")
 
 --[[ Module ]]
 local c = actions
 
 --[[ Variables ]]
 local preloadedPT = commonFunctions:read_parameter_from_smart_device_link_ini("PreloadedPT")
+local wrnMsg
 
 --[[ Common Functions ]]
 function c.registerAppWithOnDD(pAppId)
@@ -24,28 +26,43 @@ function c.registerAppWithOnDD(pAppId)
   c.getMobileSession(pAppId):ExpectNotification("OnDriverDistraction", { state = "DD_OFF" })
 end
 
-function c.onDriverDistraction(pLockScreenDismissalEnabled)
-  local function msg(pValue)
-    return "Parameter `lockScreenDismissalEnabled` is transfered to Mobile with `" .. tostring(pValue) .. "` value"
-  end
-  c.getHMIConnection():SendNotification("UI.OnDriverDistraction", { state = "DD_OFF" })
-  c.getHMIConnection():SendNotification("UI.OnDriverDistraction", { state = "DD_ON" })
-  c.getMobileSession():ExpectNotification("OnDriverDistraction",
-    { state = "DD_OFF" },
-    { state = "DD_ON", lockScreenDismissalEnabled = pLockScreenDismissalEnabled })
-  :ValidIf(function(e, d)
-      if e.occurences == 1 and d.payload.lockScreenDismissalEnabled ~= nil then
-        return false, d.payload.state .. ": " .. msg(d.payload.lockScreenDismissalEnabled)
+function c.expOnDriverDistraction(pState, pLockScreenDismissalEnabled, pAppId)
+  c.getMobileSession(pAppId):ExpectNotification("OnDriverDistraction", { state = pState })
+  :ValidIf(function(_, d)
+      if d.payload.state == "DD_OFF" and d.payload.lockScreenDismissalEnabled ~= nil then
+        return false, d.payload.state .. ": Parameter `lockScreenDismissalEnabled` is not expected"
+      end
+      if d.payload.state == "DD_ON" then
+        if pLockScreenDismissalEnabled == nil and d.payload.lockScreenDismissalEnabled ~= nil then
+          return false, d.payload.state .. ": Parameter `lockScreenDismissalEnabled` is not expected"
+        end
+        if pLockScreenDismissalEnabled ~= d.payload.lockScreenDismissalEnabled then
+          return false, d.payload.state .. ": Parameter `lockScreenDismissalEnabled` is not the same as expected"
+        end
       end
       return true
     end)
-  :ValidIf(function(e, d)
-      if e.occurences == 2 and pLockScreenDismissalEnabled == nil and d.payload.lockScreenDismissalEnabled ~= nil then
-        return false, d.payload.state .. ": " .. msg(d.payload.lockScreenDismissalEnabled)
+  :ValidIf(function(_, d)
+      if d.payload.lockScreenDismissalEnabled == true then
+        if d.payload.lockScreenDismissalWarning == nil then
+          return false, d.payload.state .. "(" .. tostring(d.payload.lockScreenDismissalEnabled) .. ")"
+            .. ": Parameter `lockScreenDismissalWarning` is missing"
+        elseif d.payload.lockScreenDismissalWarning ~= wrnMsg then
+          return false, d.payload.state .. "(" .. tostring(d.payload.lockScreenDismissalEnabled) .. ")"
+            .. ": The value for parameter `lockScreenDismissalWarning` is not the same as expected"
+        end
+      elseif d.payload.lockScreenDismissalWarning ~= nil then
+        return false, d.payload.state .. "(" .. tostring(d.payload.lockScreenDismissalEnabled) .. ")"
+          .. ": Parameter `lockScreenDismissalWarning` is not expected"
       end
       return true
     end)
-  :Times(2)
+end
+
+function c.onDriverDistraction(pState, pLockScreenDismissalEnabled, pAppId)
+  if not pAppId then pAppId = 1 end
+  c.getHMIConnection():SendNotification("UI.OnDriverDistraction", { state = pState })
+  c.expOnDriverDistraction(pState, pLockScreenDismissalEnabled, pAppId)
 end
 
 local preconditionsOrig = c.preconditions
@@ -60,6 +77,26 @@ function c.postconditions()
   commonPreconditions:RestoreFile(preloadedPT)
 end
 
+local function setLockScreenWrnMsg(pMessages)
+  local lang = string.lower(c.getConfigAppParams().languageDesired)
+  local warnSec = pMessages["LockScreenDismissalWarning"]
+  if warnSec == nil then
+    test:FailTestCase("'LockScreenDismissalWarning' message section is not defined in 'sdl_preloaded_pt' file")
+  else
+    local msg = "'LockScreenDismissalWarning' message for '" .. lang .. "' is not defined in 'sdl_preloaded_pt' file"
+    if lang == "en-us" and warnSec.languages[lang] == nil then
+      test:FailTestCase(msg)
+      return
+    end
+    if warnSec.languages[lang] == nil then
+      utils.cprint(35, msg)
+      utils.cprint(35, "'en-us' should be used instead")
+      lang = "en-us"
+    end
+    wrnMsg = warnSec.languages[lang].textBody
+  end
+end
+
 function c.updatePreloadedPT(pLockScreenDismissalEnabled, pUpdateFunc)
   local preloadedFile = commonPreconditions:GetPathToSDL() .. preloadedPT
   local pt = utils.jsonFileToTable(preloadedFile)
@@ -68,6 +105,7 @@ function c.updatePreloadedPT(pLockScreenDismissalEnabled, pUpdateFunc)
   if pUpdateFunc then
     pUpdateFunc(pt)
   end
+  setLockScreenWrnMsg(pt.policy_table.consumer_friendly_messages.messages)
   utils.tableToJsonFile(pt, preloadedFile)
 end
 
