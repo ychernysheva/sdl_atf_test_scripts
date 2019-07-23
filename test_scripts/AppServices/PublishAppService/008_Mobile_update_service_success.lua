c70740eb0ba69f74c17b4168e2423e39d9959c4d
JackLivio <jack@livio.io>
2019-07-23 16:19:44 -0400

    Update test_scripts/AppServices/PublishAppService/008_Mobile_update_service_success.lua
    Co-Authored-By: Jacob Keeler <jacob.keeler@livioradio.com>

--- test_scripts/AppServices/PublishAppService/008_Mobile_update_service_success.lua
+++ test_scripts/AppServices/PublishAppService/008_Mobile_update_service_success.lua
@@ -59,7 +59,7 @@ end
 
 --[[ Local Functions ]]
 local function processRPCSuccess(self)
-  local mobileSession = common.getMobileSession(self, 1)
+  local mobileSession = common.getMobileSession(1)
   local cid = mobileSession:SendRPC(rpc.name, rpc.params)
 
   mobileSession:ExpectNotification("OnSystemCapabilityUpdated", 
