929b28d6773c38faff85bc807889072585b426cf
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2019-08-14 09:02:19 -0400

    Extend script to check a few window capabilities including DeleteWindow

--- test_scripts/WidgetSupport/Capabilities/006_GetSC_subscription_ignored.lua
+++ test_scripts/WidgetSupport/Capabilities/006_GetSC_subscription_ignored.lua
@@ -8,19 +8,25 @@
 -- 1) SDL and HMI are started
 -- 2) App is registered
 -- Steps:
--- 1) HMI sends `OnSystemCapabilityUpdated` to SDL for display capabilities
+-- 1) HMI sends `OnSystemCapabilityUpdated` to SDL for display capabilities for particular window
 -- SDL does:
 --  - transfer `OnSystemCapabilityUpdated` notification to App
 -- 2) App sends `GetSystemCapability` request to SDL (with or without`subscribe` flag) for DISPLAYS capabilities
 -- SDL does:
 --  - proceed with request successfully
---  - provide up-to-date DISPLAYS data to App in response
+--  - provide up-to-date DISPLAYS data to App in response (for all windows)
 ---------------------------------------------------------------------------------------------------
 --[[ Required Shared libraries ]]
 local common = require('test_scripts/WidgetSupport/common')
 
 --[[ Local Variables ]]
 local systemCapabilityParams = common.getOnSystemCapabilityParams()
+local subscription = {
+  [1] = nil,
+  [2] = true,
+  [3] = false,
+  [4] = nil,
+}
 
 --[[ Local Functions ]]
 local function getWidgetParams(pId)
@@ -55,18 +61,40 @@ local function sendGetSC(pType, pSubscribe)
     end)
 end
 
-local function createWindow(pId)
+local function createWindow(pWindowId)
   local disCap = systemCapabilityParams.systemCapability.displayCapabilities[1]
-  disCap.windowCapabilities[pId] = common.cloneTable(disCap.windowCapabilities[1])
-  disCap.windowCapabilities[pId].windowID = pId
+  local winCap = common.cloneTable(disCap.windowCapabilities[1])
+  winCap.windowID = pWindowId
+  winCap.templatesAvailable = { "Template_" .. pWindowId }
+  table.insert(disCap.windowCapabilities, winCap)
   function common.getOnSystemCapabilityParams()
     local caps = common.cloneTable(systemCapabilityParams)
     caps.systemCapability.displayCapabilities[1].windowCapabilities = {
-      [1] = caps.systemCapability.displayCapabilities[1].windowCapabilities[pId]
+      [1] = winCap
     }
     return caps
   end
-  common.createWindow(getWidgetParams(pId))
+  common.createWindow(getWidgetParams(pWindowId))
+end
+
+local function deleteWindow(pId)
+  local disCap = systemCapabilityParams.systemCapability.displayCapabilities[1]
+  for k, v in pairs(disCap.windowCapabilities) do
+    if v.windowID == pId then
+      table.remove(disCap.windowCapabilities, k)
+    end
+  end
+  common.deleteWindow(pId)
+end
+
+local function sendOnSCU()
+  local winCap = systemCapabilityParams.systemCapability.displayCapabilities[1].windowCapabilities[1]
+  winCap.windowID = 0
+  winCap.templatesAvailable = { "Template_0" }
+  local paramsToSDL = common.cloneTable(systemCapabilityParams)
+  paramsToSDL.appID = common.getHMIAppId()
+  common.getHMIConnection():SendNotification("BasicCommunication.OnSystemCapabilityUpdated", paramsToSDL)
+  common.getMobileSession():ExpectNotification("OnSystemCapabilityUpdated", systemCapabilityParams)
 end
 
 --[[ Scenario ]]
@@ -74,20 +102,20 @@ common.Title("Precondition")
 common.Step("Clean environment and Back-up/update PPT", common.precondition)
 common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
 common.Step("App registration", common.registerAppWOPTU)
+common.Step("HMI sends capabilities for main window", sendOnSCU)
 common.Step("App activation", common.activateApp)
 
 common.Title("Test")
-common.Step("Create 1st widget", createWindow, { 1 })
-common.Step("App sends GetSC RPC for DISPLAYS, no subscribe", sendGetSC, { "DISPLAYS", nil })
-
-common.Step("Create 2nd widget", createWindow, { 2 })
-common.Step("App sends GetSC RPC for DISPLAYS, subscribe=true", sendGetSC, { "DISPLAYS", true })
-
-common.Step("Create 3rd widget", createWindow, { 3 })
-common.Step("App sends GetSC RPC for DISPLAYS, subscribe=false", sendGetSC, { "DISPLAYS", false })
-
-common.Step("Create 4th widget", createWindow, { 4 })
-common.Step("App sends GetSC RPC for DISPLAYS, no subscribe", sendGetSC, { "DISPLAYS", nil })
+for i = 1, 4 do
+  common.Step("Create widget " .. i, createWindow, { i })
+  common.Step("App sends GetSC RPC for DISPLAYS, subscribe=" .. tostring(subscription[i]), sendGetSC,
+    { "DISPLAYS", subscription[i] })
+end
+for i = 1, 4 do
+  common.Step("Delete widget " .. i, deleteWindow, { i })
+  common.Step("App sends GetSC RPC for DISPLAYS, subscribe=" .. tostring(subscription[i]), sendGetSC,
+    { "DISPLAYS", subscription[i] })
+end
 
 common.Title("Postconditions")
 common.Step("Stop SDL, restore SDL settings and PPT", common.postcondition)
