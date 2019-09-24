7e8822f00f02b17843799aa0f01b751321b69393
Collin <iCollin@users.noreply.github.com>
2019-09-24 13:56:25 -0400

    move MenuLayoutsAvailable to WindowCapability (#2268)
    * update tests to use new MenuLayoutsAvailable location
    
    * no need to trace correlation id of OnSystemCapabilityUpdated

--- test_scripts/Smoke/API/049_SetMenuLayout_SUCCESS.lua
+++ test_scripts/Smoke/API/049_SetMenuLayout_SUCCESS.lua
@@ -26,7 +26,6 @@
 --[[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
 local commonSmoke = require('test_scripts/Smoke/commonSmoke')
-local hmi_values = require('user_modules/hmi_values')
 
 --[[ Local Variables ]]
 local addSubMenuParams = { 
@@ -44,8 +43,83 @@ local successResponse = {
   resultCode = "SUCCESS"
 }
 
+local onSystemCapabilityUpdatedParams = {
+  systemCapability = {
+    systemCapabilityType = "DISPLAYS",
+    displayCapabilities = {
+      {
+        displayName = "displayName",
+        windowTypeSupported = {
+          {
+            type = "MAIN",
+            maximumNumberOfWindows = 1
+          },
+          {
+            type = "WIDGET",
+            maximumNumberOfWindows = 2
+          }
+        },
+        windowCapabilities = {
+          {
+            menuLayoutsAvailable = { "LIST", "TILES" },
+            textFields = {
+              {
+                name = "mainField1",
+                characterSet = "TYPE2SET",
+                width = 1,
+                rows = 1
+              }
+            },
+            imageFields = {
+              {
+                name = "choiceImage",
+                imageTypeSupported = { "GRAPHIC_PNG"
+                },
+                imageResolution = {
+                  resolutionWidth = 35,
+                  resolutionHeight = 35
+                }
+              }
+            },
+            imageTypeSupported = {
+              "STATIC"
+            },
+            templatesAvailable = {
+              "Template1", "Template2", "Template3", "Template4", "Template5"
+            },
+            numCustomPresetsAvailable = 100,
+            buttonCapabilities = {
+              {
+                longPressAvailable = true,
+                name = "VOLUME_UP",
+                shortPressAvailable = true,
+                upDownAvailable = false
+              }
+            },
+            softButtonCapabilities = {
+              {
+                shortPressAvailable = true,
+                longPressAvailable = true,
+                upDownAvailable = true,
+                imageSupported = true,
+                textSupported = true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 --[[ Local Functions ]]
 local function setMenuLayoutTiles(self)
+  onSystemCapabilityUpdatedParams.appID = commonSmoke.getHMIAppId()
+  self.hmiConnection:SendNotification("BasicCommunication.OnSystemCapabilityUpdated", onSystemCapabilityUpdatedParams)
+  
+  onSystemCapabilityUpdatedParams.appID = nil
+  self.mobileSession1:ExpectNotification("OnSystemCapabilityUpdated", onSystemCapabilityUpdatedParams)
+
   local cid = self.mobileSession1:SendRPC("SetGlobalProperties", setGlobalPropertiesParams)
   
   EXPECT_HMICALL("UI.SetGlobalProperties", {})
@@ -63,16 +137,10 @@ local function setMenuLayoutTiles(self)
   end)
 end
 
-local function getHMIParams()
-  local params = hmi_values.getDefaultHMITable()
-  params.UI.GetCapabilities.params.displayCapabilities.menuLayoutsAvailable = { "LIST", "TILES" }
-  return params
-end
-
 --[[ Scenario ]]
 runner.Title("Preconditions")
 runner.Step("Clean environment", commonSmoke.preconditions)
-runner.Step("Start SDL, HMI, connect Mobile, start Session", commonSmoke.start, { getHMIParams() })
+runner.Step("Start SDL, HMI, connect Mobile, start Session", commonSmoke.start)
 runner.Step("RAI", commonSmoke.registerApp)
 runner.Step("Activate App", commonSmoke.activateApp)
 
