4f79a3fc6b5b742cec58ffed4316eff77040de07
Taras Myza (GitHub) <40491384+TMyza@users.noreply.github.com>
2019-10-23 23:03:47 +0300

    Added 2 scripts for checking start stream from NONE HMI level (#2288)
    * Added 2 scripts for checking stream from NONE HMI level
    
    * Fix issues
    
    * Update test_scripts/API/ServiceStatusUpdateToHMI/027_Video_service_unprotected_start_stream_NONE_hmi_level.lua
    
    Co-Authored-By: Collin <iCollin@users.noreply.github.com>

---
+++ test_scripts/API/ServiceStatusUpdateToHMI/026_Video_service_protected_start_stream_NONE_hmi_level.lua
@@ -0,0 +1,62 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- Proposal:
+-- https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0211-ServiceStatusUpdateToHMI.md
+-----------------------------------------------------------------------------------------------------------------------
+-- Description: Attempt to open protected Video/Audio service with OnServiceUpdate notification in case
+-- App start stream in NONE HMI level
+--
+-- Preconditions:
+-- 1) SDL certificate is missing/expired
+-- 2) App is registered with NAVIGATION appHMIType
+-- Steps:
+-- 1) App sends StartService request (<service_type>, encryption = true)
+-- SDL does:
+--   - send OnServiceUpdate (<service_type>, REQUEST_RECEIVED) to HMI
+--   - send OnServiceUpdate (<service_type>, REQUEST_REJECTED) to HMI
+--   - send StartServiceNACK(<service_type>, encryption = false) to App
+-----------------------------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local common = require('test_scripts/API/ServiceStatusUpdateToHMI/common')
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
+
+--[[ Local Constants ]]
+local serviceId = 11
+
+--[[ Local Functions ]]
+function common.onServiceUpdateFunc(pServiceTypeValue)
+  common.getHMIConnection():ExpectNotification("BasicCommunication.OnServiceUpdate",
+    { serviceEvent = "REQUEST_RECEIVED", serviceType = pServiceTypeValue, appID = common.getHMIAppId() },
+    { serviceEvent = "REQUEST_REJECTED", serviceType = pServiceTypeValue, appID = common.getHMIAppId() })
+  :Times(2)
+end
+
+function common.serviceResponseFunc(pServiceId)
+  common.getMobileSession():ExpectControlMessage(pServiceId, {
+    frameInfo = common.const.FRAME_INFO.START_SERVICE_NACK,
+    encryption = false
+  })
+end
+
+function common.policyTableUpdateFunc()
+  common.getHMIConnection():ExpectNotification("SDL.OnStatusUpdate")
+  :Times(0)
+end
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Init SDL certificates", common.initSDLCertificates,
+  { "./files/Security/client_credential_expired.pem", true })
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+runner.Step("App registration", common.registerApp)
+runner.Step("PolicyTableUpdate", common.policyTableUpdate)
+
+runner.Title("Test")
+runner.Step("Start Video Service protected, App in NONE level",
+  common.startServiceWithOnServiceUpdate, { serviceId, 0, 0 })
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", common.postconditions)
