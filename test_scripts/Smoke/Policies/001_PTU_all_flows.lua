6e5fa1e2928e871c5583957df005a6cf39fc2edc
Oleksandr Deriabin (GitHub) <aderiabin@luxoft.com>
2019-10-24 04:17:39 +0300

    Adapt Smoke/Policies scripts for Remote connection

--- test_scripts/Smoke/Policies/001_PTU_all_flows.lua
+++ test_scripts/Smoke/Policies/001_PTU_all_flows.lua
@@ -4,13 +4,10 @@
 ---------------------------------------------------------------------------------------------
 --[[ Required Shared libraries ]]
 local runner = require('user_modules/script_runner')
-local commonFunctions = require("user_modules/shared_testcases/commonFunctions")
-local mobile_session = require("mobile_session")
-local json = require("modules/json")
-local atf_logger = require("atf_logger")
-local sdl = require("SDL")
-local commonSteps = require("user_modules/shared_testcases/commonSteps")
-local commonSmoke = require('test_scripts/Smoke/commonSmoke')
+local common = require('test_scripts/Smoke/commonSmoke')
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
 
 --[[ Local Variables ]]
 local flowType = {
@@ -18,64 +15,6 @@ local flowType = {
   EXTERNAL_PROPRIETARY = 2,
   HTTP = 3
 }
-
---[[ Local Functions ]]
-local function preconditions()
-  -- Stop SDL if process is still running
-  commonFunctions:SDLForceStop()
-  -- Remove Local Policy Update
-  commonSteps:DeletePolicyTable()
-  -- Delete log files
-  commonSteps:DeleteLogsFiles()
-end
-
--- Allow device from HMI
-local function allowSDL(self)
-  -- sending notification OnAllowSDLFunctionality from HMI to allow connected device
-  self.hmiConnection:SendNotification("SDL.OnAllowSDLFunctionality",
-    { allowed = true, source = "GUI", device = { id = commonSmoke.getDeviceMAC(), name = commonSmoke.getDeviceName() }})
-  return commonSmoke.wait(500)
-end
-
--- Start SDL and HMI, establish connection between SDL and HMI, open mobile connection via TCP
-local function start(self)
-  self:runSDL()
-  commonFunctions:waitForSDLStart(self)
-  :Do(function()
-      self:initHMI(self)
-      :Do(function()
-          commonFunctions:userPrint(35, "HMI initialized")
-          self:initHMI_onReady()
-          :Do(function()
-              commonFunctions:userPrint(35, "HMI is ready")
-              self:connectMobile()
-              :Do(function()
-                  commonFunctions:userPrint(35, "Mobile connected")
-                end)
-            end)
-        end)
-    end)
-end
-
--- Loging messages in terminal
-local function log(...)
-  local str = "[" .. atf_logger.formated_time(true) .. "]"
-  for i, p in pairs({...}) do
-    local delimiter = "\t"
-    if i == 1 then delimiter = " " end
-    str = str .. delimiter .. p
-  end
-  commonFunctions:userPrint(35, str)
-end
-
--- Convert snapshot form json to table
--- @tparam file pts_f snapshot file
-local function ptsToTable(pts_f)
-  local f = io.open(pts_f, "r")
-  local content = f:read("*all")
-  f:close()
-  return json.decode(content)
-end
 
 -- Creation policy table from snapshot
 -- @tparam table ptu snapshot table
@@ -89,7 +28,7 @@ local function getPTUFromPTS(ptu)
   -- remove usage_and_error_counts
   ptu.policy_table.usage_and_error_counts = nil
   -- write empty struct in "DataConsent-2".rpcs
-  ptu.policy_table.functional_groupings["DataConsent-2"].rpcs = json.null
+  ptu.policy_table.functional_groupings["DataConsent-2"].rpcs = common.json.null
   -- remove preloaded_pt
   ptu.policy_table.module_config.preloaded_pt = nil
   -- remove preloaded_date
@@ -108,95 +47,89 @@ local function getPTUFromPTS(ptu)
   ptu.policy_table.vehicle_data = nil
 end
 
--- Save created PT in file
--- @tparam table ptu PT table
--- @tparam string ptu_file_name file name
-local function storePTUInFile(ptu, ptu_file_name)
-  local f = io.open(ptu_file_name, "w")
-  f:write(json.encode(ptu))
-  f:close()
-end
-
 -- Check that PT is sent as binary data in OnSystem request
 -- @tparam table bin_data binary data
 -- @tparam number pFlow number of floe type from flowType
-local function checkIfPTSIsSentAsBinary(bin_data, pFlow, self)
+local function checkIfPTSIsSentAsBinary(bin_data, pFlow)
   -- decode binary data to table depending on policy flow
   local pt = nil
   if bin_data ~= nil and string.len(bin_data) > 0 then
     if flowType[pFlow] == flowType.PROPRIETARY then
-      pt = json.decode(bin_data).HTTPRequest.body
+      pt = common.json.decode(bin_data).HTTPRequest.body
     elseif flowType[pFlow] == flowType.EXTERNAL_PROPRIETARY or flowType[pFlow] == flowType.HTTP then
       pt = bin_data
     end
-    pt = json.decode(pt)
+    pt = common.json.decode(pt)
   end
   -- Check presence of policy_table in decoded PT
   if pt == nil or not pt.policy_table then
-    self:FailTestCase("PTS was not sent to Mobile as binary data in payload of OnSystemRequest")
+    common.failTestCase("PTS was not sent to Mobile as binary data in payload of OnSystemRequest")
   end
 end
 
 -- Policy table update with Proprietary flow
 -- @tparam table ptu_table PT table
 -- @tparam string pFlow policy flow
-local function ptuProprietary(ptu_table, self, pFlow)
+local function ptuProprietary(ptu_table, pFlow)
+  local mobileSession = common.getMobileSession()
+  local hmi = common.getHMIConnection()
   -- Get path to snapshot
-  local pts_file_name = commonFunctions:read_parameter_from_smart_device_link_ini("SystemFilesPath") .. "/"
-  .. commonFunctions:read_parameter_from_smart_device_link_ini("PathToSnapshot")
+  local pts_file_name = common.readParameterFromSDLINI("SystemFilesPath") .. "/"
+  .. common.readParameterFromSDLINI("PathToSnapshot")
   -- create ptu_file_name as tmp file
   local ptu_file_name = os.tmpname()
   -- Send GetPolicyConfigurationData request from HMI to SDL with service 7
-  local requestId = self.hmiConnection:SendRequest("SDL.GetPolicyConfigurationData",
+  local requestId = hmi:SendRequest("SDL.GetPolicyConfigurationData",
       { policyType = "module_config", property = "endpoints" })
-  log("HMI->SDL: RQ: SDL.GetPolicyConfigurationData")
+  common.log("HMI->SDL: RQ: SDL.GetPolicyConfigurationData")
   -- Expect response GetPolicyConfigurationData on HMI side
   EXPECT_HMIRESPONSE(requestId)
   :Do(function()
-      log("SDL->HMI: RS: SDL.GetPolicyConfigurationData")
+    common.log("SDL->HMI: RS: SDL.GetPolicyConfigurationData")
       -- After receiving GetPolicyConfigurationData response send OnSystemRequest notification from HMI
-      self.hmiConnection:SendNotification("BasicCommunication.OnSystemRequest",
+      hmi:SendNotification("BasicCommunication.OnSystemRequest",
         { requestType = "PROPRIETARY", fileName = pts_file_name })
-      log("HMI->SDL: N: BC.OnSystemRequest")
+        common.log("HMI->SDL: N: BC.OnSystemRequest")
       -- Prepare PT for update
       getPTUFromPTS(ptu_table)
       -- Save created PT for update in tmp file
-      storePTUInFile(ptu_table, ptu_file_name)
+      common.tableToJsonFile(ptu_table, ptu_file_name)
       -- Expect receiving of OnSystemRequest notification with snapshot on mobile side
-      self.mobileSession:ExpectNotification("OnSystemRequest", { requestType = "PROPRIETARY" })
+      mobileSession:ExpectNotification("OnSystemRequest", { requestType = "PROPRIETARY" })
       :Do(function(_, d)
           -- After receiving OnSystemRequest notification on mobile side check that
           -- data in notification was sent as binary data
-          checkIfPTSIsSentAsBinary(d.binaryData, pFlow, self)
-          log("SDL->MOB: N: OnSystemRequest")
+          checkIfPTSIsSentAsBinary(d.binaryData, pFlow)
+          common.log("SDL->MOB: N: OnSystemRequest")
           -- Send SystemRequest request with PT for update from mobile side
-          local corIdSystemRequest = self.mobileSession:SendRPC("SystemRequest",
+          local corIdSystemRequest = mobileSession:SendRPC("SystemRequest",
             { requestType = "PROPRIETARY" }, ptu_file_name)
-          log("MOB->SDL: RQ: SystemRequest")
+            common.log("MOB->SDL: RQ: SystemRequest")
           -- Expect SystemRequest request on HMI side
-          EXPECT_HMICALL("BasicCommunication.SystemRequest")
+          hmi:ExpectRequest("BasicCommunication.SystemRequest")
           :Do(function(_, dd)
-              log("SDL->HMI: RQ: BC.SystemRequest")
+              common.log("SDL->HMI: RQ: BC.SystemRequest")
               -- Send SystemRequest response form HMI with resultCode SUCCESS
-              self.hmiConnection:SendResponse(dd.id, dd.method, "SUCCESS", { })
-              log("HMI->SDL: RS: SUCCESS: BC.SystemRequest")
+              hmi:SendResponse(dd.id, dd.method, "SUCCESS", { })
+              common.log("HMI->SDL: RS: SUCCESS: BC.SystemRequest")
               -- Send OnReceivedPolicyUpdate notification from HMI
-              self.hmiConnection:SendNotification("SDL.OnReceivedPolicyUpdate",
+              hmi:SendNotification("SDL.OnReceivedPolicyUpdate",
                 { policyfile = dd.params.fileName })
-              log("HMI->SDL: N: SDL.OnReceivedPolicyUpdate")
+                common.log("HMI->SDL: N: SDL.OnReceivedPolicyUpdate")
             end)
           -- Expect SystemRequest response with resultCode SUCCESS on mobile side
-          self.mobileSession:ExpectResponse(corIdSystemRequest, { success = true, resultCode = "SUCCESS"})
+          mobileSession:ExpectResponse(corIdSystemRequest, { success = true, resultCode = "SUCCESS"})
           -- remove tmp PT file after receiving SystemRequest response on mobile side
           :Do(function() os.remove(ptu_file_name) end)
-          log("SDL->MOB: RS: SUCCESS: SystemRequest")
+          common.log("SDL->MOB: RS: SUCCESS: SystemRequest")
         end)
     end)
 end
 
 -- Policy table update with HTTP flow
 -- @tparam table ptu_table PT table
-local function ptuHttp(ptu_table, self)
+local function ptuHttp(ptu_table)
+  local mobileSession = common.getMobileSession()
   -- name for PT for SystemRequest
   local policy_file_name = "PolicyTableUpdate"
   -- tmp file name for PT file
@@ -204,15 +137,15 @@ local function ptuHttp(ptu_table, self)
   -- Prepare PT for update
   getPTUFromPTS(ptu_table)
   -- Save created PT for update in tmp file
-  storePTUInFile(ptu_table, ptu_file_name)
+  common.tableToJsonFile(ptu_table, ptu_file_name)
   -- Send SystemRequest form mobile app with created PT
-  local corId = self.mobileSession:SendRPC("SystemRequest",
+  local corId = mobileSession:SendRPC("SystemRequest",
     { requestType = "HTTP", fileName = policy_file_name }, ptu_file_name)
-  log("MOB->SDL: RQ: SystemRequest")
+    common.log("MOB->SDL: RQ: SystemRequest")
   -- Expect successful SystemRequest response on mobile side
-  self.mobileSession:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
+  mobileSession:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
   :Do(function()
-      log("SDL->MOB: RS: SUCCESS: SystemRequest")
+    common.log("SDL->MOB: RS: SUCCESS: SystemRequest")
     end)
   -- remove tmp PT file
   os.remove(ptu_file_name)
@@ -220,93 +153,94 @@ end
 
 -- Expect 3 OnStatusUpdate notification on HMI side during PTU
 local function expOnStatusUpdate()
-  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate",
+  common.getHMIConnection():ExpectNotification("SDL.OnStatusUpdate",
     { status = "UPDATE_NEEDED" }, { status = "UPDATING" }, {status = "UP_TO_DATE" })
   :Do(function(_, d)
-      log("SDL->HMI: N: SDL.OnStatusUpdate", d.params.status)
+    common.log("SDL->HMI: N: SDL.OnStatusUpdate", d.params.status)
     end)
   :Times(3)
 end
 
 -- Fail test cases by incorrect PTU
 -- @tparam string pRequestName request name of RPC that is failed expectations
-local function failInCaseIncorrectPTU(pRequestName, self)
-  self:FailTestCase(pRequestName .. " was sent more than once (PTU update was incorrect)")
+local function failInCaseIncorrectPTU(pRequestName)
+  common.failTestCase(pRequestName .. " was sent more than once (PTU update was incorrect)")
 end
 
 -- Registration of application with policy table update
-local function raiPTU(self)
+local function raiPTU()
+  local hmi = common.getHMIConnection()
   expOnStatusUpdate() -- temp solution due to issue in SDL:
   -- SDL.OnStatusUpdate(UPDATE_NEEDED) notification is sent before BC.OnAppRegistered (EXTERNAL_PROPRIETARY flow)
-  allowSDL(self):Do(function()
+  common.mobile.allowSDL():Do(function()
   -- creation mobile session
-  self.mobileSession = mobile_session.MobileSession(self, self.mobileConnection)
+  local mobileSession  = common.createMobileSession(1)
   -- open RPC service in created session
-  self.mobileSession:StartService(7)
+  mobileSession:StartService(7)
   :Do(function()
       -- Send RegisterAppInterface request from mobile application
-      local corId = self.mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
-      log("MOB->SDL: RQ: RegisterAppInterface")
+      local corId = mobileSession:SendRPC("RegisterAppInterface", config.application1.registerAppInterfaceParams)
+      common.log("MOB->SDL: RQ: RegisterAppInterface")
       -- Expect OnAppRegistered on HMI side from SDL
-      EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered",
+      hmi:ExpectNotification("BasicCommunication.OnAppRegistered",
         { application = { appName = config.application1.registerAppInterfaceParams.appName } })
       :Do(function()
-          log("SDL->HMI: N: BC.OnAppRegistered")
-          if sdl.buildOptions.extendedPolicy == "PROPRIETARY"
-          or sdl.buildOptions.extendedPolicy == "EXTERNAL_PROPRIETARY" then
+        common.log("SDL->HMI: N: BC.OnAppRegistered")
+        end)
+      -- Expect RegisterAppInterface response on mobile side with resultCode SUCCESS
+      mobileSession:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
+      :Do(function()
+          common.log("SDL->MOB: RS: RegisterAppInterface")
+          if common.SDL.buildOptions.extendedPolicy == "PROPRIETARY"
+          or common.SDL.buildOptions.extendedPolicy == "EXTERNAL_PROPRIETARY" then
             -- Expect PolicyUpdate request on HMI side
-            EXPECT_HMICALL("BasicCommunication.PolicyUpdate")
+            hmi:ExpectRequest("BasicCommunication.PolicyUpdate")
             :Do(function(e, d)
                 if e.occurences == 1 then -- SDL send BC.PolicyUpdate more than once if PTU update was incorrect
-                  log("SDL->HMI: RQ: BC.PolicyUpdate")
+                  common.log("SDL->HMI: RQ: BC.PolicyUpdate")
                   -- Create PT form snapshot
-                  local ptu_table = ptsToTable(d.params.file)
+                  local ptu_table = common.SDL.PTS.get()
                   -- Sending PolicyUpdate request from HMI with resultCode SUCCESS
-                  self.hmiConnection:SendResponse(d.id, d.method, "SUCCESS", { })
-                  log("HMI->SDL: RS: BC.PolicyUpdate")
+                  hmi:SendResponse(d.id, d.method, "SUCCESS", { })
+                  common.log("HMI->SDL: RS: BC.PolicyUpdate")
                   -- PTU proprietary flow
-                  ptuProprietary(ptu_table, self, sdl.buildOptions.extendedPolicy)
+                  ptuProprietary(ptu_table, common.SDL.buildOptions.extendedPolicy)
                 else
-                  failInCaseIncorrectPTU("BC.PolicyUpdate", self)
+                  failInCaseIncorrectPTU("BC.PolicyUpdate")
                 end
               end)
-          elseif sdl.buildOptions.extendedPolicy == "HTTP" then
+          elseif common.SDL.buildOptions.extendedPolicy == "HTTP" then
             -- Expect OnSystemRequest notification on mobile side
-            self.mobileSession:ExpectNotification("OnSystemRequest")
+            mobileSession:ExpectNotification("OnSystemRequest")
             :Do(function(e, d)
-                log("SDL->MOB: N: OnSystemRequest", e.occurences, d.payload.requestType)
+              common.log("SDL->MOB: N: OnSystemRequest", e.occurences, d.payload.requestType)
                 if d.payload.requestType == "HTTP" then
                   if e.occurences <= 2 then -- SDL send OnSystemRequest more than once if PTU update was incorrect
                     -- Check data in receives OnSystemRequest notification on mobile side
-                    checkIfPTSIsSentAsBinary(d.binaryData, sdl.buildOptions.extendedPolicy, self)
+                    checkIfPTSIsSentAsBinary(d.binaryData, common.SDL.buildOptions.extendedPolicy)
                     if d.binaryData then
                       -- Create PT form binary data
-                      local ptu_table = json.decode(d.binaryData)
+                      local ptu_table = common.json.decode(d.binaryData)
                       -- PTU HTTP flow
-                      ptuHttp(ptu_table, self)
+                      ptuHttp(ptu_table)
                     end
                   else
-                    failInCaseIncorrectPTU("OnSystemRequest", self)
+                    failInCaseIncorrectPTU("OnSystemRequest")
                   end
                 end
               end)
             :Times(2)
           end
-        end)
-      -- Expect RegisterAppInterface response on mobile side with resultCode SUCCESS
-      self.mobileSession:ExpectResponse(corId, { success = true, resultCode = "SUCCESS" })
-      :Do(function()
-          log("SDL->MOB: RS: RegisterAppInterface")
           -- Expect OnHMIStatus with hmiLevel NONE on mobile side form SDL
-          self.mobileSession:ExpectNotification("OnHMIStatus",
+          mobileSession:ExpectNotification("OnHMIStatus",
             { hmiLevel = "NONE", audioStreamingState = "NOT_AUDIBLE", systemContext = "MAIN" })
           :Do(function(_, d)
-              log("SDL->MOB: N: OnHMIStatus", d.payload.hmiLevel)
+            common.log("SDL->MOB: N: OnHMIStatus", d.payload.hmiLevel)
             end)
           -- Expect OnPermissionsChange on mobile side form SDL
-          self.mobileSession:ExpectNotification("OnPermissionsChange")
+          mobileSession:ExpectNotification("OnPermissionsChange")
           :Do(function()
-              log("SDL->MOB: N: OnPermissionsChange")
+            common.log("SDL->MOB: N: OnPermissionsChange")
             end)
           :Times(2)
         end)
@@ -315,32 +249,29 @@ local function raiPTU(self)
 end
 
 -- Check update status
-local function checkPTUStatus(self)
+local function checkPTUStatus()
+  local hmi = common.getHMIConnection()
   -- Send GetStatusUpdate form HMI to SDL
-  local reqId = self.hmiConnection:SendRequest("SDL.GetStatusUpdate")
-  log("HMI->SDL: RQ: SDL.GetStatusUpdate")
+  local reqId = hmi:SendRequest("SDL.GetStatusUpdate")
+  common.log("HMI->SDL: RQ: SDL.GetStatusUpdate")
   -- Expect GetStatusUpdate response from SDL to HMI with update status
-  EXPECT_HMIRESPONSE(reqId, { result = { status = "UP_TO_DATE" }})
+  hmi:ExpectResponse(reqId, { result = { status = "UP_TO_DATE" }})
   :Do(function(_, d)
-      log("HMI->SDL: RS: SDL.GetStatusUpdate", tostring(d.result.status))
+    common.log("HMI->SDL: RS: SDL.GetStatusUpdate", tostring(d.result.status))
     end)
 end
 
 -- Pring in terminal build options(RC status and policy slow)
 local function printSDLConfig()
-  commonFunctions:printTable(sdl.buildOptions)
-end
-
-local function postconditions()
-  StopSDL()
+  print(common.tableToString(common.SDL.buildOptions))
 end
 
 --[[ Scenario ]]
 runner.Title("Preconditions")
 -- Stop SDL if process is still running, delete local policy table and log files
-runner.Step("Clean environment", preconditions)
+runner.Step("Clean environment", common.preconditions)
 -- Start SDL and HMI, establish connection between SDL and HMI, open mobile connection via TCP
-runner.Step("Start SDL, HMI, connect Mobile", start)
+runner.Step("Start SDL, HMI, connect Mobile", common.start)
 -- Pring in terminal build options(RC status and policy slow)
 runner.Step("SDL Configuration", printSDLConfig)
 
@@ -351,4 +282,4 @@ runner.Step("RAI, PTU", raiPTU)
 runner.Step("Check Status", checkPTUStatus)
 
 runner.Title("Postconditions")
-runner.Step("Stop SDL", postconditions)
+runner.Step("Stop SDL", common.postconditions)
