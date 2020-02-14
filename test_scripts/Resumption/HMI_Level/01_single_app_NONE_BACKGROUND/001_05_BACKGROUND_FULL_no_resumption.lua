efa866a822f043da189a22c0f3ae67c4b21498d6
Dmytro Boltovskyi (GitHub) <dboltovskyi@luxoft.com>
2020-02-14 11:03:10 -0500

    Add test scripts to check resumption of HMI level (#2294)

---
+++ test_scripts/Resumption/HMI_Level/01_single_app_NONE_BACKGROUND/001_05_BACKGROUND_FULL_no_resumption.lua
@@ -0,0 +1,37 @@
+---------------------------------------------------------------------------------------------------
+-- Description:
+-- Check absence of HMI level resumption in case if:
+--  - app has BACKGROUND level before unexpected disconnect
+--  - app has been registered and switched to FULL just after unexpected disconnect
+--
+-- Preconditions:
+-- 1) SDL and HMI are started
+-- 2) App is registered and switched to BACKGROUND HMI level
+--
+-- Steps:
+-- 1) App is disconnected unexpectedly and re-registered again
+-- 2) App switched to FULL within default 3 sec. timeout
+-- 3) Timeout expires and SDL starts HMI level resumption process
+-- SDL does not resume app's HMI level
+-- 4) App switched to FULL
+-- SDL does not change app's HMI level
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require("test_scripts/Resumption/HMI_Level/common")
+
+--[[ Scenario ]]
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+common.Step("Register App 1", common.registerApp, { "DEFAULT" })
+common.Step("Set HMI level App 1", common.setAppHMILevel, { "BACKGROUND" })
+
+common.Title("Test")
+common.Step("Unexpected disconnect", common.unexpectedDisconnect)
+common.Step("Register App 1", common.registerApp, { "DEFAULT" })
+common.Step("Set HMI level App 1", common.setAppHMILevel, { "FULL" })
+common.Step("Check HMI level resumption App 1", common.checkHMILevelResumption, { nil })
+common.Step("Activate App 1", common.activateApp, { nil })
+
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
---
+++ test_scripts/Resumption/HMI_Level/02_single_app_LIMITED_FULL/002_03_LIMITED_BACKGROUND_no_resumption.lua
@@ -0,0 +1,37 @@
+---------------------------------------------------------------------------------------------------
+-- Description:
+-- Check absence of HMI level resumption in case if:
+--  - app has LIMITED level before unexpected disconnect
+--  - app has been registered and switched to LIMITED just after unexpected disconnect
+--
+-- Preconditions:
+-- 1) SDL and HMI are started
+-- 2) App is registered and switched to LIMITED HMI level
+--
+-- Steps:
+-- 1) App is disconnected unexpectedly and re-registered again
+-- 2) App switched to LIMITED within default 3 sec. timeout
+-- 3) Timeout expires and SDL starts HMI level resumption process
+-- SDL does not resume app's HMI level
+-- 4) App switched to FULL
+-- SDL does change app's HMI level
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local common = require("test_scripts/Resumption/HMI_Level/common")
+
+--[[ Scenario ]]
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+common.Step("Register App 1", common.registerApp, { "NAVIGATION" })
+common.Step("Set HMI level App 1", common.setAppHMILevel, { "LIMITED" })
+
+common.Title("Test")
+common.Step("Unexpected disconnect", common.unexpectedDisconnect)
+common.Step("Register App 1", common.registerApp, { "DEFAULT" })
+common.Step("Set HMI level App 1", common.setAppHMILevel, { "BACKGROUND" })
+common.Step("Check HMI level resumption App 1", common.checkHMILevelResumption, { nil })
+common.Step("Activate App 1", common.activateApp, { "FULL" })
+
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
