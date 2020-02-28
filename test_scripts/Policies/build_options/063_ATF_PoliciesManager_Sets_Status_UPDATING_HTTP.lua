9ce1ac128890623ecd90f733efd578512dd3412d
jacobkeeler <jacob.keeler@livioradio.com>
2020-02-28 15:04:01 -0500

    Fix style

--- test_scripts/Policies/build_options/063_ATF_PoliciesManager_Sets_Status_UPDATING_HTTP.lua
+++ test_scripts/Policies/build_options/063_ATF_PoliciesManager_Sets_Status_UPDATING_HTTP.lua
@@ -66,18 +66,16 @@ function Test:TestStep_PoliciesManager_changes_status_UPDATING()
   EXPECT_HMINOTIFICATION("BasicCommunication.OnAppRegistered", {application = { appName = config.application1.registerAppInterfaceParams.appName } })
   :Do(function()
 
-
-
-      EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate"):Times(2)
-      :Do(function(exp,data)
-          print("SDL -> HMI: OnStatusUpdate, status: " .. data.params.status)
-          if(data.params.status == "UPDATE_NEEDED" and exp.occurences ~= 1) then
-            self:FailTestCase("SDL.OnStatusUpdate(UPDATE_NEEDED) is not received for first OnStatusUpdate, Received at occurences: " .. exp.occurences)
-          elseif(data.params.status == "UPDATING" and exp.occurences ~= 2) then
-            self:FailTestCase("SDL.OnStatusUpdate(UPDATING) is not received for second OnStatusUpdate, Received at occurences: " .. exp.occurences)
-          end
-        end)
+  EXPECT_HMINOTIFICATION("SDL.OnStatusUpdate"):Times(2)
+  :Do(function(exp,data)
+      print("SDL -> HMI: OnStatusUpdate, status: " .. data.params.status)
+      if(data.params.status == "UPDATE_NEEDED" and exp.occurences ~= 1) then
+        self:FailTestCase("SDL.OnStatusUpdate(UPDATE_NEEDED) is not received for first OnStatusUpdate, Received at occurences: " .. exp.occurences)
+      elseif(data.params.status == "UPDATING" and exp.occurences ~= 2) then
+        self:FailTestCase("SDL.OnStatusUpdate(UPDATING) is not received for second OnStatusUpdate, Received at occurences: " .. exp.occurences)
+      end
     end)
+  end)
 end
 
 --[[ Postconditions ]]
